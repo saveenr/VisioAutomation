494d4de7a9e51b20d25e31e108b9a0e2d3b07c01
saveenr@live.com <saveenr@live.com>
2011-03-26 15:49:11 -0700

    Simplified the launching of VisioAutomation.Scripting via IronPython
    --HG--
    rename : VisioAutomation_3/VisioAutomation.Scripting/visioipy.cmd => VisioAutomation_3/VisioAutomation.Scripting/launchinteractive.cmd
    rename : VisioAutomation_3/VisioAutomation.Scripting/visioipy.py => VisioAutomation_3/VisioAutomation.Scripting/visio.py

--- VisioAutomation_3/VisioAutomation.Scripting/visio.py
+++ VisioAutomation_3/VisioAutomation.Scripting/visio.py
@@ -7,19 +7,10 @@ import System
 # to help sort out where the problem might be
 debug = False
 
-visioipy_startup = os.environ.get( 'IRONPYTHONSTARTUP' , None )
-
-# if the IRONPYTHONSTART env variable exists add it to sys.path
-if ( visioipy_startup != None ) :
-    visioipy_path = os.path.dirname( visioipy_startup )
-    if (visioipy_path not in sys.path) :
-        sys.path.append(visioipy_path)
-
-if ( debug ) :
-	print "IRONPYTHONSTARTUP =", visioipy_startup
-	for i in xrange(len(sys.path)) :
-		print "sys.path[%s] = \"%s\"" % (i, sys.path[i])
-	print
+script_file = os.path.abspath( __file__ )
+visioipy_path = os.path.dirname( script_file )
+if (visioipy_path not in sys.path) :
+    sys.path.append(visioipy_path)
 
 # Get a reference to Visio
 clr.AddReference( "Microsoft.Office.Interop.Visio" )
@@ -27,7 +18,6 @@ import Microsoft.Office.Interop.Visio
 
 # Load the VisioAutomation assemblies
 # Note: Adding this directory to syspath is needed for the imports of these DLLs to work
-sys.path.append( os.path.join( sys.path[0] ,"") ) 
 clr.AddReferenceToFileAndPath( System.IO.Path.Combine( visioipy_path, "VisioAutomation.dll" ) )
 clr.AddReferenceToFileAndPath( System.IO.Path.Combine( visioipy_path, "VisioAutomation.Scripting.dll" ) )
 
@@ -40,6 +30,7 @@ VA = VisioAutomation
 VAS = VisioAutomation.Scripting
 
 # Start a new Scripting session
-if ( "visio" not in dir() ) : visio = VAS.Session()
-assert( visio != None )
+if ( "visio" not in dir() ) : 
+    visio = VAS.Session()
 
+assert( visio != None )
