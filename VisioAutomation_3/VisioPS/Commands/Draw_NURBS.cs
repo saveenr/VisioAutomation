604a6fa5fb4da43248c6b537b998ca235b1275cb
saveenr@live.com <saveenr@live.com>
2011-03-28 17:49:50 -0700

    VisioPS - added Draw-NURBS cmdlet

---
+++ VisioAutomation_3/VisioPS/Commands/Draw_NURBS.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using VisioAutomation;
+using VAS = VisioAutomation.Scripting;
+using VA = VisioAutomation;
+using SMA = System.Management.Automation;
+
+namespace VisioPS.Commands
+{
+    [SMA.Cmdlet("Draw", "NURBS")]
+    public class Draw_NURBS : VisioPS.VisioPSCmdlet
+    {
+        [SMA.Parameter(Position = 0, Mandatory = true)]
+        public double[] ControlPoints { get; set; }
+
+        [SMA.Parameter(Position = 1, Mandatory = true)]
+        public double[] Knots { get; set; }
+
+        [SMA.Parameter(Position = 2, Mandatory = true)]
+        public double[] Weights { get; set; }
+
+        [SMA.Parameter(Position = 3, Mandatory = true)]
+        public int Degree { get; set; }
+        
+
+        protected override void ProcessRecord()
+        {
+            var scriptingsession = this.ScriptingSession;
+            var points = VA.Drawing.DrawingUtil.DoublesToPoints(this.ControlPoints).ToList();
+            var shape = scriptingsession.Draw.DrawNURBSCurve(points,this.Knots,this.Weights,this.Degree);
+            this.WriteObject(shape);
+        }
+    }
+}
