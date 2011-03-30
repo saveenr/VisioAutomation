e29039c3186db2993bc3aa9401659d8232ab218d
saveenr@gmail.com <saveenr@gmail.com>
2011-03-30 04:02:26 -0700

    Moved InnerGlow from VA.Scripting to VA.Effects

--- VisioAutomation_3/VisioAutomation.Scripting/DrawCommandsUtil.cs
+++ VisioAutomation_3/VisioAutomation.Scripting/DrawCommandsUtil.cs
@@ -8,22 +8,6 @@ namespace VisioAutomation.Scripting
 {
     static class DrawCommandsUtil
     {
-
-        private static VA.Drawing.Rectangle[] GetOuterBorderRects(VA.Drawing.Rectangle R, double w)
-        {
-            VA.Drawing.Rectangle[] rects = {
-                                    new VA.Drawing.Rectangle(R.LowerLeft.Add(-w, -w), R.LowerLeft.Add(0, 0)),
-                                    new VA.Drawing.Rectangle(R.LowerRight.Add(0, -w), R.LowerRight.Add(w, 0)),
-                                    new VA.Drawing.Rectangle(R.UpperLeft.Add(-w, 0), R.UpperLeft.Add(0, w)),
-                                    new VA.Drawing.Rectangle(R.UpperRight.Add(0, 0), R.UpperRight.Add(w, w)),
-                                    new VA.Drawing.Rectangle(R.LowerLeft.Add(-w, 0), R.UpperLeft.Add(0, 0)),
-                                    new VA.Drawing.Rectangle(R.LowerRight.Add(0, 0), R.UpperRight.Add(w, 0)),
-                                    new VA.Drawing.Rectangle(R.UpperLeft.Add(0, 0), R.UpperRight.Add(0, w)),
-                                    new VA.Drawing.Rectangle(R.LowerLeft.Add(0, -w), R.LowerRight.Add(0, 0))
-                                };
-            return rects;
-        }
-
         private static Drawing.Rectangle[] GetInnerBorderRects(VA.Drawing.Rectangle R, double w)
         {
             VA.Drawing.Rectangle[] rects = {
@@ -38,57 +22,8 @@ namespace VisioAutomation.Scripting
                                 };
             return rects;
         }
-        public static IVisio.Shape DrawOuterGlow(IVisio.Page page,
-                                                 VA.Drawing.Rectangle rect,
-                                                 double glow_width,
-                                                 VA.Drawing.ColorRGB glow_color,
-                                                 int glow_trans)
-        {
-            const int bg_trans = 1;
-            var bg_color = glow_color;
-
-            var rects = GetOuterBorderRects(rect, glow_width);
-
-            VA.Format.FillPattern[] grads = {
-                                      VA.Format.FillPattern.RadialUpperRight,
-                                      VA.Format.FillPattern.RadialUpperLeft,
-                                      VA.Format.FillPattern.RadialLowerRight,
-                                      VA.Format.FillPattern.RadialLowerLeft,
-                                      VA.Format.FillPattern.LinearRightToLeft,
-                                      VA.Format.FillPattern.LinearLeftToRight,
-                                      VA.Format.FillPattern.LinearBottomToTop,
-                                      VA.Format.FillPattern.LinearTopToBottom
-                                  };
-
-            var shapes = (from r in rects
-                          select page.DrawRectangle(r))
-                .ToArray();
-
-            var update = new VA.ShapeSheet.Update.SIDSRCUpdate();
 
 
-            for (int i=0;i<shapes.Length;i++)
-            {
-                int shapeid = shapes[i].ID;
-
-                update.SetFormula((short)shapeid, VA.ShapeSheet.SRCConstants.FillPattern, (int)grads[i]);
-                update.SetFormula((short)shapeid, VA.ShapeSheet.SRCConstants.FillForegnd, VA.Convert.ColorToFormulaRGB(glow_color) );
-                update.SetFormula((short)shapeid, VA.ShapeSheet.SRCConstants.FillBkgnd, VA.Convert.ColorToFormulaRGB(bg_color) );
-                update.SetFormula((short)shapeid, VA.ShapeSheet.SRCConstants.FillForegndTrans, glow_trans);
-                update.SetFormula((short)shapeid, VA.ShapeSheet.SRCConstants.FillBkgndTrans, bg_trans);
-                update.SetFormula((short)shapeid, VA.ShapeSheet.SRCConstants.LinePattern, 0);
-            }
-
-            update.Execute(page);
-
-            var application = page.Application;
-            var active_window = application.ActiveWindow;
-            active_window.DeselectAll();
-            var group = VA.SelectionHelper.SelectAndGroup(active_window, shapes);
-            VA.ShapeHelper.SetGroupSelectMode(group, IVisio.VisCellVals.visGrpSelModeGroupOnly);
-
-            return group;
-        }
 
         public static IVisio.Shape DrawInnerGlow(IVisio.Page page,
                                                  VA.Drawing.Rectangle rect,
---
+++ VisioAutomation_3/VisioAutomation/Effects/OuterGlow.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using VisioAutomation.Extensions;
+using IVisio = Microsoft.Office.Interop.Visio;
+using VA = VisioAutomation;
+
+namespace VisioAutomation.Effects
+{
+    public class OuterGlow
+    {
+        public VA.Drawing.Rectangle Rectangle { get; set; }
+        public double GlowWidth { get; set; }
+        public VA.Drawing.ColorRGB GlowColor { get; set; }
+        public int GlowTransparency { get; set; }
+
+        public IVisio.Shape Draw(IVisio.Page page)
+        {
+            var glow = this;
+            const int bg_trans = 1;
+            var bg_color = glow.GlowColor;
+
+            var rects = GetOuterBorderRects(glow.Rectangle, glow.GlowWidth);
+
+            VA.Format.FillPattern[] grads = {
+                                      VA.Format.FillPattern.RadialUpperRight,
+                                      VA.Format.FillPattern.RadialUpperLeft,
+                                      VA.Format.FillPattern.RadialLowerRight,
+                                      VA.Format.FillPattern.RadialLowerLeft,
+                                      VA.Format.FillPattern.LinearRightToLeft,
+                                      VA.Format.FillPattern.LinearLeftToRight,
+                                      VA.Format.FillPattern.LinearBottomToTop,
+                                      VA.Format.FillPattern.LinearTopToBottom
+                                  };
+
+            var shapes = (from r in rects
+                          select page.DrawRectangle(r))
+                .ToArray();
+
+            var update = new VA.ShapeSheet.Update.SIDSRCUpdate();
+
+
+            for (int i = 0; i < shapes.Length; i++)
+            {
+                int shapeid = shapes[i].ID;
+
+                update.SetFormula((short)shapeid, VA.ShapeSheet.SRCConstants.FillPattern, (int)grads[i]);
+                update.SetFormula((short)shapeid, VA.ShapeSheet.SRCConstants.FillForegnd, VA.Convert.ColorToFormulaRGB(glow.GlowColor));
+                update.SetFormula((short)shapeid, VA.ShapeSheet.SRCConstants.FillBkgnd, VA.Convert.ColorToFormulaRGB(bg_color));
+                update.SetFormula((short)shapeid, VA.ShapeSheet.SRCConstants.FillForegndTrans, glow.GlowTransparency);
+                update.SetFormula((short)shapeid, VA.ShapeSheet.SRCConstants.FillBkgndTrans, bg_trans);
+                update.SetFormula((short)shapeid, VA.ShapeSheet.SRCConstants.LinePattern, 0);
+            }
+
+            update.Execute(page);
+
+            var application = page.Application;
+            var active_window = application.ActiveWindow;
+            active_window.DeselectAll();
+            var group = VA.SelectionHelper.SelectAndGroup(active_window, shapes);
+            VA.ShapeHelper.SetGroupSelectMode(group, IVisio.VisCellVals.visGrpSelModeGroupOnly);
+
+            return group;
+        }
+
+        private static VA.Drawing.Rectangle[] GetOuterBorderRects(VA.Drawing.Rectangle R, double w)
+        {
+            VA.Drawing.Rectangle[] rects = {
+                                    new VA.Drawing.Rectangle(R.LowerLeft.Add(-w, -w), R.LowerLeft.Add(0, 0)),
+                                    new VA.Drawing.Rectangle(R.LowerRight.Add(0, -w), R.LowerRight.Add(w, 0)),
+                                    new VA.Drawing.Rectangle(R.UpperLeft.Add(-w, 0), R.UpperLeft.Add(0, w)),
+                                    new VA.Drawing.Rectangle(R.UpperRight.Add(0, 0), R.UpperRight.Add(w, w)),
+                                    new VA.Drawing.Rectangle(R.LowerLeft.Add(-w, 0), R.UpperLeft.Add(0, 0)),
+                                    new VA.Drawing.Rectangle(R.LowerRight.Add(0, 0), R.UpperRight.Add(w, 0)),
+                                    new VA.Drawing.Rectangle(R.UpperLeft.Add(0, 0), R.UpperRight.Add(0, w)),
+                                    new VA.Drawing.Rectangle(R.LowerLeft.Add(0, -w), R.LowerRight.Add(0, 0))
+                                };
+            return rects;
+        }
+
+    }
+}
