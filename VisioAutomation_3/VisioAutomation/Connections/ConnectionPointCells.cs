745be85a3b5491f9992a0a6ae5e71e0b3df82f01
saveenr@gmail.com <saveenr@gmail.com>
2011-04-08 00:50:09 -0700

    separated classes into their own files

--- VisioAutomation_3/VisioAutomation/Connections/ConnectionPointCells.cs
+++ VisioAutomation_3/VisioAutomation/Connections/ConnectionPointCells.cs
@@ -5,13 +5,6 @@ using System.Linq;
 
 namespace VisioAutomation.Connections
 {
-    public enum ConnectionPointType
-    {
-        Inward = IVisio.VisCellVals.visCnnctTypeInward,
-        Outward = IVisio.VisCellVals.visCnnctTypeOutward,
-        InwardOutward = IVisio.VisCellVals.visCnnctTypeInwardOutward
-    }
-
     public class ConnectionPointCells
     {
         public VA.ShapeSheet.CellData<double> X { get; set; }
@@ -64,82 +57,4 @@ namespace VisioAutomation.Connections
         }
 
     }
-
-    public static class ConnectionPointHelper
-    {
-        public static int AddConnectionPoint(
-            IVisio.Shape shape,
-            Connections.ConnectionPointCells cp)
-        {
-            if (shape == null)
-            {
-                throw new System.ArgumentNullException("shape");
-            }
-
-            if (!cp.X.Formula.HasValue)
-            {
-                throw new System.ArgumentException("Must provide an X Formula");
-            }
-
-            if (!cp.Y.Formula.HasValue)
-            {
-                throw new System.ArgumentException("Must provide an Y Formula");
-            }
-
-            var n = shape.AddRow((short)IVisio.VisSectionIndices.visSectionConnectionPts,
-                                 (short)IVisio.VisRowIndices.visRowLast,
-                                 (short)IVisio.VisRowTags.visTagCnnctPt);
-
-            var update = new VA.ShapeSheet.Update.SRCUpdate();
-            cp.Apply(update,n);
-            update.Execute(shape);
-
-            return n;
-        }
-
-
-        public static void DeleteConnectionPoint(IVisio.Shape shape, int index)
-        {
-            if (shape == null)
-            {
-                throw new System.ArgumentNullException("shape");
-            }
-
-            if (index < 0)
-            {
-                throw new System.ArgumentOutOfRangeException("index");
-            }
-
-            var row = (IVisio.VisRowIndices)index;
-            shape.DeleteRow(ConnectionPointCells.query.Section, (short)row);
-        }
-
-        public static int GetConnectionPointCount(IVisio.Shape shape)
-        {
-            if (shape == null)
-            {
-                throw new System.ArgumentNullException("shape");
-            }
-
-            return shape.RowCount[ConnectionPointCells.query.Section];
-        }
-
-        public static int DeleteAllConnectionPoints(IVisio.Shape shape)
-        {
-            if (shape == null)
-            {
-                throw new System.ArgumentNullException("shape");
-            }
-
-            int n = GetConnectionPointCount(shape);
-            for (int i = n - 1; i >= 0; i--)
-            {
-                DeleteConnectionPoint(shape, i);
-            }
-
-            return n;
-        }
-    }
-
-
 }
