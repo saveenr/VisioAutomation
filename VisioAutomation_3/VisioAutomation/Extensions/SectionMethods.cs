1a115230e8aee6fd43831df9e6086e26c65f1916
saveenr@live.com <saveenr@live.com>
2011-06-21 08:15:31 -0700

    Added unit test for section.AsEnumerable()

--- VisioAutomation_3/VisioAutomation/Extensions/SectionMethods.cs
+++ VisioAutomation_3/VisioAutomation/Extensions/SectionMethods.cs
@@ -5,6 +5,8 @@ namespace VisioAutomation.Extensions
 {
     public static class SectionMethods
     {
+        // Section object: http://msdn.microsoft.com/en-us/library/ms408988(v=office.12).aspx
+
         public static IEnumerable<IVisio.Row> AsEnumerable(this IVisio.Section section)
         {
             int row_count = section.Count;
