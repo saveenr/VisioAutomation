d975aed7f325e35613dbf699469ead9f421cd8af
snd\saveenr_cp <none@none>
2011-07-04 13:02:51 -0700

    internal refactoring

--- VisioAutomation_3/VisioAutomation.Metadata/MetadataDB.cs
+++ VisioAutomation_3/VisioAutomation.Metadata/MetadataDB.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Data;
 using System.Reflection;
 using System.Xml;
+using System.Xml.Linq;
 using VA=VisioAutomation;
 
 namespace VisioAutomation.Metadata
@@ -44,18 +45,20 @@ namespace VisioAutomation.Metadata
             List<Section> xsections;
             List<AutomationConstant> xautomationconstants;
 
+            string cells_filename = System.IO.Path.Combine(path, "cells.xml");
+            string cv_filename = System.IO.Path.Combine(path, "cellvalues.xml");
+            string sec_filename = System.IO.Path.Combine(path, "sections.xml");
+            string con_filename = System.IO.Path.Combine(path, "constants.xml");
+
             var cellreader = new XmlPersist.XmlTableReader<Cell>();
-            xcells = cellreader.LoadFromFile(System.IO.Path.Combine(path, "cells.xml")).ToList();
-
             var cvreader = new XmlPersist.XmlTableReader<CellValue>();
-            xcellvalues = cvreader.LoadFromFile(System.IO.Path.Combine(path, "cellvalues.xml")).ToList();
-
             var secreader = new XmlPersist.XmlTableReader<Section>();
-            xsections = secreader.LoadFromFile(System.IO.Path.Combine(path, "sections.xml")).ToList();
-
             var conreader = new XmlPersist.XmlTableReader<AutomationConstant>();
-            xautomationconstants = conreader.LoadFromFile(System.IO.Path.Combine(path, "constants.xml")).ToList();
 
+            xcells = cellreader.LoadFromFile(cells_filename).ToList();
+            xcellvalues = cvreader.LoadFromFile(cv_filename).ToList();
+            xsections = secreader.LoadFromFile(sec_filename).ToList();
+            xautomationconstants = conreader.LoadFromFile(con_filename).ToList();
         }
         /*
          * NOTES
@@ -64,12 +67,85 @@ namespace VisioAutomation.Metadata
 
         private MetadataDB()
         {
-            initconstants();
-            initautoenums();
-            initcellvalues();
-            initcells();
-            initsections();
-            initcellvalueenums();
+
+            var dom_constants = System.Xml.Linq.XDocument.Parse(VA.Metadata.Properties.Resources.constants);
+            var dom_cells = System.Xml.Linq.XDocument.Parse(VA.Metadata.Properties.Resources.cells);
+            var dom_cv = System.Xml.Linq.XDocument.Parse(VA.Metadata.Properties.Resources.cells);
+            var dom_sec = System.Xml.Linq.XDocument.Parse(VA.Metadata.Properties.Resources.sections);
+            init_from_doms(dom_cells, dom_sec, dom_constants, dom_cv);
+        }
+
+        private void init_from_doms(XDocument dom_cells, XDocument dom_sec, XDocument dom_constants, XDocument dom_cv)
+        {
+// Load constants
+            var conreader = new XmlPersist.XmlTableReader<AutomationConstant>();
+
+            this._constants = conreader.Load(dom_constants).ToList();
+            this._name_to_constants = this._constants.ToDictionary(i => i.Name, i => i);
+
+            // Create enums from the constants
+            this._autoenums = new List<AutomationEnum>();
+            this._name_to_autoenums = new Dictionary<string, AutomationEnum>();
+            foreach (var constant in this.Constants)
+            {
+                var enum_name = constant.Enum;
+                AutomationEnum a_enum = null;
+                this._name_to_autoenums.TryGetValue(constant.Enum, out a_enum);
+                if (a_enum == null)
+                {
+                    a_enum = new AutomationEnum(constant.Enum);
+                    this._name_to_autoenums[a_enum.Name] = a_enum;
+                    this._autoenums.Add(a_enum);
+                }
+
+                a_enum.Add(constant.Name, constant.GetValueAsInt());
+            }
+
+            // Load cell values
+            var cvreader = new XmlPersist.XmlTableReader<CellValue>();
+            this._cellvals = cvreader.Load(dom_cv).ToList();
+
+            // Load Cell data
+            var cellreader = new XmlPersist.XmlTableReader<Cell>();
+            this._cells = cellreader.Load(dom_cells).ToList();
+
+            // Index cells
+            this._namecode_to_cell = this.Cells.ToDictionary(i => i.NameCode, i => i);
+
+            // Initialize sections
+            var secreader = new XmlPersist.XmlTableReader<Section>();
+            this._sections = secreader.Load(dom_sec).ToList();
+
+            this._int_to_section = new Dictionary<int, Section>();
+            foreach (var section in this.Sections)
+            {
+                string secindex_name = section.Enum;
+                int secindex_int = this.GetAutomationConstantByName(secindex_name).GetValueAsInt();
+
+                this._int_to_section[secindex_int] = section;
+            }
+
+            // load the cell values
+
+            this._name_to_cellvalueenums = new Dictionary<string, CellValueEnum>();
+
+            foreach (var c in this.CellValues)
+            {
+                string enum_name = c.Enum;
+
+                bool s;
+                CellValueEnum cve;
+                s = this._name_to_cellvalueenums.TryGetValue(enum_name, out cve);
+                if (!s)
+                {
+                    cve = new CellValueEnum();
+                    cve.Name = enum_name;
+                    cve.Items = new List<CellValue>();
+                    this._name_to_cellvalueenums[enum_name] = cve;
+                }
+
+                cve.Items.Add(c);
+            }
         }
 
         public static MetadataDB Load()
@@ -121,87 +197,6 @@ namespace VisioAutomation.Metadata
         public List<CellValue> CellValues
         {
             get { return this._cellvals; }
-        }
-
-        private void initautoenums()
-        {
-            this._autoenums = new List<AutomationEnum>();
-            this._name_to_autoenums = new Dictionary<string, AutomationEnum>();
-            foreach (var constant in this.Constants)
-            {
-                var enum_name = constant.Enum;
-                AutomationEnum a_enum=null;
-                this._name_to_autoenums.TryGetValue(constant.Enum, out a_enum);
-                if (a_enum ==null)
-                {
-                    a_enum = new AutomationEnum(constant.Enum);
-                    this._name_to_autoenums[a_enum.Name] = a_enum;
-                    this._autoenums.Add(a_enum);
-                }
-
-                a_enum.Add(constant.Name,constant.GetValueAsInt());
-            }
-        }
-
-        private void initcells()
-        {
-            var cellreader = new XmlPersist.XmlTableReader<Cell>();
-            this._cells  = cellreader.LoadFromString(VA.Metadata.Properties.Resources.cells).ToList();
-            this._namecode_to_cell = this.Cells.ToDictionary(i => i.NameCode, i => i);
-        }
-
-        private void initsections()
-        {
-            var secreader = new XmlPersist.XmlTableReader<Section>();
-            this._sections = secreader.LoadFromString(VA.Metadata.Properties.Resources.sections).ToList();
-            
-            this._int_to_section = new Dictionary<int, Section>();
-            foreach (var section in this.Sections)
-            {
-                string secindex_name = section.Enum;
-                int secindex_int = this.GetAutomationConstantByName(secindex_name).GetValueAsInt();
-
-                this._int_to_section[secindex_int] = section;
-            }
-        }
-
-        private void initconstants()
-        {
-            var conreader = new XmlPersist.XmlTableReader<AutomationConstant>();
-            this._constants = conreader.LoadFromString(VA.Metadata.Properties.Resources.constants).ToList();
-            this._name_to_constants = this._constants.ToDictionary(i => i.Name, i => i);
-        }
-
-
-        private void initcellvalues()
-        {
-            var cvreader = new XmlPersist.XmlTableReader<CellValue>();
-            this._cellvals = cvreader.LoadFromString(VA.Metadata.Properties.Resources.cellvalues).ToList();
-        }
-
-        private void initcellvalueenums()
-        {
-
-            this._name_to_cellvalueenums = new Dictionary<string, CellValueEnum>();
-            
-            foreach (var c in this.CellValues)
-            {
-                string enum_name = c.Enum;
-
-                bool s;
-                CellValueEnum cve;
-                s= this._name_to_cellvalueenums.TryGetValue(enum_name, out cve);
-                if (!s)
-                {
-                    cve = new CellValueEnum();
-                    cve.Name = enum_name;
-                    cve.Items = new List<CellValue>();
-                    this._name_to_cellvalueenums[enum_name] = cve;
-                }
-
-                cve.Items.Add(c);
-
-            }
         }
     }
 }
