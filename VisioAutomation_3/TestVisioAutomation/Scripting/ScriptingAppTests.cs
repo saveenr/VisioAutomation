ccb8a4f279b8c9e091ff267c8fdf73818b83d972
snd\saveenr_cp <none@none>
2011-07-31 15:16:37 -0700

    Added a GetHelp method for ScriptingSession commands

--- VisioAutomation_3/TestVisioAutomation/Scripting/ScriptingAppTests.cs
+++ VisioAutomation_3/TestVisioAutomation/Scripting/ScriptingAppTests.cs
@@ -5,87 +5,18 @@ using VA = VisioAutomation;
 namespace TestVisioAutomation
 {
     [TestClass]
-    public class ScriptingAppTests : VisioAutomationTest
+    public class ScriptingSessionTests : VisioAutomationTest
     {
         [TestMethod]
         public void Scripting_Test_GetWindowText()
         {
             var ss = GetScriptingSession();
-            var t = ss.Application.GetWindowText();
+
+            string t1 = ss.Document.GetHelp();
+            string t2 = ss.CustomProp.GetHelp();
+
 
         }
 
-        [TestMethod]
-        public void Scripting_Test_Resize_Application_Window()
-        {
-            var ss = GetScriptingSession();
-
-            var old_size = ss.Application.GetWindowSize();
-            var desired_size = new System.Drawing.Size(600, 800);
-
-            ss.Application.SetWindowSize(desired_size.Width, desired_size.Height);
-
-            var actual_size = ss.Application.GetWindowSize();
-            Assert.AreEqual(desired_size, actual_size);
-            ss.Application.SetWindowSize(old_size.Width, old_size.Height);
-            actual_size = ss.Application.GetWindowSize();
-            Assert.AreEqual(old_size, actual_size);
-        }
-
-        [TestMethod]
-        public void Scripting_Test_Resize_Application_Window2()
-        {
-            var ss = GetScriptingSession();
-
-            var doc = ss.Document.NewDocument(10, 5);
-
-            Assert.IsTrue(ss.HasActiveDrawing());
-
-            var pagesize = ss.Page.GetPageSize();
-            Assert.AreEqual(10.0, pagesize.Width);
-            Assert.AreEqual(5.0, pagesize.Height);
-            Assert.AreEqual(0, ss.Layout.GetSelectedShapeCount());
-            ss.Draw.DrawRectangle(1, 1, 2, 2);
-            Assert.AreEqual(1, ss.Layout.GetSelectedShapeCount());
-
-            ss.Document.CloseAllDocumentsWithoutSaving();
-        }
-
-        [TestMethod]
-        public void Scripting_Test_App_to_Front()
-        {
-            var ss = GetScriptingSession();
-            ss.Application.WindowToFront();
-        }
-
-        [TestMethod]
-        public void Scripting_Test_Undo()
-        {
-            var ss = GetScriptingSession();
-            var drawing = ss.Document.NewDocument(8.5, 11);
-            var page = ss.Page.NewPage(new VA.Drawing.Size(8.5, 11), false);
-            Assert.AreEqual(0, page.Shapes.Count);
-            page.DrawRectangle(1, 1, 3, 3);
-            Assert.AreEqual(1, page.Shapes.Count);
-            ss.Undo();
-            Assert.AreEqual(0, page.Shapes.Count);
-            ss.Document.CloseDocument(true);
-        }
-
-        [TestMethod]
-        public void Scripting_Test_close_all()
-        {
-            var ss = GetScriptingSession();
-            var doc1 = ss.Document.NewDocument(10, 5);
-            var doc2 = ss.Document.NewDocument(10, 5);
-            var doc3 = ss.Document.NewDocument(10, 5);
-
-            ss.Document.CloseAllDocumentsWithoutSaving();
-
-            Assert.IsFalse(ss.HasActiveDrawing());
-            var application = ss.VisioApplication;
-            var documents = application.Documents;
-            Assert.AreEqual(0, documents.Count);
-        }
     }
 }
--- VisioAutomation_3/VisioAutomation.Scripting/SessionCommands.cs
+++ VisioAutomation_3/VisioAutomation.Scripting/SessionCommands.cs
@@ -1,6 +1,7 @@
-﻿using IVisio = Microsoft.Office.Interop.Visio;
+﻿using System.Collections.Generic;
+using IVisio = Microsoft.Office.Interop.Visio;
 using VA=VisioAutomation;
-
+using System.Linq;
 namespace VisioAutomation.Scripting
 {
     public class SessionCommands
@@ -13,5 +14,54 @@ namespace VisioAutomation.Scripting
         {
             this.Session = session;
         }       
+
+        public virtual string GetHelp()
+        {
+            var lines = new List<string>();
+
+            var mytype = this.GetType();
+
+            // retrieve all public nonstatic methods
+            var methods = mytype.GetMethods().Where(m=>m.IsPublic).Where(m=>!m.IsStatic);
+            
+            var sb = new System.Text.StringBuilder();
+            foreach (var method in methods)
+            {
+                if (method.Name == "ToString" || method.Name == "GetHashCode" || method.Name == "GetType" || method.Name == "Equals")
+                {
+                    continue;
+                }
+
+                sb.Length = 0;
+                var method_params = method.GetParameters();
+                int i = 0;
+                foreach (var param in method_params)
+                {
+                    if (i > 0)
+                    {
+                        sb.Append(", ");
+                    }
+
+                    string paramtext = string.Format("[{0}] {1}", param.ParameterType.Name, param.Name);
+                    sb.Append(paramtext);
+
+                    i++;
+                }
+                
+                string line = string.Format("{0}({1})", method.Name, sb.ToString());
+
+                lines.Add(line.ToString());
+
+            }
+
+            var helpstr = new System.Text.StringBuilder(lines.Select(s => s.Length + 2).Sum());
+            foreach (var line in lines)
+            {
+                helpstr.Append(line);
+                helpstr.Append("\r\n");
+            }
+
+            return helpstr.ToString();
+        }
     }
 }
