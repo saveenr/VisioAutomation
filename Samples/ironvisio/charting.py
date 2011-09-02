34e756fe19e99e2eaf1f9914a980a6cf858fe531
saveenr@live.com <saveenr@live.com>
2011-09-02 10:13:39 -0700

    aligned internal names of rects and shapes

--- Samples/ironvisio/charting.py
+++ Samples/ironvisio/charting.py
@@ -15,19 +15,20 @@ class VerticalBarChart :
         self.Origin = Point(0,0)
 
     def Draw(self, page) :
+
         # Calculate Geometry
         numpoints = len(self.DataPoints)
         heights = normalize_to( (p.Value for p in self.DataPoints), self.MaxHeight)
-        bar_origin = self.Origin.AddSize( Size(0, self.CategoryDistance+self.CategoryHeight) )
-        bar_rects = get_rects_horiz_vary_heights( bar_origin, self.BarWidth, heights, self.HorizontalDistance, numpoints )
-        cat_rects = get_rects_horiz( self.Origin , Size(self.BarWidth,self.CategoryHeight), self.HorizontalDistance, numpoints )
+        top_row_origin = self.Origin.AddSize( Size(0, self.CategoryDistance+self.CategoryHeight) )
+        top_row_rects = get_rects_horiz_vary_heights( top_row_origin, self.BarWidth, heights, self.HorizontalDistance, numpoints )
+        bottom_row_rects = get_rects_horiz( self.Origin , Size(self.BarWidth,self.CategoryHeight), self.HorizontalDistance, numpoints )
 
         # draw bars
-        barshapes = drawrects( page, bar_rects )
+        barshapes = drawrects( page, top_row_rects )
         settext( barshapes, [ p.Label for p in self.DataPoints ] )
 
         # draw category textboxes
-        catshapes = drawrects( page, cat_rects )
+        catshapes = drawrects( page, bottom_row_rects )
         settext( catshapes, self.Categories )
 
 
@@ -36,7 +37,7 @@ class CircleChart :
     def __init__( self) : 
         self.DataPoints = []
         self.Categories = []
-        self.MaxRadius= 0.5
+        self.MaxHeight = 1.0
         self.HorizontalDistance=0.5
         self.CategoryHeight = 0.5
         self.CategoryDistance=0.0
@@ -48,20 +49,20 @@ class CircleChart :
                 
         normalized_values = normalize( (p.Value for p in self.DataPoints) )
         radii = [ math.sqrt(v/math.pi) for v in normalized_values]
-        radii = normalize_to( radii, self.MaxRadius )
+        radii = normalize_to( radii, self.MaxHeight/2.0 )
 
-        bar_origin = self.Origin.AddSize( Size(0, self.CategoryDistance+self.CategoryHeight) )
-        bar_rects = get_rects_horiz( bar_origin, Size(self.MaxRadius*2, self.MaxRadius*2), self.HorizontalDistance, numpoints )
-        centers = [ r.Center for r in bar_rects ]
+        top_row_origin = self.Origin.AddSize( Size(0, self.CategoryDistance+self.CategoryHeight) )
+        top_row_rects = get_rects_horiz( top_row_origin, Size(self.MaxHeight, self.MaxHeight), self.HorizontalDistance, numpoints )
+        centers = [ r.Center for r in top_row_rects ]
         circlerects = [ Rectangle.FromPointAndRadius(c,r) for (c,r) in zip(centers,radii) ]
-        cat_rects = get_rects_horiz( self.Origin , Size(2*self.MaxRadius,self.CategoryHeight), self.HorizontalDistance, numpoints )
+        bottom_row_rects = get_rects_horiz( self.Origin , Size(self.MaxHeight,self.CategoryHeight), self.HorizontalDistance, numpoints )
 
         # draw circle
         circleshapes = drawovals(page, circlerects)
         settext( circleshapes, [p.Label for p in self.DataPoints] )
 
         # draw category textboxes
-        catshapes = drawrects( page, cat_rects )
+        catshapes = drawrects( page, bottom_row_rects )
         settext( catshapes, self.Categories )
 
 def drawrects( page, rects ) :
