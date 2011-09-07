e3e516ac235ed23b137d9ffce858df2e0747e043
saveenr@live.com <saveenr@live.com>
2011-09-07 03:45:06 -0700

    Separated classes into files
    --HG--
    rename : Samples/ironvisio/InfoGraphicsPy/Class1.cs => Samples/ironvisio/InfoGraphicsPy/Session.cs

---
+++ Samples/ironvisio/InfoGraphicsPy/DataPoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using IVisio=Microsoft.Office.Interop.Visio;
+using IG=InfoGraphicsPy;
+using System.Linq;
+using VA=VisioAutomation;
+using VisioAutomation.Extensions;
+
+namespace InfoGraphicsPy
+{
+    public class DataPoint
+    {
+        public double Value;
+        public string Text;
+        public string Tooltip;
+
+        public DataPoint(double v)
+        {
+            this.Value = v;
+            this.Text = null;
+            this.Tooltip = null;
+        }
+
+        public DataPoint(double v, string t)
+        {
+            this.Value = v;
+            this.Text = t;
+            this.Tooltip = null;
+        }
+
+    }
+}
