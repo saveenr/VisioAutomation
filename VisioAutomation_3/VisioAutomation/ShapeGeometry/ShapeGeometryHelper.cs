4728d31f173fcecfbd34bf71aa6baf03654cd5e5
saveenr@live.com <saveenr@live.com>
2011-09-18 03:16:04 -0700

    Removed unused code

--- VisioAutomation_3/VisioAutomation/ShapeGeometry/ShapeGeometryHelper.cs
+++ VisioAutomation_3/VisioAutomation/ShapeGeometry/ShapeGeometryHelper.cs
@@ -6,39 +6,6 @@ namespace VisioAutomation.ShapeGeometry
 {
     public static class ShapeGeometryHelper
     {
-        public static short AddGeometryMoveToRow(IVisio.Shape shape, short sec_index)
-        {
-            if (shape == null)
-            {
-                throw new System.ArgumentNullException("shape");
-            }
-
-            if (sec_index < ((int)IVisio.VisSectionIndices.visSectionFirstComponent))
-            {
-                throw new System.ArgumentException("sec_index must be >= VisSectionIndices.visSectionFirstComponent");
-            }
-
-            short row_index = shape.AddRow(sec_index, (short)IVisio.VisRowIndices.visRowVertex, (short)IVisio.VisRowTags.visTagMoveTo);
-
-            return row_index;
-        }
-
-        public static short AddGeometryLineToRow(IVisio.Shape shape, short sec_index)
-        {
-            if (shape == null)
-            {
-                throw new System.ArgumentNullException("shape");
-            }
-
-            if (sec_index < ((int)IVisio.VisSectionIndices.visSectionFirstComponent))
-            {
-                throw new System.ArgumentException("sec_index must be >= VisSectionIndices.visSectionFirstComponent");
-            }
-            short row_index = shape.AddRow(sec_index, (short)IVisio.VisRowIndices.visRowVertex, (short)IVisio.VisRowTags.visTagLineTo);
-
-            return row_index;
-        }
-
         public static short AddGeometrySection(IVisio.Shape shape)
         {
             if (shape == null)
@@ -47,11 +14,35 @@ namespace VisioAutomation.ShapeGeometry
             }
 
             int num_geometry_sections = shape.GeometryCount;
-            short new_sec_index = (short)(((int)IVisio.VisSectionIndices.visSectionFirstComponent) + (num_geometry_sections));
-            short id = shape.AddSection(new_sec_index);
+            short new_sec_index = GetGeometrySectionIndex((short)num_geometry_sections);
+            short actual_sec_index = shape.AddSection(new_sec_index);
+
+            if (actual_sec_index != new_sec_index)
+            {
+                throw new VA.AutomationException("Internal Error");
+            }
             short row_index = shape.AddRow(new_sec_index, (short)IVisio.VisRowIndices.visRowComponent, (short)IVisio.VisRowTags.visTagComponent);
 
             return new_sec_index;
+        }
+
+        public static short GetGeometrySectionIndex(short index)
+        {
+            short new_sec_index =
+                (short) (((int) IVisio.VisSectionIndices.visSectionFirstComponent) + (index));
+            return new_sec_index;
+        }
+
+        public static void DeleteGeometrySection(IVisio.Shape shape, short section_index)
+        {
+            if (shape == null)
+            {
+                throw new System.ArgumentNullException("shape");
+            }
+
+            int num_geometry_sections = shape.GeometryCount;
+            short target_section_index = GetGeometrySectionIndex((short)num_geometry_sections);
+            shape.DeleteSection(target_section_index);
         }
     }
 }
