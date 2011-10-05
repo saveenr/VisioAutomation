cd26b1506c345ac3b0f1713f6db06ac82c760a74
snd\saveenr_cp <none@none>
2011-10-05 14:14:04 -0700

    Added a specific collection to hold DirectGraphShapes

--- VisioAutomation_3/VisioAutomation.Layout.MSAGL/DirectedGraph/Drawing.cs
+++ VisioAutomation_3/VisioAutomation.Layout.MSAGL/DirectedGraph/Drawing.cs
@@ -3,12 +3,78 @@ using System.Linq;
 using VA = VisioAutomation;
 using IVisio=Microsoft.Office.Interop.Visio;
 using VisioAutomation.Extensions;
+using System.Collections;
 
 namespace VisioAutomation.Layout.DirectedGraph
 {
+    public class IDList<T> : IEnumerable<T>
+    {
+        private Dictionary<string, T> items;
+
+        public IDList()
+        {
+            this.items  = new Dictionary<string, T>();
+        }
+
+        public void Add(string id, T g)
+        {
+            this.items.Add(id,g);
+        }
+
+        public T this[string index]
+        {
+            get { return this.items[index]; }
+        }
+
+        public int Count
+        {
+            get { return this.items.Count; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var i in this.items.Values)
+            {
+                yield return i;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()     // Explicit implementation
+        {                                           // keeps it hidden.
+            return GetEnumerator();
+        }
+
+        public bool ContainsKey(string id)
+        {
+            return this.items.ContainsKey(id);
+        }
+
+        public IEnumerable<string> IDs
+        {
+            get
+            {
+                foreach (var id in this.items.Keys)
+                {
+                    yield return id;
+                }
+                
+            }
+        }
+
+        
+    }
+
+    public class ShapeList : IDList<Shape>
+    {
+        public ShapeList()
+            : base()
+        {
+        }
+    }
+
     public class Drawing
     {
-        private Dictionary<string, Shape> shapes;
+        public ShapeList Shapes;
         private Dictionary<string, Connector> connectors;
 
         public Shape AddShape(string id, string label, string stencil_name, string master_name)
@@ -18,19 +84,8 @@ namespace VisioAutomation.Layout.DirectedGraph
             s0.StencilName = stencil_name;
             s0.MasterName = master_name;
 
-            this.shapes.Add(id, s0);
+            this.Shapes.Add(id, s0);
             return s0;
-        }
-
-        public IEnumerable<Shape> Shapes
-        {
-            get
-            {
-                foreach (var kv in this.shapes)
-                {
-                    yield return kv.Value;
-                }
-            }
         }
 
         public Connector Connect(string id, Shape from, Shape to)
@@ -66,7 +121,7 @@ namespace VisioAutomation.Layout.DirectedGraph
 
         public Drawing()
         {
-            this.shapes = new Dictionary<string, Shape>();
+            this.Shapes = new ShapeList();
             this.connectors = new Dictionary<string, Connector>();
         }
 
@@ -80,9 +135,9 @@ namespace VisioAutomation.Layout.DirectedGraph
 
         private bool try_get_shape(string id, ref Shape shape)
         {
-            if (this.shapes.ContainsKey(id))
+            if (this.Shapes.ContainsKey(id))
             {
-                shape = this.shapes[id];
+                shape = this.Shapes[id];
                 return true;
             }
             else
@@ -155,10 +210,7 @@ namespace VisioAutomation.Layout.DirectedGraph
         {
             get
             {
-                foreach (var kv in this.shapes)
-                {
-                    yield return kv.Key;
-                }
+                return this.Shapes.IDs;
             }
         }
     }
