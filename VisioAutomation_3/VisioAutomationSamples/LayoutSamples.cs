b705ac6aa8f9feec5f70baaa8fdb18d3ac9ccb16
snd\saveenr_cp <none@none>
2011-10-20 15:14:41 -0700

    updated sample

--- VisioAutomation_3/VisioAutomationSamples/LayoutSamples.cs
+++ VisioAutomation_3/VisioAutomationSamples/LayoutSamples.cs
@@ -88,7 +88,7 @@ namespace VisioAutomationSamples
             var visapp = new IVisio.Application();
             var doc = visapp.Documents.Add("");
 
-            var fontnames = new[] {"Consolas", "Ubuntu Mono"};
+            var fontnames = new[] {"Arial", "Roboto"};
 
             var sampletext = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz" +
                              "<>[](),./|\\:;\'\"1234567890!@#$%^&*()`~";
