899ff3814115719968501661ce7080cd3969ad5c
snd\saveenr_cp <none@none>
2011-12-17 14:03:27 -0800

    Factored out common test code
    --HG--
    rename : VisioAutomation_2007/TestVisioAutomationVDX/TestHelper.cs => VisioAutomation_2007/TestCommon/TestHelper.cs
    rename : VisioAutomation_2007/TestVisioAutomationVDX/VisioTestCommon.cs => VisioAutomation_2007/TestCommon/VisioTestCommon.cs

--- VisioAutomation_2007/TestCommon/TestHelper.cs
+++ VisioAutomation_2007/TestCommon/TestHelper.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Generic;
-using IVisio = Microsoft.Office.Interop.Visio;
-using VA = VisioAutomation;
 using System.Linq;
 
-namespace TestVisioAutomationVDX
+namespace TestCommon
 {
     public class TestHelper
     {
@@ -69,34 +67,6 @@ namespace TestVisioAutomationVDX
             return GetMethodName(2) + ext;
         }
 
-        public static void AreEqual(double x, double y, VA.Drawing.Point p, double delta)
-        {
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(x, p.X, delta);
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(y, p.Y, delta);
-        }
-
-        public static void AreEqual(double x, double y, VA.Drawing.Size p, double delta)
-        {
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(x, p.Width, delta);
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(y, p.Height, delta);
-        }
-
-        public static VA.ShapeSheet.Query.CellQuery BuildCellQuery(IList<VA.ShapeSheet.SRC> srcs)
-        {
-            var query = new VA.ShapeSheet.Query.CellQuery();
-            foreach (var src in srcs)
-            {
-                query.AddColumn(src);
-            }
-            return query;
-        }
-
-        public static void setformulas(VA.DOM.ShapeCells shapecells, IVisio.Page page, IVisio.Shape shape)
-        {
-            var update = new VA.ShapeSheet.Update.SIDSRCUpdate();
-            shapecells.Apply(update, shape.ID16);
-            update.Execute(page);
-        }
 
         public static Dictionary<string, T> EnumToDictionary<T>(System.Type t)
         {
--- VisioAutomation_2007/TestCommon/VisioTestCommon.cs
+++ VisioAutomation_2007/TestCommon/VisioTestCommon.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections.Generic;
-using IVisio = Microsoft.Office.Interop.Visio;
-using VA = VisioAutomation;
 using System.Linq;
 
-namespace TestVisioAutomationVDX
+namespace TestCommon
 {
     public static class VisioTestCommon
     {
         public static TestHelper Helper = new TestHelper("VisioAutomation Test Output");
     }
 }
+
