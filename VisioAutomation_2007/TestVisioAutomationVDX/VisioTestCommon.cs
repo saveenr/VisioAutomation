3eaacb6a55cb4f1ab81431f4ae1784f72f3309ea
snd\saveenr_cp <none@none>
2011-12-17 13:45:12 -0800

    Created a separate Test Project for Visio VDX

---
+++ VisioAutomation_2007/TestVisioAutomationVDX/VisioTestCommon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using IVisio = Microsoft.Office.Interop.Visio;
+using VA = VisioAutomation;
+using System.Linq;
+
+namespace TestVisioAutomationVDX
+{
+    public static class VisioTestCommon
+    {
+        public static TestHelper Helper = new TestHelper("VisioAutomation Test Output");
+    }
+}
