8f5a70896b4843d3aeb4756b292383ee6e64f959
saveenr@live.com <saveenr@live.com>
2011-12-20 12:05:45 -0800

    Improved Unit tests for textformatting

--- VisioAutomation_2010/TestVisioAutomation/Text/TextHelper_Tests.cs
+++ VisioAutomation_2010/TestVisioAutomation/Text/TextHelper_Tests.cs
@@ -103,44 +103,5 @@ namespace TestVisioAutomation
             page1.Delete(0);
         }
 
-        [TestMethod]
-        public void SetTabStops()
-        {
-            var no_tab_stops = new VA.Text.TabStop[] { };
-            var tabstops = new[]
-                               {
-                                   new VA.Text.TabStop(0.5, VA.Text.TabStopAlignment.Left),
-                                   new VA.Text.TabStop(1.5, VA.Text.TabStopAlignment.Right),
-                                   new VA.Text.TabStop(2.5, VA.Text.TabStopAlignment.Center),
-                                   new VA.Text.TabStop(4.0, VA.Text.TabStopAlignment.Decimal)
-                               };
-
-            var page1 = GetNewPage();
-
-            var s1 = page1.DrawRectangle(0, 0, 4, 4);
-
-            // shapes shoould not have any tab stops by default
-            var m0 = VA.Text.TextFormat.GetFormat(s1);
-            Assert.AreEqual(0, m0.TabStops.Count);
-
-            // clearing tab stops shoudl work even if there are no tab stops
-            VA.Text.TextFormat.SetTabStops(s1, no_tab_stops);
-            var m1 = VA.Text.TextFormat.GetFormat(s1);
-            Assert.AreEqual(0, m1.TabStops.Count);
-
-            // set the 3 tab stops
-            VA.Text.TextFormat.SetTabStops(s1, tabstops);
-
-            // should have exactly the same number we set
-            var m2 = VA.Text.TextFormat.GetFormat(s1);
-            Assert.AreEqual(tabstops.Length, m2.TabStops.Count);
-
-            // clear the tab stops
-            VA.Text.TextFormat.SetTabStops(s1, no_tab_stops);
-            var m3 = VA.Text.TextFormat.GetFormat(s1);
-            Assert.AreEqual(0, m3.TabStops.Count);
-
-            page1.Delete(0);
-        }
     }
 }
