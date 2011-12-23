8681b71770ef969957ee4e3996518eea239592d7
snd\saveenr_cp <none@none>
2011-12-23 14:20:42 -0800

    Added formatting to the progressbar sample

--- VisioAutomation_2010/VisioAutomationSamples/SmartShapeSamples.cs
+++ VisioAutomation_2010/VisioAutomationSamples/SmartShapeSamples.cs
@@ -10,24 +10,39 @@ namespace VisioAutomationSamples
     {
         public static void ProgressBar()
         {
-            var page = SampleEnvironment.Application.ActiveDocument.Pages.Add();
-            var bk = page.DrawRectangle(0, 0, 5, 1);
-            var fg = page.DrawRectangle(0, 0, 1, 1);
+            var page_a = SampleEnvironment.Application.ActiveDocument.Pages.Add();
 
-            string bkname = bk.NameID;
 
-            string pinyf = string.Format("GUARD({0}!PinY)", bkname);
-            string pinxf = string.Format("GUARD({0}!PinX-{0}!LocPinX+LocPinX)", bkname);
-            string heightf = string.Format("GUARD({0}!Height)", bkname);
-            string widthf = string.Format("GUARD({0}!Width*(PAGENUMBER()/PAGECOUNT()))", bkname);
+            // Draw some shapes
+            var background = page_a.DrawRectangle(0, 0, 5, 1);
+            var progress = page_a.DrawRectangle(0, 0, 1, 1);
 
-            fg.CellsU["PinY"].Formula = pinyf;
-            fg.CellsU["PinX"].Formula = pinxf;
-            fg.CellsU["Height"].Formula = heightf;
-            fg.CellsU["Width"].Formula = widthf;
+            var background_fmt = new VA.Format.ShapeFormatCells();
+            background_fmt.FillForegnd= "rgb(240,240,240)";
+            background_fmt.LineColor = "rgb(100,100,100)";
 
-            page.Application.ActiveWindow.SelectAll();
-            var group = page.Application.ActiveWindow.Selection.Group();
+
+            var progress_fmt = new VA.Format.ShapeFormatCells();
+            progress_fmt.FillForegnd = "rgb(100,150,240)";
+            progress_fmt.LineColor = "rgb(100,100,100)";
+
+            // group the two shapes together
+            page_a.Application.ActiveWindow.SelectAll();
+            var group = page_a.Application.ActiveWindow.Selection.Group();
+
+            // Set the progress shape update itself based on its position
+            string bkname = background.NameID;
+            var xform = new VA.Layout.XFormCells();
+            xform.PinX = string.Format("GUARD({0}!PinX-{0}!LocPinX+LocPinX)", bkname);
+            xform.PinY = string.Format("GUARD({0}!PinY)", bkname);
+            xform.Width = string.Format("GUARD({0}!Width*(PAGENUMBER()/PAGECOUNT()))", bkname);
+            xform.Height = string.Format("GUARD({0}!Height)", bkname); 
+
+            var update = new VA.ShapeSheet.Update.SIDSRCUpdate();
+            xform.Apply(update,progress.ID16);
+            background_fmt.Apply(update,background.ID16);
+            progress_fmt.Apply(update, progress.ID16);
+            update.Execute(page_a);
 
             var markup1 = new VA.Text.Markup.TextElement();
             markup1.AppendField(VA.Text.Markup.Fields.PageName);
