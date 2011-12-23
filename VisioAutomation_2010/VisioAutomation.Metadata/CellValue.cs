d2810606575760d8b9467dfc7fee5a73a97612fb
snd\saveenr_cp <none@none>
2011-12-22 17:32:39 -0800

    VA2010 removed the unused ID attributes from the Metadata XML

--- VisioAutomation_2010/VisioAutomation.Metadata/CellValue.cs
+++ VisioAutomation_2010/VisioAutomation.Metadata/CellValue.cs
@@ -2,7 +2,6 @@ namespace VisioAutomation.Metadata
 {
     public class CellValue
     {
-        public string ID { get; set; }
         public string Enum { get; set; }
         public string Name { get; set; }
         public string Value { get; set; }
