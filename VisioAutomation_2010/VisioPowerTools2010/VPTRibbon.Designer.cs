1a147fe9f1826046d9f298d343266aad416cb84c
saveenr@live.com <saveenr@live.com>
2011-12-29 09:34:40 -0800

    Added a new Visio Power Tools 2010 Project

---
+++ VisioAutomation_2010/VisioPowerTools2010/VPTRibbon.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace VisioPowerTools2010
+{
+    partial class VPTRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public VPTRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.tab2 = this.Factory.CreateRibbonTab();
+            this.tab1.SuspendLayout();
+            this.tab2.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // tab2
+            // 
+            this.tab2.Label = "Power Tools";
+            this.tab2.Name = "tab2";
+            // 
+            // VPTRibbon
+            // 
+            this.Name = "VPTRibbon";
+            this.RibbonType = "Microsoft.Visio.Drawing";
+            this.Tabs.Add(this.tab1);
+            this.Tabs.Add(this.tab2);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.VPTRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.tab2.ResumeLayout(false);
+            this.tab2.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        private Microsoft.Office.Tools.Ribbon.RibbonTab tab2;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal VPTRibbon VPTRibbon
+        {
+            get { return this.GetRibbon<VPTRibbon>(); }
+        }
+    }
+}
---
+++ VisioAutomation_2010/VisioPowerTools2010/VPTRibbon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace VisioPowerTools2010
+{
+    public partial class VPTRibbon
+    {
+        private void VPTRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
