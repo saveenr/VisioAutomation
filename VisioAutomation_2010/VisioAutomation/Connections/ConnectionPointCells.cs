b8b5c44c0fb6017c4f722ad5d02249095f2fc735
snd\saveenr_cp <none@none>
2011-12-31 14:26:11 -0800

    cleanup

--- VisioAutomation_2010/VisioAutomation/Connections/ConnectionPointCells.cs
+++ VisioAutomation_2010/VisioAutomation/Connections/ConnectionPointCells.cs
@@ -14,12 +14,6 @@ namespace VisioAutomation.Connections
         public VA.ShapeSheet.CellData<int> DirY { get; set; }
         public VA.ShapeSheet.CellData<int> Type { get; set; }
 
-        [System.Obsolete]
-        public static IList<ConnectionPointCells> GetConnectionPoints(IVisio.Shape shape)
-        {
-            return ConnectionPointCells.GetCells(shape);
-        }
-
         protected override void ApplyFormulas(ApplyFormula func, short row)
         {
             func(VA.ShapeSheet.SRCConstants.Connections_X.ForRow(row), this.X.Formula);
@@ -29,7 +23,7 @@ namespace VisioAutomation.Connections
             func(VA.ShapeSheet.SRCConstants.Connections_Type.ForRow(row), this.Type.Formula);
         }
 
-        private static ConnectionPointCells get_cells_from_row2(ConnectionPointQuery query, VA.ShapeSheet.Data.QueryDataRow<double> row)
+        private static ConnectionPointCells get_cells_from_row(ConnectionPointQuery query, VA.ShapeSheet.Data.QueryDataRow<double> row)
         {
             var cells = new ConnectionPointCells();
             cells.X = row[query.X];
@@ -44,13 +38,13 @@ namespace VisioAutomation.Connections
         internal static IList<List<ConnectionPointCells>> GetCells(IVisio.Page page, IList<int> shapeids)
         {
             var query = new ConnectionPointQuery();
-            return VA.ShapeSheet.CellGroups.CellGroupRow._GetObjectsFromRowsGrouped(page, shapeids, query, get_cells_from_row2);
+            return VA.ShapeSheet.CellGroups.CellGroupRow._GetObjectsFromRowsGrouped(page, shapeids, query, get_cells_from_row);
         }
 
         internal static IList<ConnectionPointCells> GetCells(IVisio.Shape shape)
         {
             var query = new ConnectionPointQuery();
-            return VA.ShapeSheet.CellGroups.CellGroupRow._GetObjectsFromRows(shape, query, get_cells_from_row2);
+            return VA.ShapeSheet.CellGroups.CellGroupRow._GetObjectsFromRows(shape, query, get_cells_from_row);
         }
 
         class ConnectionPointQuery : VA.ShapeSheet.Query.SectionQuery
