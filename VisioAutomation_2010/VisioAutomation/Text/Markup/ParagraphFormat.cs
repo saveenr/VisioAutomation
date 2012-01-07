b4afa7da34e768fad7d44fb8f93c69db33c2f25a
saveenr@live.com <saveenr@live.com>
2012-01-06 17:20:28 -0800

    Added Paragraph indent control
    --HG--
    rename : VisioAutomation_2010/TestVisioAutomation/Controls/ControlHelper_Tests.cs => VisioAutomation_2010/TestVisioAutomation/Connections/Controls/ControlHelper_Tests.cs

--- VisioAutomation_2010/VisioAutomation/Text/Markup/ParagraphFormat.cs
+++ VisioAutomation_2010/VisioAutomation/Text/Markup/ParagraphFormat.cs
@@ -6,13 +6,15 @@ namespace VisioAutomation.Text.Markup
     public class ParagraphFormat
     {
         public AlignmentHorizontal? HAlign { get; set; }
-        public double? Indent { get; set; }
+        public double? IndentFirst { get; set; }
+        public double? IndentLeft { get; set; }
         public bool? Bullets { get; set; }
 
         public void UpdateFrom(ParagraphFormat other)
         {
             this.HAlign =  other.HAlign;
-            this.Indent =  other.Indent;
+            this.IndentFirst = other.IndentFirst;
+            this.IndentLeft = other.IndentLeft;
             this.Bullets = other.Bullets;
         }
 
