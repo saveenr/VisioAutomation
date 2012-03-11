f01f34ef3aca2b5177c75b1e12964f63df87c4ea
saveenr@live.com <saveenr@live.com>
2012-03-10 20:52:24 -0800

    Removed font chart from samples

--- VisioAutomation_2010/VisioAutomationSamples/TextSamples.cs
+++ VisioAutomation_2010/VisioAutomationSamples/TextSamples.cs
@@ -59,55 +59,5 @@ namespace VisioAutomationSamples
             s2.GetCell(VA.ShapeSheet.SRCConstants.Char_Size).Formula =
                 "11pt * 10/SQRT(LEN(SHAPETEXT(TheText)))";
         }
-
-        public static void FontChart()
-        {
-            var stencil = SampleEnvironment.Application.Documents.OpenStencil("basic_u.vss");
-            var master = stencil.Masters["Rectangle"];
-            var page = SampleEnvironment.Application.ActiveDocument.Pages.Add();
-
-            var fonts = new[] {"Segoe UI", "Calibri", "Arial"};
-            var sizes = new[] {"28.0pt", "18.0pt", "14.0pt", "12.0pt", "10.0pt"};
-            var fontids = fonts.Select(f => page.Document.Fonts[f].ID).ToList();
-
-            var layout = new VA.Layout.Grid.GridLayout(sizes.Length, fonts.Length, new VA.Drawing.Size(3.0, 0.5), master);
-            layout.Origin = new VA.Drawing.Point(0, VA.Pages.PageHelper.GetSize(page).Height);
-            layout.CellSpacing = new VA.Drawing.Size(0.5, 0.5);
-            layout.RowDirection = VA.Layout.Grid.RowDirection.TopToBottom;
-            
-            layout.PerformLayout();
-            
-            layout.Render(page);
-
-            page.ResizeToFitContents(1.0,1.0);
-            var nodes = layout.Nodes.ToList();
-
-            var items = from fi in Enumerable.Range(0, fonts.Count())
-                        from size in sizes
-                        select new {font = fonts[fi], size = size, fontid = fontids[fi]};
-
-            var update = new VA.ShapeSheet.Update.SIDSRCUpdate();
-
-            var charcells = new VA.Text.CharacterFormatCells();
-            var fmt = new VA.Format.ShapeFormatCells();
-            int i = 0;
-            foreach (var item in items)
-            {
-                var shape = nodes[i].Shape;
-                shape.Text = item.font + " " + item.size;
-                var shapeid = nodes[i].ShapeID;
-                charcells.Size = item.size;
-                charcells.Font = item.fontid;
-                charcells.Apply(update, shapeid, 0);
-
-                fmt.FillForegnd = "rgb(250,250,250)";
-                fmt.LinePattern = 0;
-                fmt.LineWeight = 0;
-                fmt.Apply(update,shapeid);
-                i++;
-            }
-
-            update.Execute(page);
-        }
     }
 }
