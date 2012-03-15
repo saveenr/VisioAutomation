b62801244ae69dcba512209b42a97defee8950a0
snd\saveenr_cp <none@none>
2012-03-15 14:45:07 -0700

    removed unused enum

--- VisioAutomation_2010/VisioAutomation/Internal/ShapeUtil.cs
+++ VisioAutomation_2010/VisioAutomation/Internal/ShapeUtil.cs
@@ -11,7 +11,7 @@ namespace VisioAutomation.Internal
         public static void SetSelectGroupFirst(IVisio.Shape shape)
         {
             var src_selectmode = VA.ShapeSheet.SRCConstants.SelectMode;
-            var mode = VA.Internal.GroupSelectMode.GroupOnly;
+            var mode = IVisio.VisCellVals.visGrpSelModeGroupOnly;
             var select_mode_cell = shape.CellsSRC[src_selectmode.Section, src_selectmode.Row, src_selectmode.Cell];
             select_mode_cell.FormulaU = ((int)mode).ToString(System.Globalization.CultureInfo.InvariantCulture);
         }
