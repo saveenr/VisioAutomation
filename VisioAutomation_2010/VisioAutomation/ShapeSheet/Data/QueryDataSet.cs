724e400eafa9a2f736f0a2fb571e368cadd98a47
saveenr <none@none>
2012-03-23 09:47:25 -0700

    preparing for refactoring

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/Data/QueryDataSet.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/Data/QueryDataSet.cs
@@ -64,67 +64,13 @@ namespace VisioAutomation.ShapeSheet.Data
             this.ColumnCount = columncount;
 
             this.Groups = new TableRowGroupList();
-            foreach (var g in this.GetGrouping(shapeids, groupcounts))
+            foreach (var g in VA.ShapeSheet.ShapeSheetHelper.GetGrouping(shapeids, groupcounts, this.Count))
             {
                 this.Groups.Add(g);
             }
 
             this.Formulas = formulas_array != null ? this.BuildTableFromArray(formulas_array) : null;
             this.Results = results_array != null ? this.BuildTableFromArray(results_array) : null;
-        }
-
-        private TableRowGroup[] GetGrouping(IList<int> shape_ids, IList<int> group_counts)
-        {
-            var table_total_rows = this.Count;
-
-            if (group_counts == null)
-            {
-                throw new System.ArgumentNullException("group_counts");
-            }
-
-            if (group_counts.Count != shape_ids.Count)
-            {
-                string msg = String.Format("Number of group counts {0} does not match number of shape ids {1}",
-                                           group_counts.Count, shape_ids.Count);
-                throw new AutomationException(msg);
-            }
-
-            // Group the rows
-            var groups = new TableRowGroup[group_counts.Count];
-            int cur_group_start = 0;
-
-            for (int i = 0; i < group_counts.Count; i++)
-            {
-                int cur_group_count = group_counts[i];
-                var cur_group_shape_id = shape_ids[i];
-
-                if (cur_group_count < 1)
-                {
-                    // the group has no rows, so create an empty RowGroup
-                    var new_group = new TableRowGroup(cur_group_shape_id, cur_group_count, -1, -1);
-                    groups[i] = new_group;
-                }
-                else
-                {
-                    // the group contains at least 1 row create a non-empty RowGroup
-                    int new_group_start = cur_group_start;
-                    int new_group_end = new_group_start + cur_group_count - 1;
-                    var new_group = new TableRowGroup(cur_group_shape_id, cur_group_count, new_group_start, new_group_end);
-                    groups[i] = new_group;
-
-                    // update the new starting position for the next group
-                    cur_group_start += cur_group_count;
-                }
-            }
-
-            // verify that the groups account for all the rows
-            var total_rows_in_groups = groups.Select(g => g.Count).Sum();
-            if (total_rows_in_groups != table_total_rows)
-            {
-                throw new AutomationException("Internal Error: rows in groups and total rows do not match");
-            }
-
-            return groups;
         }
 
         private Table<X> BuildTableFromArray<X>(X[] array)
