7a52e3745550c00f0244aba60dedc2294649af8b
snd\saveenr_cp <none@none>
2012-05-04 19:46:35 -0700

    Added PerfContext for high-speed rendering

--- visiopy/visiopy/visiopy/DOM.py
+++ visiopy/visiopy/visiopy/DOM.py
@@ -77,85 +77,87 @@ class DOM(object):
         return stencil
 
     def Render( self, page ) :
-        # Load all the stencils
-        # Goal: prevent trying to reload the same stencil multiple times
-        # Goal: minimize having to use COM to lookup stencil documents by name
-        docs = page.Application.Documents
-        stencilnames = set(s.Master.StencilName.lower() for s in self.Shapes)
-        stencil_cache = {}
-        stencildocflags = win32com.client.constants.visOpenRO | win32com.client.constants.visOpenDocked 
-        for stencilname in stencilnames:
-            stencildoc = docs.OpenEx(stencilname , stencildocflags )
-            stencil_cache[ stencilname ] = stencildoc 
 
-        # cache all the master references
-        # Goal: minimize having to use COM to lookup master objects by name
-        masters = set( s.Master for s in self.Shapes if s.Master != None)
-        for cxn in self.Connections :
-            masters.add( cxn.Master )
+        with PerfContext(page.Application) :
+            # Load all the stencils
+            # Goal: prevent trying to reload the same stencil multiple times
+            # Goal: minimize having to use COM to lookup stencil documents by name
+            docs = page.Application.Documents
+            stencilnames = set(s.Master.StencilName.lower() for s in self.Shapes)
+            stencil_cache = {}
+            stencildocflags = win32com.client.constants.visOpenRO | win32com.client.constants.visOpenDocked 
+            for stencilname in stencilnames:
+                stencildoc = docs.OpenEx(stencilname , stencildocflags )
+                stencil_cache[ stencilname ] = stencildoc 
 
-        master_cache = {}
-        for master in masters:
-            stencildoc = stencil_cache[ master.StencilName.lower() ]
-            mastername = master.MasterName.lower()
-            vmaster = master_cache.get( mastername, None )
-            if (vmaster == None) :
-                vmaster = stencildoc.Masters.ItemU(master.MasterName) 
-            master.VisioMaster = vmaster 
-            if (master.VisioMaster==None) :
-                raise Errors.VisioPyError()
+            # cache all the master references
+            # Goal: minimize having to use COM to lookup master objects by name
+            masters = set( s.Master for s in self.Shapes if s.Master != None)
+            for cxn in self.Connections :
+                masters.add( cxn.Master )
 
-        # Perform the basic drop of all the shapes
-        vmasters = []
-        xyarray = []
-        for shape in self.Shapes:
-            vmasters.append( shape.Master.VisioMaster )
-            xyarray.append( shape.DropPosition.X )
-            xyarray.append( shape.DropPosition.Y )
-        num_shapes,shape_ids = page.DropMany( vmasters, xyarray) 
+            master_cache = {}
+            for master in masters:
+                stencildoc = stencil_cache[ master.StencilName.lower() ]
+                mastername = master.MasterName.lower()
+                vmaster = master_cache.get( mastername, None )
+                if (vmaster == None) :
+                    vmaster = stencildoc.Masters.ItemU(master.MasterName) 
+                master.VisioMaster = vmaster 
+                if (master.VisioMaster==None) :
+                    raise Errors.VisioPyError()
+
+            # Perform the basic drop of all the shapes
+            vmasters = []
+            xyarray = []
+            for shape in self.Shapes:
+                vmasters.append( shape.Master.VisioMaster )
+                xyarray.append( shape.DropPosition.X )
+                xyarray.append( shape.DropPosition.Y )
+            num_shapes,shape_ids = page.DropMany( vmasters, xyarray) 
 
 
-        # Ensure that we have stored the corresponding shape object and shapeid for each dropped object
-        page_shapes = page.Shapes
-        for i,shape in enumerate( self.Shapes ) :
-            shape.VisioShapeID = shape_ids[i]
-            shape.VisioShape = page_shapes.ItemFromID( shape_ids[i] )
+            # Ensure that we have stored the corresponding shape object and shapeid for each dropped object
+            page_shapes = page.Shapes
+            for i,shape in enumerate( self.Shapes ) :
+                shape.VisioShapeID = shape_ids[i]
+                shape.VisioShape = page_shapes.ItemFromID( shape_ids[i] )
 
-        #set any shape properties
-        u = ShapeSheet.Update()
-        for shape in self.Shapes:
-            if (shape.DropSize!=None):
-                u.Add( shape.VisioShapeID, ShapeSheet.SRCConstants.Width , shape.DropSize.Width)
-                u.Add( shape.VisioShapeID, ShapeSheet.SRCConstants.Width , shape.DropSize.Height)
-            if (len(shape.Cells)>0) :
-                for src in shape.Cells :
-                    formula = shape.Cells[src]
-                    u.Add( shape.VisioShapeID, src, formula)
+            #set any shape properties
+            u = ShapeSheet.Update()
+            for shape in self.Shapes:
+                if (shape.DropSize!=None):
+                    u.Add( shape.VisioShapeID, ShapeSheet.SRCConstants.Width , shape.DropSize.Width)
+                    u.Add( shape.VisioShapeID, ShapeSheet.SRCConstants.Width , shape.DropSize.Height)
+                if (len(shape.Cells)>0) :
+                    for src in shape.Cells :
+                        formula = shape.Cells[src]
+                        u.Add( shape.VisioShapeID, src, formula)
                     
-        result = u.SetFormulas(page) 
+            result = u.SetFormulas(page) 
         
 
-        self.__connectshapes(page)
+            self.__connectshapes(page)
 
-        #set any connection properties
-        u = ShapeSheet.Update()
-        for cxn in self.Connections:
-            shape = cxn.VisioShape
-            shapeid = cxn.VisioShapeID
-            if (len(cxn.Cells)>0) :
-                for src in cxn.Cells :
-                    formula = cxn.Cells[src]
-                    u.Add( cxn.VisioShapeID, src, formula)
-        result = u.SetFormulas(page) 
+            #set any connection properties
+            u = ShapeSheet.Update()
+            for cxn in self.Connections:
+                shape = cxn.VisioShape
+                shapeid = cxn.VisioShapeID
+                if (len(cxn.Cells)>0) :
+                    for src in cxn.Cells :
+                        formula = cxn.Cells[src]
+                        u.Add( cxn.VisioShapeID, src, formula)
+            result = u.SetFormulas(page) 
 
-        # Set the text for shapes and connections
-        for shape in self.Shapes:
-            if (shape.Text != None and shape.Text!='') :
-                shape.VisioShape.Text = shape.Text
+            # Set the text for shapes and connections
+            for shape in self.Shapes:
+                if (shape.Text != None and shape.Text!='') :
+                    shape.VisioShape.Text = shape.Text
 
-        for cxn in self.Connections:
-            if (cxn.Text != None and cxn.Text!='') :
-                cxn.VisioShape.Text = cxn.Text
+            for cxn in self.Connections:
+                if (cxn.Text != None and cxn.Text!='') :
+                    cxn.VisioShape.Text = cxn.Text
 
 
     def __connectshapes( self , page ) :
@@ -189,3 +191,37 @@ class DOM(object):
 
             cxn.VisioShape = connectorshape
             cxn.VisioShapeID = cxn.VisioShape.ID
+
+
+class PerfContext:
+
+    screen_updating_fast = 0 #// disable screen updating
+    defer_recalc_fast = 1 # // defer recalc
+    enable_autoconnect_fast = False #// diable autoconnect
+    livedynamics_fast = False #// diable live dynamics
+
+    def __init__(self, app):
+        self.app= app
+        self.old_autoconnect=None
+        self.old_livedynamics=None
+        self.old_screenupdating=None
+        self.old_deferrecalc=None
+
+    def __enter__(self):
+        app_settings = self.app.Settings
+        self.old_livedynamics = self.app.LiveDynamics
+        self.old_autoconnect = app_settings.EnableAutoConnect
+        self.old_deferrecalc = self.app.DeferRecalc
+        self.old_screenupdating = self.app.ScreenUpdating
+
+        self.app.ScreenUpdating = PerfContext.screen_updating_fast
+        self.app.DeferRecalc = PerfContext.defer_recalc_fast
+        self.app.Settings.EnableAutoConnect = PerfContext.enable_autoconnect_fast
+        self.app.LiveDynamics = PerfContext.livedynamics_fast
+
+    def __exit__(self, type, value, tb):
+        self.app.ScreenUpdating = self.old_screenupdating;
+        self.app.DeferRecalc = self.old_deferrecalc;
+        self.app.Settings.EnableAutoConnect = self.old_autoconnect;
+        self.app.LiveDynamics = self.old_livedynamics;
+        del self.app
