7c0621a248719530d2e95c56258595afc0365e1c
snd\saveenr_cp <none@none>
2012-05-07 17:59:34 -0700

    VA - moved PerfSettings to VisioAutomation.Internal namespace

--- VisioAutomation_2010/VisioAutomation/PerfScope.cs
+++ VisioAutomation_2010/VisioAutomation/PerfScope.cs
@@ -1,53 +1,23 @@
 ﻿using IVisio = Microsoft.Office.Interop.Visio;
+using VA=VisioAutomation;
 
 namespace VisioAutomation
 {
-    internal class PerfSettings
-    {
-        public bool? EnableAutoConnect;
-        public bool? LiveDynamics;
-        public short? ScreenUpdating;
-        public short? DeferRecalc;        
-
-        public PerfSettings()
-        {
-            
-        }
-
-        public void Load(IVisio.Application app)
-        {
-            var app_settings = app.Settings;
-            this.LiveDynamics = app.LiveDynamics;
-            this.EnableAutoConnect = app_settings.EnableAutoConnect;
-            this.DeferRecalc = app.DeferRecalc;
-            this.ScreenUpdating = app.ScreenUpdating;
-        }
-
-        public void Apply(IVisio.Application app)
-        {
-            if (this.ScreenUpdating.HasValue) {app.ScreenUpdating = this.ScreenUpdating.Value;}
-            if (this.DeferRecalc.HasValue) {app.DeferRecalc = this.DeferRecalc.Value;}
-            if (this.EnableAutoConnect.HasValue) {app.Settings.EnableAutoConnect = this.EnableAutoConnect.Value;}
-            if (this.LiveDynamics.HasValue) {app.LiveDynamics = this.LiveDynamics.Value;}
-        }
-
-    }
-
     public class PerfScope : System.IDisposable
     {
         private IVisio.Application app;
-        private PerfSettings old_settings;
+        private VA.Internal.PerfSettings old_settings;
 
         public PerfScope(IVisio.Application vis)
         {
             this.app = vis;
 
             // save the old settings
-            this.old_settings = new PerfSettings();
+            this.old_settings = new VA.Internal.PerfSettings();
             this.old_settings.Load(this.app);
 
             // Set the new settings
-            var fast = new PerfSettings();
+            var fast = new VA.Internal.PerfSettings();
             fast.DeferRecalc = 1;
             fast.ScreenUpdating = 0;
             fast.EnableAutoConnect = false;
