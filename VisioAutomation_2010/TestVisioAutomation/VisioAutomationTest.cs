1b2f0304a76b489ad0e4f4eb521988b4dabc8b2d
snd\saveenr_cp <none@none>
2012-05-19 16:57:47 -0700

    Cleanup

--- VisioAutomation_2010/TestVisioAutomation/VisioAutomationTest.cs
+++ VisioAutomation_2010/TestVisioAutomation/VisioAutomationTest.cs
@@ -6,35 +6,6 @@ using VisioAutomation.Extensions;
 
 namespace TestVisioAutomation
 {
-    public class VisioApplicationSafeReference
-    {
-        // this class ensures that a valid application instance is always available
-
-        private IVisio.Application app;
-
-        public IVisio.Application GetVisioApplication()
-        {
-            if (this.app == null)
-            {
-                this.app = new IVisio.Application();
-            }
-            else
-            {
-                // we have an instance, but it may not be valid
-                try
-                {
-                    string s = app.Name;
-                }
-                catch (System.Runtime.InteropServices.COMException e)
-                {
-                    this.app = new IVisio.Application();
-                }
-            }
-
-            return this.app;
-        }
-    }
-
     [TestClass]
     public class VisioAutomationTest
     {
