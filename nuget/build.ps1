5ab00746839fd853c3115c08bd509fe41f746943
saveenr@SAVEENR7.redmond.corp.microsoft.com <saveenr@SAVEENR7.redmond.corp.microsoft.com>
2012-07-01 10:08:21 -0700

    Added nuget packaging for VisioAutomation 2007

--- nuget/build.ps1
+++ nuget/build.ps1
@@ -3,7 +3,7 @@ $localfeed ="D:\saveenr\live-mesh\code\mynugetfeed"
 $publicfeedurl="https://www.nuget.org"
 
 $publocal = $true
-$pubremote = $false
+$pubremote = $true
 
 $url="https://www.nuget.org"
 
@@ -12,32 +12,42 @@ add-type @"
 public class PackageRecord
 {
    public string Name;
-   public string Version;
 }
 "@
 
 $pkg_va2010 = new-object PackageRecord
 $pkg_va2010.Name = "VisioAutomation2010"
-$pkg_va2010.Version = "1.0.0.0"
+
+$pkg_va2007 = new-object PackageRecord
+$pkg_va2007.Name = "VisioAutomation2007"
 
 $pkg_vavdx = new-object PackageRecord
 $pkg_vavdx.Name = "VisioAutomation.VDX"
-$pkg_vavdx.Version = "1.0.0.0"
 
-$packagerecords = @($pkg_va2010, $pkg_vavdx)
+$packagerecords = @($pkg_va2010, $pkg_va2007, $pkg_vavdx)
 
 foreach ($pk in $packagerecords)
 {
-    
-    $packagefilename = $pk.Name + "." + $pk.version + ".nupkg"
     $nuspec = $pk.Name + ".nuspec"
+    $nuspecdata = [xml](get-content $nuspec)
+    $version = $nuspecdata.package.metadata.version
 
     Write-Host ">>>>>>>>>", $pk.Name
     Write-Host ">>>>>>>>>", $nuspec 
+    Write-Host ">>>>>>>>>", $version
+
+    $packagefilename = $pk.Name + "." + $version + ".nupkg"
+
 
     # Create the NuGet Package
-    ./nuget.exe pack $nuspec -Verbose -Version $version
-    
+    ./nuget.exe pack $nuspec -Verbose 
+
+    if ( !($lastexitcode -eq 0) )
+    {
+        Write-Host "Error running nuget"
+        exit 1
+    } 
+        
     # Local publishing
     if ($publocal)
     {
