17ce688e84aa873db6920a9e36b82d757bc28216
saveenr <none@none>
2012-07-15 12:34:50 -0700

    Cleanup

---
+++ VisioAutomation_2010/VisioAutomation/DOM/Document.cs
@@ -0,0 +1,25 @@
+﻿using IVisio = Microsoft.Office.Interop.Visio;
+using VA = VisioAutomation;
+
+namespace VisioAutomation.DOM
+{
+    public class Document
+    {
+        public PageList Pages;
+
+        public Document()
+        {
+            this.Pages = new PageList();
+        }
+
+        public IVisio.Document Render(IVisio.Application app)
+        {
+            var appdocs = app.Documents;
+            var vdoc = appdocs.Add("");
+            var docpages = vdoc.Pages;
+            var starpage = docpages[1];
+            this.Pages.Render(starpage);
+            return vdoc;
+        }
+    }
+}
---
+++ VisioAutomation_2010/VisioAutomation/DOM/PageList.cs
@@ -0,0 +1,73 @@
+﻿using IVisio = Microsoft.Office.Interop.Visio;
+using VA = VisioAutomation;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace VisioAutomation.DOM
+{
+    public class PageList : Node, IEnumerable<Page>
+    {
+        private NodeList<Page> pages;
+
+        public PageList()
+        {
+            this.pages = new NodeList<Page>(this);
+        }
+
+        public IEnumerator<Page> GetEnumerator()
+        {
+            foreach (var i in this.pages)
+            {
+                yield return i;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()     // Explicit implementation
+        {                                           // keeps it hidden.
+            return GetEnumerator();
+        }
+
+        public void Add(Page page)
+        {
+            this.pages.Add(page);
+        }
+
+        public int Count
+        {
+            get { return this.pages.Count; }
+        }
+
+        public IList<IVisio.Page> Render(IVisio.Document doc)
+        {
+            var vpages = new List<IVisio.Page>(this.Count);
+            foreach (var dompage in this.pages)
+            {
+                var vpage = dompage.Render(doc);
+                vpages.Add(vpage);
+            }
+            return vpages;
+        }
+
+        public IList<IVisio.Page> Render(IVisio.Page startpage)
+        {
+            var doc = startpage.Document;
+            int count = 0;
+            var vpages = new List<IVisio.Page>(this.Count);
+            foreach (var dompage in this.pages)
+            {
+                if (count == 0)
+                {
+                    dompage.Render(startpage);
+                    vpages.Add(startpage);
+                }
+                else
+                {
+                    var vpage = dompage.Render(doc);
+                    vpages.Add(vpage);
+                }
+            }
+            return vpages;
+        }
+
+    }
+}
