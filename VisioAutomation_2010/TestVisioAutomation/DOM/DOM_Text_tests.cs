bc965a51fb63f8c9913839197182d9eead76daa7
saveenr@asgard7 <saveenr@asgard7>
2012-07-26 07:33:28 -0700

    Enhanced unit tests

--- VisioAutomation_2010/TestVisioAutomation/DOM/DOM_Text_tests.cs
+++ VisioAutomation_2010/TestVisioAutomation/DOM/DOM_Text_tests.cs
@@ -11,7 +11,7 @@ namespace TestVisioAutomation
     public class DOM_Text_Tests : VisioAutomationTest
     {
         [TestMethod]
-        public void Markup_Simple_Plain()
+        public void MarkupCharacterPlain()
         {
             var m = new VA.Text.Markup.TextElement("Normal Text");
             var page1 = this.GetNewPage(new VA.Drawing.Size(5, 5));
@@ -26,7 +26,7 @@ namespace TestVisioAutomation
         }
 
         [TestMethod]
-        public void Markup_Simple_Bold()
+        public void MarkupCharacterBold()
         {
             var m = new VA.Text.Markup.TextElement("Bold Text");
             m.CharacterFormat.Style = VA.Text.CharStyle.Bold;
@@ -43,7 +43,7 @@ namespace TestVisioAutomation
         }
 
         [TestMethod]
-        public void Markup_Simple_Italic()
+        public void MarkupCharacterItalic()
         {
             var m = new VA.Text.Markup.TextElement("Italic Text");
             m.CharacterFormat.Style = VA.Text.CharStyle.Italic;
@@ -60,7 +60,7 @@ namespace TestVisioAutomation
         }
 
         [TestMethod]
-        public void Markup_Simple_Font()
+        public void MarkupCharacterFont()
         {
             var page1 = this.GetNewPage(new VA.Drawing.Size(5, 5));
 
@@ -80,55 +80,57 @@ namespace TestVisioAutomation
         }
 
         [TestMethod]
-        public void Render_Markup_Simple_Font_Multiple()
+        public void MarkupCharacterComplex()
         {
             var page1 = this.GetNewPage(new VA.Drawing.Size(5, 5));
-            var impact = page1.Document.Fonts["Impact"];
-            var m = new VA.Text.Markup.TextElement("Normal Text in Impact Font with Red Color");
-            m.CharacterFormat.FontID = impact.ID;
-            m.CharacterFormat.Color = new VA.Drawing.ColorRGB(0xff0000);
-            var s0 = page1.DrawRectangle(0, 0, 4, 4);
-            m.SetText(s0);
+            var doc = page1.Document;
+            var fonts = doc.Fonts;
 
-            var textfmt = VA.Text.TextFormat.GetFormat(s0);
-            var charfmt = textfmt.CharacterFormats;
-            Assert.AreEqual(1, charfmt.Count);
-            Assert.AreEqual(0, charfmt[0].Style.Result);
-            Assert.AreEqual(impact.ID, charfmt[0].Font.Result);
-
-            page1.Delete(0);
-        }
-
-        [TestMethod]
-        public void Markup_Overlap_Multiple()
-        {
-            var page1 = this.GetNewPage(new VA.Drawing.Size(5, 5));
-            var segoeui = page1.Document.Fonts["Segoe UI"];
+            var segoeui = fonts["Segoe UI"];
+            var impact = fonts["Impact"];
+            var couriernew = fonts["Courier New"];
+            var georgia = fonts["Georgia"];
 
             var t1 = new VA.Text.Markup.TextElement("{Normal}");
             t1.CharacterFormat.FontID = segoeui.ID;
             
             var t2 = t1.AppendElement("{Italic}");
             t2.CharacterFormat.Style = VA.Text.CharStyle.Italic;
+            t2.CharacterFormat.FontID = impact.ID;
 
             var t3 = t2.AppendElement("{Bold}");
             t3.CharacterFormat.Style = VA.Text.CharStyle.Bold;
+            t3.CharacterFormat.FontID = couriernew.ID;
 
             var t4 = t2.AppendElement("{Bold Italic}");
             t4.CharacterFormat.Style = VA.Text.CharStyle.Bold | VA.Text.CharStyle.Italic;
+            t4.CharacterFormat.FontID = georgia.ID;
 
             var s0 = page1.DrawRectangle(0, 0, 4, 4);
             t1.SetText(s0);
 
             var textfmt = VA.Text.TextFormat.GetFormat(s0);
             var charfmt = textfmt.CharacterFormats;
+            
+            // check the number of character regions
             Assert.AreEqual(5, charfmt.Count);
+
+            // check the fonts
+            Assert.AreEqual(segoeui.ID, charfmt[0].Font.Result);
+            Assert.AreEqual(impact.ID, charfmt[1].Font.Result);
+            Assert.AreEqual(couriernew.ID, charfmt[2].Font.Result);
+            Assert.AreEqual(georgia.ID, charfmt[3].Font.Result);
+            Assert.AreEqual(segoeui.ID, charfmt[4].Font.Result);
+
+
+            // check the styles
             Assert.AreEqual((int)VA.Text.CharStyle.None, charfmt[0].Style.Result);
             Assert.AreEqual((int)VA.Text.CharStyle.Italic, charfmt[1].Style.Result);
             Assert.AreEqual((int)VA.Text.CharStyle.Bold, charfmt[2].Style.Result);
             Assert.AreEqual((int) (VA.Text.CharStyle.Italic | VA.Text.CharStyle.Bold), charfmt[3].Style.Result);
             Assert.AreEqual((int)(VA.Text.CharStyle.None), charfmt[4].Style.Result);
 
+            // check the text run content
             var charruns= textfmt.CharacterTextRuns;
             Assert.AreEqual(4, charruns.Count);
             Assert.AreEqual("{Normal}", charruns[0].Text);
