77c0d2533377256cc1981c54cbe1287ec0e0f896
saveenr@SAVEENR7.redmond.corp.microsoft.com <saveenr@SAVEENR7.redmond.corp.microsoft.com>
2012-08-08 11:36:00 -0700

    Added support for installing VisioPS as a PowerShell module for the current user

---
+++ VisioAutomation_2010/VisioPS/install.ps1
@@ -0,0 +1,31 @@
+﻿
+
+
+
+$modulename = "VisioPS"
+
+Write-Host Installing $modulename module
+
+
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+
+$src_path = $dir
+write-host "FROM" $dir
+
+$mydocs = [Environment]::GetFolderPath("MyDocuments")
+$psmodfolder = join-path $mydocs ( "WindowsPowerShell\Modules\" + $modulename )
+
+write-host "TO" $psmodfolder
+
+if (!(test-path $psmodfolder) )
+{
+    New-Item $psmodfolder -type directory
+}
+
+robocopy $src_path $psmodfolder /mir
+
+
+
+
+
