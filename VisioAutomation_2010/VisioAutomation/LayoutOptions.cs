09865bfa5a29317230202e702e49775d2fa37a7b
saveenr@SAVEENR7.redmond.corp.microsoft.com <saveenr@SAVEENR7.redmond.corp.microsoft.com>
2012-09-08 12:04:38 -0700

    Updated VA.2010 nuspec Merged MSAGL into VisioAutomation via ilmerge Merged XAMLtune into VisioAutomation.Scripting instead of going through separate project

---
+++ VisioAutomation_2010/VisioAutomation/LayoutOptions.cs
@@ -0,0 +1,17 @@
+﻿using VA=VisioAutomation;
+
+namespace VisioAutomation.Layout.MSAGL
+{
+    public class LayoutOptions : VA.Layout.Models.DirectedGraph.LayoutOptions
+    {
+        public double ScalingFactor { get; set; }
+        public bool UseDynamicConnectors { get; set; }
+
+        public LayoutOptions() :
+            base()
+        {
+            UseDynamicConnectors = true;
+            ScalingFactor = 14;
+        }
+    }
+}
