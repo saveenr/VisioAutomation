57dcf52e54f596450b3da4974f28ff25244e00dc
saveenr@SAVEENR7.redmond.corp.microsoft.com <saveenr@SAVEENR7.redmond.corp.microsoft.com>
2012-09-09 16:59:48 -0700

    Removed the VisioAutomation.MetaData code. Nobody was using it. And it was not worth the effort to keep up-to-date for each release of Visio.

--- VisioAutomation_2010/VisioAutomation.Metadata/Section.cs
+++
@@ -1,9 +0,0 @@
-namespace VisioAutomation.Metadata
-{
-    public class Section
-    {
-        public string Name { get; set; }
-        public string DisplayName { get; set; }
-        public string Enum { get; set; }
-    }
-}
