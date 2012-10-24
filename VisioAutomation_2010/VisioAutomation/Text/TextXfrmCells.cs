a8af869a34f6b74a7d1ed4617c505a49594fe399
saveenr@SAVEENR7.redmond.corp.microsoft.com <saveenr@SAVEENR7.redmond.corp.microsoft.com>
2012-10-23 21:36:55 -0700

    Added TextXFormCells

---
+++ VisioAutomation_2010/VisioAutomation/Text/TextXfrmCells.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using IVisio = Microsoft.Office.Interop.Visio;
+using VA = VisioAutomation;
+using VisioAutomation.Extensions;
+
+namespace VisioAutomation.Text
+{
+        public class TextXFormCells : VA.ShapeSheet.CellGroups.CellGroup
+    {
+        public VA.ShapeSheet.CellData<double> TxtPinX { get; set; }
+        public VA.ShapeSheet.CellData<double> TxtPinY { get; set; }
+        public VA.ShapeSheet.CellData<double> TxtLocPinX { get; set; }
+        public VA.ShapeSheet.CellData<double> TxtLocPinY { get; set; }
+        public VA.ShapeSheet.CellData<double> TxtWidth { get; set; }
+        public VA.ShapeSheet.CellData<double> TxtHeight { get; set; }
+        public VA.ShapeSheet.CellData<double> TxtAngle { get; set; }
+
+        protected override void ApplyFormulas(ApplyFormula func)
+        {
+            func(ShapeSheet.SRCConstants.TxtPinX, this.TxtPinX.Formula);
+            func(ShapeSheet.SRCConstants.TxtPinY, this.TxtPinY.Formula);
+            func(ShapeSheet.SRCConstants.TxtLocPinX, this.TxtLocPinX.Formula);
+            func(ShapeSheet.SRCConstants.TxtLocPinY, this.TxtLocPinY.Formula);
+            func(ShapeSheet.SRCConstants.TxtWidth, this.TxtWidth.Formula);
+            func(ShapeSheet.SRCConstants.TxtHeight, this.TxtHeight.Formula);
+            func(ShapeSheet.SRCConstants.TxtAngle, this.TxtAngle.Formula);
+        }
+
+        private static TextXFormCells get_cells_from_row(TextXFormQuery query,
+                                                         VA.ShapeSheet.Data.TableRow<VA.ShapeSheet.CellData<double>> row)
+        {
+            var cells = new TextXFormCells();
+            cells.TxtPinX = row[query.TxtPinX];
+            cells.TxtPinY = row[query.TxtPinY];
+            cells.TxtLocPinX = row[query.TxtLocPinX];
+            cells.TxtLocPinY = row[query.TxtLocPinY];
+            cells.TxtWidth = row[query.TxtWidth];
+            cells.TxtHeight = row[query.TxtHeight];
+            cells.TxtAngle = row[query.TxtAngle];
+            return cells;
+        }
+
+        internal static IList<TextXFormCells> GetCells(IVisio.Page page, IList<int> shapeids)
+        {
+            var query = new TextXFormQuery();
+            return VA.ShapeSheet.CellGroups.CellGroup.CellsFromRows(page, shapeids, query, get_cells_from_row);
+        }
+
+        internal static TextXFormCells GetCells(IVisio.Shape shape)
+        {
+            var query = new TextXFormQuery();
+            return VA.ShapeSheet.CellGroups.CellGroup.CellsFromRow(shape, query, get_cells_from_row);
+        }
+
+        private class TextXFormQuery : VA.ShapeSheet.Query.CellQuery
+        {
+            public VA.ShapeSheet.Query.QueryColumn TxtWidth { get; set; }
+            public VA.ShapeSheet.Query.QueryColumn TxtHeight { get; set; }
+            public VA.ShapeSheet.Query.QueryColumn TxtPinX { get; set; }
+            public VA.ShapeSheet.Query.QueryColumn TxtPinY { get; set; }
+            public VA.ShapeSheet.Query.QueryColumn TxtLocPinX { get; set; }
+            public VA.ShapeSheet.Query.QueryColumn TxtLocPinY { get; set; }
+            public VA.ShapeSheet.Query.QueryColumn TxtAngle { get; set; }
+
+            public TextXFormQuery() :
+                base()
+            {
+                TxtPinX = this.AddColumn(VA.ShapeSheet.SRCConstants.TxtPinX, "TxtPinX");
+                TxtPinY = this.AddColumn(VA.ShapeSheet.SRCConstants.TxtPinY, "TxtPinY");
+                TxtLocPinX = this.AddColumn(VA.ShapeSheet.SRCConstants.TxtLocPinX, "TxtLocPinX");
+                TxtLocPinY = this.AddColumn(VA.ShapeSheet.SRCConstants.TxtLocPinY, "TxtLocPinY");
+                TxtWidth = this.AddColumn(VA.ShapeSheet.SRCConstants.TxtWidth, "TxtWidth");
+                TxtHeight = this.AddColumn(VA.ShapeSheet.SRCConstants.TxtHeight, "TxtHeight");
+                TxtAngle = this.AddColumn(VA.ShapeSheet.SRCConstants.TxtAngle, "TxtAngle");
+            }
+        }
+    }
+}
--- VisioAutomation_2010/VisioAutomationSamples/TextSamples.cs
+++ VisioAutomation_2010/VisioAutomationSamples/TextSamples.cs
@@ -29,31 +29,5 @@ namespace VisioAutomationSamples
             markup1.AddText(") ");
             markup1.SetText(s0);
         }
-
-        public static void TextSizing()
-        {
-            var page = SampleEnvironment.Application.ActiveDocument.Pages.Add();
-
-            var s0 = page.DrawRectangle(0, 0, 4, 4);
-
-            // Alignment Box fits to accomodate text
-            s0.Text = "Alignment Box fits to accomodate text";
-
-            s0.GetCell(VA.ShapeSheet.SRCConstants.Width).Formula = "2.0";
-            s0.GetCell(VA.ShapeSheet.SRCConstants.Height).Formula = "GUARD(TxtHeight)";
-            s0.GetCell(VA.ShapeSheet.SRCConstants.TxtWidth).Formula = "Width*1";
-            s0.GetCell(VA.ShapeSheet.SRCConstants.TxtHeight).Formula = "TEXTHEIGHT(TheText,TxtWidth)";
-
-            // Text Scales Proportional to Shape Height
-            var s1 = page.DrawRectangle(0, 4, 8, 8);
-            s1.Text = "Text Scales Proportional to Shape Height";
-            s0.GetCell(VA.ShapeSheet.SRCConstants.Char_Size).Formula = "Height*0.25";
-
-            // Text scales smaller to fit more text
-            var s2 = page.DrawRectangle(4, 0, 8, 4);
-            s2.Text = "Text scales smaller to fit more text";
-            s2.GetCell(VA.ShapeSheet.SRCConstants.Char_Size).Formula =
-                "11pt * 10/SQRT(LEN(SHAPETEXT(TheText)))";
-        }
     }
 }
