5eab52d1a57a156a23f325dbcb2ae3e8f39d7c37
saveenr <none@none>
2012-12-31 09:18:54 -0800

    Forget this file in the last checkin

--- VisioAutomation_2010/VisioAutomation.Scripting/SessionContext.cs
+++ VisioAutomation_2010/VisioAutomation.Scripting/SessionContext.cs
@@ -2,6 +2,13 @@
 {
     public class SessionContext
     {
+        // this class is for storing additional data
+        // about a session and handling I/O
+        // for example, if you want to use a Scripting Session
+        // and you want to handle all I/O (to log it or to send it
+        // to special outputs) then derive from this class and
+        // set the Session.Context property
+
         public virtual void WriteDebug(string s)
         {
             string msg = string.Format("DEBUG: {0}", s);
