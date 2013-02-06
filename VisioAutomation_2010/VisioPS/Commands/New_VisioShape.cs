3117ee9b39e21337a32bfbeee4dc6756ba7faea7
saveenr <none@none>
2013-02-06 14:36:53 -0800

    added ability to select shapes with new-VisioShape

--- VisioAutomation_2010/VisioPS/Commands/New_VisioShape.cs
+++ VisioAutomation_2010/VisioPS/Commands/New_VisioShape.cs
@@ -14,14 +14,38 @@ namespace VisioPS.Commands
         [SMA.Parameter(Position = 1, Mandatory = true)]
         public double [] Points { get; set; }
 
+        [SMA.Parameter(Mandatory = false)]
+        public SMA.SwitchParameter GetShapeIDs=false;
+
+        [SMA.Parameter(Mandatory = false)]
+        public SMA.SwitchParameter NoSelect=false;
+
         protected override void ProcessRecord()
         {
             var scriptingsession = this.ScriptingSession;
             var points = VA.Drawing.Point.FromDoubles(Points).ToList();
             var shape_ids = scriptingsession.Master.Drop(Masters, points);
-            var page = scriptingsession.Page.Get();
-            var shape_objects = VA.ShapeHelper.GetShapesFromIDs(page.Shapes, shape_ids);
-            this.WriteObject(shape_objects);
+
+            if (NoSelect)
+            {
+                
+            }
+            else
+            {
+                
+            }
+
+
+            if (this.GetShapeIDs)
+            {
+                this.WriteObject(shape_ids);
+            }
+            else
+            {
+                var page = scriptingsession.Page.Get();
+                var shape_objects = VA.ShapeHelper.GetShapesFromIDs(page.Shapes, shape_ids);
+                this.WriteObject(shape_objects);
+            }
         }
     }
 }
