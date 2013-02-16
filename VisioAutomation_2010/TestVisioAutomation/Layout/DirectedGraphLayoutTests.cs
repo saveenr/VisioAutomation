1f92ca49c8ef079d47facf2368693930ed257522
saveenr <none@none>
2013-02-16 15:43:30 -0800

    unit test cleanup

--- VisioAutomation_2010/TestVisioAutomation/Layout/DirectedGraphLayoutTests.cs
+++ VisioAutomation_2010/TestVisioAutomation/Layout/DirectedGraphLayoutTests.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VisioAutomation.Extensions;
 using IVisio = Microsoft.Office.Interop.Visio;
 using VA = VisioAutomation;
-using System.Linq;
 using DG = VisioAutomation.Layout.Models.DirectedGraph;
 
 namespace TestVisioAutomation
@@ -14,58 +13,22 @@ namespace TestVisioAutomation
         [TestMethod]
         public void RenderDirectedGraphWithBezierConnectors()
         {
-            var directed_graph_drawing = new DG.Drawing();
-            DG.Shape[] shapes;
-            DG.Connector[] connectors;
-            this.filldrawing(directed_graph_drawing,out shapes,out connectors);
+            var directed_graph_drawing = this.create_sample_graph();
             
             var options = new DG.MSAGLLayoutOptions();
             options.UseDynamicConnectors = false;
+            
             var visapp = this.GetVisioApplication();
             var doc = this.GetNewDoc();
             var page = visapp.ActivePage;
-
             directed_graph_drawing.Render(page,options);
-
-
             doc.Close(true);
-        }
-
-        private void filldrawing(DG.Drawing directed_graph_drawing, out DG.Shape[] nodes, out DG.Connector[] connectors)
-        {
-            var basic_stencil = "basic_u.vss";
-            var n0 = directed_graph_drawing.AddShape("n0", "Node 0", basic_stencil, "Rectangle");
-            n0.Size = new VA.Drawing.Size(3, 2);
-            var n1 = directed_graph_drawing.AddShape("n1", "Node 1", basic_stencil, "Rectangle");
-            var n2 = directed_graph_drawing.AddShape("n2", "Node 2", basic_stencil, "Rectangle");
-            var n3 = directed_graph_drawing.AddShape("n3", "Node 3", basic_stencil, "Rectangle");
-            var n4 = directed_graph_drawing.AddShape("n4", "Node 4\nUnconnected", basic_stencil, "Rectangle");
-
-            nodes = new DG.Shape[]
-            {
-                n0,n1,n2,n3,n4
-            };
-
-            var c0 = directed_graph_drawing.Connect("c0", n0, n1, "0 -> 1", VA.Connections.ConnectorType.Curved);
-            var c1 = directed_graph_drawing.Connect("c1", n1, n2, "1 -> 2", VA.Connections.ConnectorType.RightAngle);
-            var c2 = directed_graph_drawing.Connect("c2", n1, n0, "0 -> 1", VA.Connections.ConnectorType.Curved);
-            var c3 = directed_graph_drawing.Connect("c3", n0, n2, "0 -> 2", VA.Connections.ConnectorType.Straight);
-            var c4 = directed_graph_drawing.Connect("c4", n2, n3, "2 -> 3", VA.Connections.ConnectorType.Curved);
-            var c5 = directed_graph_drawing.Connect("c5", n3, n0, "3 -> 0", VA.Connections.ConnectorType.Curved);
-
-            connectors = new DG.Connector[]
-            {
-                c0,c1,c2,c3,c4,c5
-            };
         }
 
         [TestMethod]
         public void RenderDirectedGraphWithDynamicConnectors()
         {
-            var directed_graph_drawing = new DG.Drawing();
-            DG.Shape[] shapes;
-            DG.Connector[] connectors;
-            this.filldrawing(directed_graph_drawing, out shapes, out connectors);
+            var directed_graph_drawing = this.create_sample_graph();
 
             var options = new VA.Layout.Models.DirectedGraph.MSAGLLayoutOptions();
             options.UseDynamicConnectors = true;
@@ -81,9 +44,9 @@ namespace TestVisioAutomation
         [TestMethod]
         public void RenderDirectedGraphWithCustomProps()
         {
-            var directed_graph_drawing = new VA.Layout.Models.DirectedGraph.Drawing();
+            var d = new VA.Layout.Models.DirectedGraph.Drawing();
 
-            var n0 = directed_graph_drawing.AddShape("n0", "Untitled Node", "basflo_u.vss",
+            var n0 = d.AddShape("n0", "Untitled Node", "basflo_u.vss",
                                    "Decision");
             n0.Size = new VA.Drawing.Size(3, 2);
             n0.CustomProperties = new Dictionary<string, VA.CustomProperties.CustomPropertyCells>();
@@ -98,7 +61,7 @@ namespace TestVisioAutomation
             var doc = this.GetNewDoc();
             var page1 = visapp.ActivePage;
 
-            directed_graph_drawing.Render(page1, options);
+            d.Render(page1, options);
             
             Assert.IsNotNull(n0.VisioShape);
             var props_dic = VA.CustomProperties.CustomPropertyHelper.Get(n0.VisioShape);
@@ -110,6 +73,28 @@ namespace TestVisioAutomation
             page1.Application.ActiveWindow.ViewFit = (short) IVisio.VisWindowFit.visFitPage;
 
             doc.Close(true);
+        }
+
+        private DG.Drawing create_sample_graph()
+        {
+            var d = new DG.Drawing();
+
+            var basic_stencil = "basic_u.vss";
+            var n0 = d.AddShape("n0", "Node 0", basic_stencil, "Rectangle");
+            n0.Size = new VA.Drawing.Size(3, 2);
+            var n1 = d.AddShape("n1", "Node 1", basic_stencil, "Rectangle");
+            var n2 = d.AddShape("n2", "Node 2", basic_stencil, "Rectangle");
+            var n3 = d.AddShape("n3", "Node 3", basic_stencil, "Rectangle");
+            var n4 = d.AddShape("n4", "Node 4\nUnconnected", basic_stencil, "Rectangle");
+
+            var c0 = d.Connect("c0", n0, n1, "0 -> 1", VA.Connections.ConnectorType.Curved);
+            var c1 = d.Connect("c1", n1, n2, "1 -> 2", VA.Connections.ConnectorType.RightAngle);
+            var c2 = d.Connect("c2", n1, n0, "0 -> 1", VA.Connections.ConnectorType.Curved);
+            var c3 = d.Connect("c3", n0, n2, "0 -> 2", VA.Connections.ConnectorType.Straight);
+            var c4 = d.Connect("c4", n2, n3, "2 -> 3", VA.Connections.ConnectorType.Curved);
+            var c5 = d.Connect("c5", n3, n0, "3 -> 0", VA.Connections.ConnectorType.Curved);
+
+            return d;
         }
     }
 }
