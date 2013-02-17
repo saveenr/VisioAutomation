d15f04b397dfad97d2f34a28b063efe55ac4dd7e
saveenr <none@none>
2013-02-17 12:24:57 -0800

    Made GetCommandHelp more reliable

--- VisioAutomation_2010/VisioPS/Scripts/GetCommandHelp.ps1
+++ VisioAutomation_2010/VisioPS/Scripts/GetCommandHelp.ps1
@@ -1,13 +1,27 @@
-Import-Module ..\bin\debug\VisioPS.dll
+Set-StrictMode -Version 2
+$ErrorActionPreference = "Stop"
+
+$script_path = $myinvocation.mycommand.path
+$script_folder = Split-Path $script_path -Parent
+$project_path = Split-Path $script_folder -Parent
+$bindebug_path = Join-Path $src_path "bin/Debug"
+$localdll = Join-Path $bindebug_path "VisioPS.dll"
+
+if (!(Test-Path $visiops_localdll))
+{
+    Write-Host "Error: Cannot find" $localdll
+    Break
+}
+
+Import-Module $visiops_localdll 
 
 $cmds = Get-Command -Module VisioPS
 
-$n = 0
 foreach ($cmd in $cmds) 
 { 
-    Write-Host; "--------------------"; 
+    Write-Host "--------------------"
+    Write-Host
+
 	get-help $cmd.Name 
-    $n++
 } 
 
-Write-Host Found $n commands in Module
