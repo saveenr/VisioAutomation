caa3f37cc15d876513543d8ed3e9ff85bee7f1ca
saveenr <none@none>
2013-03-06 19:33:15 -0800

    Added Set-VisioPageProperty

---
+++ VisioAutomation_2010/VisioPS/Commands/Set_VisioPageProperty.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using IVisio = Microsoft.Office.Interop.Visio;
+using SMA = System.Management.Automation;
+using System.Linq;
+using VA=VisioAutomation;
+
+namespace VisioPS.Commands
+{
+    [SMA.Cmdlet(SMA.VerbsCommon.Set, "VisioPageProperty")]
+    public class Set_VisioPageProperty: VisioPSCmdlet
+    {
+        [SMA.Parameter(Mandatory = false)] public string Width { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string Height { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string PageBottomMargin;
+        [SMA.Parameter(Mandatory = false)] public string PageHeight;
+        [SMA.Parameter(Mandatory = false)] public string PageLeftMargin;
+        [SMA.Parameter(Mandatory = false)] public string PageLineJumpDirX;
+        [SMA.Parameter(Mandatory = false)] public string PageLineJumpDirY;
+        [SMA.Parameter(Mandatory = false)] public string PageRightMargin;
+        [SMA.Parameter(Mandatory = false)] public string PageScale;
+        [SMA.Parameter(Mandatory = false)] public string PageShapeSplit;
+        [SMA.Parameter(Mandatory = false)] public string PageTopMargin;
+        [SMA.Parameter(Mandatory = false)] public string PageWidth;
+        [SMA.Parameter(Mandatory = false)] public IList<IVisio.Page> Pages;
+
+        [SMA.Parameter(Mandatory = false)]
+        public SMA.SwitchParameter BlastGuards;
+
+        [SMA.Parameter(Mandatory = false)]
+        public SMA.SwitchParameter TestCircular;
+
+
+        protected override void ProcessRecord()
+        {
+            var scriptingsession = this.ScriptingSession;
+
+            var update = new VisioAutomation.ShapeSheet.Update();
+            update.BlastGuards = this.BlastGuards;
+            update.TestCircular= this.TestCircular;
+
+            var target_pages = this.Pages ?? new [] { scriptingsession.Page.Get() };
+
+            foreach (var page in target_pages)
+            {
+                var pagesheet = page.PageSheet;
+                var id = pagesheet.ID16;
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.PageBottomMargin, this.PageBottomMargin);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.PageHeight, this.Height);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.PageLeftMargin, this.PageLeftMargin);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.PageLineJumpDirX, this.PageLineJumpDirX);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.PageLineJumpDirY, this.PageLineJumpDirY);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.PageRightMargin, this.PageRightMargin);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.PageScale, this.PageScale);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.PageShapeSplit, this.PageShapeSplit);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.PageTopMargin, this.PageTopMargin);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.PageWidth, this.PageWidth);
+                update.Execute(page);
+
+            }
+
+        }
+    }
+}
--- VisioAutomation_2010/VisioPS/Commands/Set_VisioShapeProperty.cs
+++ VisioAutomation_2010/VisioPS/Commands/Set_VisioShapeProperty.cs
@@ -9,129 +9,86 @@ namespace VisioPS.Commands
     [SMA.Cmdlet(SMA.VerbsCommon.Set, "VisioShapeProperty")]
     public class Set_VisioShapeProperty: VisioPSCmdlet
     {
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string Width { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string Width { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string Height { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string PinX { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string PinY { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string LocPinX { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string LocPinY { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string Angle { get; set; }
 
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string Height { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string PinX { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string PinY { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string LocPinX { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string LocPinY { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string FillPattern { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string FillForegroundColor { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string FillForegroundtransparency { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string FillBackgroundColor { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string FillBackgroundtransparency { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string FillPattern { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string FillForegroundColor { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string FillForegroundtransparency { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string FillBackgroundColor { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string FillBackgroundtransparency { get; set; }
         
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string LinePattern { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string LinePattern { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string LineWeight{ get; set; }
+        [SMA.Parameter(Mandatory = false)] public string LineColor { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string LineCap { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string Rounding { get; set; }
 
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string LineWeight{ get; set; }
+        [SMA.Parameter(Mandatory = false)] public string CharCase { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string CharColor { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string CharFont { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string CharFontScale { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string CharLetterspace { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string CharSize { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string CharStyle { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string CharColorTransparency { get; set; }
 
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string LineColor { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string BeginX { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string BeginY{ get; set; }
+        [SMA.Parameter(Mandatory = false)] public string EndX{ get; set; }
+        [SMA.Parameter(Mandatory = false)] public string EndY{ get; set; }
 
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string LineCap { get; set; }
 
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string CharCase { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string CharColor { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-
-        public string CharFont { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string CharFontScale { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string CharLetterspace { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string CharSize { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string CharStyle { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string CharColorTransparency { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string BeginX { get; set; }
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string BeginY{ get; set; }
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string EndX{ get; set; }
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string EndY{ get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string Rounding { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string ShadowBackground { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string ShadowBackgroundTransparency { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string ShadowForeground { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string ShadowBackground { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string ShadowBackgroundTransparency { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string ShadowForeground { get; set; }
         
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string ShadowForegroundTransparency { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string ShadowForegroundTransparency { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string ShadowObliqueAngle { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string ShadowOffsetX { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string ShadowOffsetY { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string ShadowPattern { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string ShadowScalefactor { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string ShadowType { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string SelectMode { get; set; }
+        [SMA.Parameter(Mandatory = false)] public SMA.SwitchParameter BlastGuards;
+        [SMA.Parameter(Mandatory = false)] public SMA.SwitchParameter TestCircular;
 
 
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string ShadowObliqueAngle { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string LockAspect;
+        [SMA.Parameter(Mandatory = false)] public string LockBegin;
+        [SMA.Parameter(Mandatory = false)] public string LockCalcWH;
+        [SMA.Parameter(Mandatory = false)] public string LockCrop;
+        [SMA.Parameter(Mandatory = false)] public string LockCustProp;
+        [SMA.Parameter(Mandatory = false)] public string LockDelete;
+        [SMA.Parameter(Mandatory = false)] public string LockEnd;
+        [SMA.Parameter(Mandatory = false)] public string LockFormat;
+        [SMA.Parameter(Mandatory = false)] public string LockFromGroupFormat;
+        [SMA.Parameter(Mandatory = false)] public string LockGroup;
+        [SMA.Parameter(Mandatory = false)] public string LockHeight;
+        [SMA.Parameter(Mandatory = false)] public string LockMoveX;
+        [SMA.Parameter(Mandatory = false)] public string LockMoveY;
+        [SMA.Parameter(Mandatory = false)] public string LockRotate;
+        [SMA.Parameter(Mandatory = false)] public string LockSelect;
+        [SMA.Parameter(Mandatory = false)] public string LockTextEdit;
+        [SMA.Parameter(Mandatory = false)] public string LockThemeColors;
+        [SMA.Parameter(Mandatory = false)] public string LockThemeEffects;
+        [SMA.Parameter(Mandatory = false)] public string LockVtxEdit;
+        [SMA.Parameter(Mandatory = false)] public string LockWidth;
 
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string ShadowOffsetX { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string ShadowOffsetY { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string ShadowPattern { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string ShadowScalefactor { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string ShadowType { get; set; }
-
-        [SMA.Parameter(Position = 1, Mandatory = false)]
-        public string SelectMode { get; set; }
+        [SMA.Parameter(Mandatory = false)] public string TxtAngle;
+        [SMA.Parameter(Mandatory = false)] public string TxtHeight;
+        [SMA.Parameter(Mandatory = false)] public string TxtLocPinX ;
+        [SMA.Parameter(Mandatory = false)] public string TxtLocPinY;
+        [SMA.Parameter(Mandatory = false)] public string TxtPinX;
+        [SMA.Parameter(Mandatory = false)] public string TxtPinY;
+        [SMA.Parameter(Mandatory = false)] public string TxtWidth;
         
-        [SMA.Parameter(Mandatory = false)]
-        public SMA.SwitchParameter BlastGuards;
-
-        [SMA.Parameter(Mandatory = false)]
-        public SMA.SwitchParameter TestCircular;
-
-
         [SMA.Parameter(Mandatory = false)]
         public IList<IVisio.Shape> Shapes;
 
@@ -152,6 +109,8 @@ namespace VisioPS.Commands
                 update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.Height, this.Height);
                 update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.PinX, this.PinX);
                 update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.PinY, this.PinY);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.Angle, this.Angle);
+ 
                 update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.FillForegnd, this.FillForegroundColor);
                 update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.FillPattern, this.FillPattern);
                 update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.FillBkgnd, this.FillBackgroundColor);
@@ -194,6 +153,37 @@ namespace VisioPS.Commands
                 update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LocPinY, this.LocPinY);
 
                 update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.SelectMode, this.SelectMode);
+
+
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockAspect , this.LockAspect);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockBegin, this.LockBegin);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockCalcWH, this.LockCalcWH);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockCrop, this.LockCrop);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockCustProp, this.LockCustProp);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockDelete, this.LockDelete);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockEnd, this.LockEnd);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockFormat, this.LockFormat);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockAspect, this.LockFromGroupFormat);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockGroup, this.LockGroup);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockHeight, this.LockHeight);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockMoveX, this.LockMoveX);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockMoveY, this.LockMoveY);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockRotate, this.LockRotate);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockSelect, this.LockSelect);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockTextEdit, this.LockTextEdit);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockThemeColors, this.LockThemeColors);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockThemeEffects, this.LockThemeEffects);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockVtxEdit, this.LockVtxEdit);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.LockWidth, this.LockWidth);
+
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.TxtAngle, this.TxtAngle);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.TxtHeight, this.TxtHeight);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.TxtLocPinX, this.TxtHeight);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.TxtLocPinY, this.TxtLocPinY);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.TxtPinX, this.TxtPinX);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.TxtPinY, this.TxtPinY);
+                update.SetFormulaIgnoreNull(id, VisioAutomation.ShapeSheet.SRCConstants.TxtWidth, this.TxtWidth);
+
 
             }
 
