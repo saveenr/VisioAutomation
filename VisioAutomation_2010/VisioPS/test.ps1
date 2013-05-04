739416534176c34a6a9ec2f5cc0ec7c5ef4861e6
saveenr <none@none>
2013-05-04 11:36:39 -0700

    Updated the test script

--- VisioAutomation_2010/VisioPS/test.ps1
+++ VisioAutomation_2010/VisioPS/test.ps1
@@ -14,3 +14,15 @@ $types_file = Join-Path $bin_debug "VisioPS.Types.ps1xml"
 Import-Module $visiops_dll 
 
 Update-TypeData $types_file
+
+New-VisioApplication
+New-VisioDocument
+
+$doc = New-VisioDocument
+$basic_stencil = Open-VisioDocument "basic_u.vss"
+
+$rectangle_master = Get-VisioMaster -Master "Rectangle" -Stencil $basic_stencil
+
+
+$shapes = New-VisioShape -Masters $rectangle_master -Points 2.0,2.0,4.0,4.0,6.0,6.0
+
