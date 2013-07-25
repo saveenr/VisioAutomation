95496627c52b12b94bf04133ce980ba23eaf5e6a
saveenr <none@none>
2013-07-25 07:32:41 -0700

    Internal code simplification when doing getresults
    --HG--
    branch : NewQuery

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/ShapeSheetHelper.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/ShapeSheetHelper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using IVisio=Microsoft.Office.Interop.Visio;
@@ -90,84 +91,49 @@ namespace VisioAutomation.ShapeSheet
 
         public static TResult[] GetResults<TResult>(IVisio.Page page, short[] stream, IList<IVisio.VisUnitCodes> unitcodes)
         {
-            return _GetResults<TResult>(page, stream, unitcodes);
-        }
-
-        public static TResult[] GetResults<TResult>(IVisio.Shape shape, short[] stream, IList<IVisio.VisUnitCodes> unitcodes)
-        {
-            return _GetResults<TResult>(shape, stream, unitcodes);
-        }
-
-        public static TResult[] _GetResults<TResult>(object visio_object, short[] stream, IList<IVisio.VisUnitCodes> unitcodes)
-        {
             EnforceValidResultType(typeof(TResult));
 
-            int numitems; 
-
-            if (visio_object is IVisio.Shape)
-            {
-                numitems = check_stream_size(stream, 3);
-            }
-            else if (visio_object is IVisio.Page)
-            {
-                numitems = check_stream_size(stream, 4);
-            }
-            else
-            {
-                throw new VA.AutomationException("Internal error: Only Page and Shape objects supported in Execute()");
-            }
-            
+            int numitems = check_stream_size(stream, 4);
             if (numitems < 1)
             {
                 return new TResult[0];
             }
 
             var result_type = typeof(TResult);
-            
-            // Create the unit codes array
-            object[] unitcodes_obj_array = null;
-            if (unitcodes!=null)
+            var unitcodes_obj_array = get_unit_code_obj_array(unitcodes);
+            var flags = get_get_set_args<TResult>(result_type);
+
+            System.Array results_sa = null;
+            page.GetResults(stream, (short)flags, unitcodes_obj_array, out results_sa);
+            var results = get_results_array<TResult>(results_sa, numitems);
+
+            return results;
+        }
+
+        public static TResult[] GetResults<TResult>(IVisio.Shape shape, short[] stream, IList<IVisio.VisUnitCodes> unitcodes)
+        {
+            EnforceValidResultType(typeof(TResult));
+
+            int numitems  = check_stream_size(stream, 3);
+
+            if (numitems < 1)
             {
-                unitcodes_obj_array = new object[unitcodes.Count];
-                for (int i = 0; i < unitcodes.Count; i++)
-                {
-                    unitcodes_obj_array[i] = unitcodes[i];
-                }
+                return new TResult[0];
             }
 
-            // Calculate the flags based on the result datatype
-            IVisio.VisGetSetArgs flags;
-            if (result_type == typeof(int))
-            {
-                flags = IVisio.VisGetSetArgs.visGetTruncatedInts;
-            }
-            else if (result_type == typeof(double))
-            {
-                flags = IVisio.VisGetSetArgs.visGetFloats;
-            }
-            else if (result_type == typeof(string))
-            {
-                flags = IVisio.VisGetSetArgs.visGetStrings;
-            }
-            else
-            {
-                string msg = string.Format("Internal error: Unsupported Result Type: {0}", result_type.Name);
-                throw new VA.AutomationException(msg);
-            }
-            
-            System.Array results_sa=null;
-            if (visio_object is IVisio.Shape)
-            {
-                var shape = (IVisio.Shape) visio_object;
-                shape.GetResults(stream, (short)flags, unitcodes_obj_array, out results_sa);
-            }
-            else if (visio_object is IVisio.Page)
-            {
-                var page = (IVisio.Page)visio_object;
-                page.GetResults( stream, (short)flags, unitcodes_obj_array, out results_sa);
-            }
+            var result_type = typeof(TResult);
+            var unitcodes_obj_array = get_unit_code_obj_array(unitcodes);
+            var flags = get_get_set_args<TResult>(result_type);
 
-            // Convert the System.Array back to a strongly typed array
+            System.Array results_sa = null;
+            shape.GetResults(stream, (short)flags, unitcodes_obj_array, out results_sa);
+            var results = get_results_array<TResult>(results_sa, numitems);
+
+            return results;
+        }
+
+        private static TResult[] get_results_array<TResult>(Array results_sa, int numitems)
+        {
             if (results_sa.Length != numitems)
             {
                 string msg = string.Format(
@@ -178,9 +144,46 @@ namespace VisioAutomation.ShapeSheet
             }
 
             TResult[] results = new TResult[results_sa.Length];
-            results_sa.CopyTo(results, 0 );
-
+            results_sa.CopyTo(results, 0);
             return results;
+        }
+
+        private static IVisio.VisGetSetArgs get_get_set_args<TResult>(Type result_type)
+        {
+            IVisio.VisGetSetArgs flags;
+            if (result_type == typeof (int))
+            {
+                flags = IVisio.VisGetSetArgs.visGetTruncatedInts;
+            }
+            else if (result_type == typeof (double))
+            {
+                flags = IVisio.VisGetSetArgs.visGetFloats;
+            }
+            else if (result_type == typeof (string))
+            {
+                flags = IVisio.VisGetSetArgs.visGetStrings;
+            }
+            else
+            {
+                string msg = string.Format("Internal error: Unsupported Result Type: {0}", result_type.Name);
+                throw new VA.AutomationException(msg);
+            }
+            return flags;
+        }
+
+        private static object[] get_unit_code_obj_array(IList<IVisio.VisUnitCodes> unitcodes)
+        {
+            // Create the unit codes array
+            object[] unitcodes_obj_array = null;
+            if (unitcodes != null)
+            {
+                unitcodes_obj_array = new object[unitcodes.Count];
+                for (int i = 0; i < unitcodes.Count; i++)
+                {
+                    unitcodes_obj_array[i] = unitcodes[i];
+                }
+            }
+            return unitcodes_obj_array;
         }
 
         internal static void EnforceValidResultType(System.Type result_type)
