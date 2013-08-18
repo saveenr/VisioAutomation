4790cfb8b36ec904bcd02e7cf51993b317d61555
saveenr <none@none>
2013-08-18 10:17:58 -0700

    Added control over grid spacing

--- VisioAutomation_2010/VisioPS/Commands/New/New_VisioGridLayout.cs
+++ VisioAutomation_2010/VisioPS/Commands/New/New_VisioGridLayout.cs
@@ -11,28 +11,35 @@ namespace VisioPS.Commands
         [SMA.Parameter(Position = 0, Mandatory = true)]
         public IVisio.Master Master { get; set; }
 
-        [SMA.Parameter(Position=1, Mandatory = true)]
+        [SMA.Parameter(Mandatory = true)]
         public int Columns { get; set; }
 
-        [SMA.Parameter(Position=2, Mandatory = true)]
+        [SMA.Parameter(Mandatory = true)]
         public int Rows { get; set; }
 
-        [SMA.Parameter(Position = 3, Mandatory = true)]
+        [SMA.Parameter(Mandatory = true)]
         public double CellWidth = 0.5;
         
-        [SMA.Parameter(Position = 4, Mandatory = true)]
+        [SMA.Parameter(Mandatory = true)]
         public double CellHeight = 0.5;
 
-        [SMA.Parameter(Position = 5, Mandatory = false)]
+        [SMA.Parameter(Mandatory = false)]
+        public double CellHorizontalSpacing = 0.25;
+
+        [SMA.Parameter(Mandatory = false)]
+        public double CellVerticalSpacing = 0.25;
+
+        [SMA.Parameter(Mandatory = false)]
         public GRID.RowDirection RowDirection = GRID.RowDirection.BottomToTop;
 
-        [SMA.Parameter(Position = 5, Mandatory = false)]
+        [SMA.Parameter(Mandatory = false)]
         public GRID.ColumnDirection ColumnDirection = GRID.ColumnDirection.LeftToRight;
 
         protected override void ProcessRecord()
         {
             var cellsize = new VA.Drawing.Size(CellWidth, CellHeight);
             var layout = new GRID.GridLayout(this.Columns, this.Rows, cellsize, this.Master);
+            layout.CellSpacing = new VA.Drawing.Size(this.CellHorizontalSpacing, this.CellVerticalSpacing);
             layout.RowDirection = this.RowDirection;
             layout.ColumnDirection = this.ColumnDirection;
             this.WriteObject(layout);
