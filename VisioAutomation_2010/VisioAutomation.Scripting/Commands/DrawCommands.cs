55e08298e813f325986750c408a1e97400f11011
saveenr <none@none>
2013-08-25 03:45:03 -0700

    Moved duplicate code

--- VisioAutomation_2010/VisioAutomation.Scripting/Commands/DrawCommands.cs
+++ VisioAutomation_2010/VisioAutomation.Scripting/Commands/DrawCommands.cs
@@ -345,5 +345,102 @@ namespace VisioAutomation.Scripting.Commands
             this.Session.WriteVerbose("Finished rendering all pages");
             this.Session.WriteVerbose("Finished rendering directed graph.");
         }
+
+        public void Duplicate(int n)
+        {
+            this.CheckVisioApplicationAvailable();
+            this.CheckActiveDrawingAvailable();
+
+            if (n < 1)
+            {
+                throw new System.ArgumentOutOfRangeException("n");
+            }
+            if (!this.Session.HasSelectedShapes())
+            {
+                return;
+            }
+
+            // TODO: Add ability to duplicate all the selected shapes, not just the first one
+            // this dupicates exactly 1 shape N - times what it
+            // it should do is duplicate all M selected shapes N times so that M*N shapes are created
+
+            var application = this.Session.VisioApplication;
+            using (var undoscope = new VA.Application.UndoScope(this.Session.VisioApplication, string.Format("Duplicate Shape {0} Times", n)))
+            {
+                var active_window = application.ActiveWindow;
+                var selection = active_window.Selection;
+                var active_page = application.ActivePage;
+                DrawCommands.CreateDuplicates(active_page, selection[1], n);
+            }
+        }
+
+        private static IList<IVisio.Shape> CreateDuplicates(IVisio.Page page,
+                                           IVisio.Shape shape,
+                                           int n)
+        {
+            // NOTE: n is the total number you want INCLUDING the original shape
+            // example n=0 then result={s0}
+            // example n=1, result={s0}
+            // example n=2, result={s0,s1}
+            // example n=3, result={s0,s1,s2}
+            // where s0 is the original shape
+
+            if (n < 2)
+            {
+                return new List<IVisio.Shape> { shape };
+            }
+
+            int num_doubles = (int)System.Math.Log(n, 2.0);
+            int leftover = n - ((int)System.Math.Pow(2.0, num_doubles));
+            if (leftover < 0)
+            {
+                throw new System.InvalidOperationException("internal error: leftover value must greater than or equal to zero");
+            }
+
+            var duplicated_shapes = new List<IVisio.Shape> { shape };
+
+            var application = page.Application;
+            var win = application.ActiveWindow;
+
+            foreach (int i in Enumerable.Range(0, num_doubles))
+            {
+                win.DeselectAll();
+                win.Select(duplicated_shapes, IVisio.VisSelectArgs.visSelect);
+                var selection = win.Selection;
+                selection.Duplicate();
+                var selection1 = win.Selection;
+                duplicated_shapes.AddRange(selection1.AsEnumerable());
+            }
+
+            if (leftover > 0)
+            {
+                var leftover_shapes = duplicated_shapes.Take(leftover);
+                win.DeselectAll();
+                win.Select(leftover_shapes, IVisio.VisSelectArgs.visSelect);
+                var selection = win.Selection;
+                selection.Duplicate();
+                var selection1 = win.Selection;
+                duplicated_shapes.AddRange(selection1.AsEnumerable());
+            }
+
+            win.DeselectAll();
+            win.Select(duplicated_shapes, IVisio.VisSelectArgs.visSelect);
+
+            if (duplicated_shapes.Count != n)
+            {
+                string msg = string.Format("internal error: failed to create {0} shapes, instead created {1}", n,
+                                           duplicated_shapes.Count);
+                throw new VA.Scripting.ScriptingException(msg);
+            }
+
+            var selection2 = win.Selection;
+            if (selection2.Count != n)
+            {
+                throw new VA.Scripting.ScriptingException("internal error: failed to select the duplicated shapes");
+            }
+
+            return duplicated_shapes;
+        }
+
     }
 }
--- VisioAutomation_2010/VisioAutomation.Scripting/Commands/FormatCommands.cs
+++ VisioAutomation_2010/VisioAutomation.Scripting/Commands/FormatCommands.cs
@@ -2,9 +2,6 @@
 using System.Linq;
 using VA=VisioAutomation;
 using IVisio = Microsoft.Office.Interop.Visio;
-using System.Collections.Generic;
-using System.Linq;
-using VisioAutomation.Extensions;
 
 namespace VisioAutomation.Scripting.Commands
 {
@@ -54,34 +51,6 @@ namespace VisioAutomation.Scripting.Commands
             var shapeids = shapes.Select(s => s.ID).ToList();
             var fmts = VA.Format.ShapeFormatCells.GetCells(this.Session.VisioApplication.ActivePage, shapeids);
             return fmts;
-        }
-
-        public void Duplicate(int n)
-        {
-            this.CheckVisioApplicationAvailable();
-            this.CheckActiveDrawingAvailable();
-            
-            if (n < 1)
-            {
-                throw new System.ArgumentOutOfRangeException("n");
-            }
-            if (!this.Session.HasSelectedShapes())
-            {
-                return;
-            }
-
-            // TODO: Add ability to duplicate all the selected shapes, not just the first one
-            // this dupicates exactly 1 shape N - times what it
-            // it should do is duplicate all M selected shapes N times so that M*N shapes are created
-
-            var application = this.Session.VisioApplication;
-            using (var undoscope = new VA.Application.UndoScope(this.Session.VisioApplication, string.Format("Duplicate Shape {0} Times",n)))
-            {
-                var active_window = application.ActiveWindow;
-                var selection = active_window.Selection;
-                var active_page = application.ActivePage;
-                FormatCommands.CreateDuplicates(active_page, selection[1], n);
-            }
         }
 
         private double? cached_size_width;
@@ -199,74 +168,5 @@ namespace VisioAutomation.Scripting.Commands
 
             this.cache.PasteFormat(active_page, shapeids, category, apply_formulas);
         }
-
-        public static IList<IVisio.Shape> CreateDuplicates(IVisio.Page page,
-                                                   IVisio.Shape shape,
-                                                   int n)
-        {
-            // NOTE: n is the total number you want INCLUDING the original shape
-            // example n=0 then result={s0}
-            // example n=1, result={s0}
-            // example n=2, result={s0,s1}
-            // example n=3, result={s0,s1,s2}
-            // where s0 is the original shape
-
-            if (n < 2)
-            {
-                return new List<IVisio.Shape> { shape };
-            }
-
-            int num_doubles = (int)System.Math.Log(n, 2.0);
-            int leftover = n - ((int)System.Math.Pow(2.0, num_doubles));
-            if (leftover < 0)
-            {
-                throw new System.InvalidOperationException("internal error: leftover value must greater than or equal to zero");
-            }
-
-            var duplicated_shapes = new List<IVisio.Shape> { shape };
-
-            var application = page.Application;
-            var win = application.ActiveWindow;
-
-            foreach (int i in Enumerable.Range(0, num_doubles))
-            {
-                win.DeselectAll();
-                win.Select(duplicated_shapes, IVisio.VisSelectArgs.visSelect);
-                var selection = win.Selection;
-                selection.Duplicate();
-                var selection1 = win.Selection;
-                duplicated_shapes.AddRange(selection1.AsEnumerable());
-            }
-
-            if (leftover > 0)
-            {
-                var leftover_shapes = duplicated_shapes.Take(leftover);
-                win.DeselectAll();
-                win.Select(leftover_shapes, IVisio.VisSelectArgs.visSelect);
-                var selection = win.Selection;
-                selection.Duplicate();
-                var selection1 = win.Selection;
-                duplicated_shapes.AddRange(selection1.AsEnumerable());
-            }
-
-            win.DeselectAll();
-            win.Select(duplicated_shapes, IVisio.VisSelectArgs.visSelect);
-
-            if (duplicated_shapes.Count != n)
-            {
-                string msg = string.Format("internal error: failed to create {0} shapes, instead created {1}", n,
-                                           duplicated_shapes.Count);
-                throw new VA.Scripting.ScriptingException(msg);
-            }
-
-            var selection2 = win.Selection;
-            if (selection2.Count != n)
-            {
-                throw new VA.Scripting.ScriptingException("internal error: failed to select the duplicated shapes");
-            }
-
-            return duplicated_shapes;
-        }
-
     }
 }
