1fef2a93ca2344c02477ef41565c22fd6a6c727e
saveenr <none@none>
2013-09-12 00:52:37 -0700

    Merged Sector class with PieSlice
    --HG--
    branch : NewNamespace

--- VisioAutomation_2010/VisioAutomation/Models/Charting/Sector.cs
+++ VisioAutomation_2010/VisioAutomation/Models/Charting/Sector.cs
@@ -5,19 +5,6 @@ namespace VisioAutomation.Models.Charting
 {
     public struct Sector
     {
-        public double StartAngle { get; private set; }
-        public double EndAngle { get; private set; }
 
-        public Sector(double start, double end) :
-            this()
-        {
-            this.StartAngle = start;
-            this.EndAngle = end;
-        }
-
-        public double Angle
-        {
-            get { return this.EndAngle - this.StartAngle; }
-        }
     }
 }
