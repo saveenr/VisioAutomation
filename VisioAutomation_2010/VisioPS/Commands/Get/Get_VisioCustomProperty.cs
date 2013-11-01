e2667c22f735f7b4c6f655cf3028f5925eb937c8
saveenr <none@none>
2013-10-31 22:29:35 -0700

    VisioPS - retrieving properties returns a datatable instead of a dictionary

--- VisioAutomation_2010/VisioPS/Commands/Get/Get_VisioCustomProperty.cs
+++ VisioAutomation_2010/VisioPS/Commands/Get/Get_VisioCustomProperty.cs
@@ -9,24 +9,60 @@ namespace VisioPS.Commands
     {
         [SMA.Parameter(Mandatory = false)]
         public IVisio.Shape[] Shapes;
-        
+
+        [SMA.Parameter(Mandatory = false)]
+        public SMA.SwitchParameter GetCells;
+
         protected override void ProcessRecord()
         {
-            if (this.Shapes == null)
-            {
-                this.WriteVerboseEx("Null shapes array passed to Get-VisioCustomProperty");
-                return;
-            }
-
-            if (this.Shapes.Length == 0)
-            {
-                this.WriteVerboseEx("0 shapes array passed to Get-VisioCustomProperty");
-                return;
-            }
-
             var scriptingsession = this.ScriptingSession;
             var dic = scriptingsession.CustomProp.Get(this.Shapes);
-            this.WriteObject(dic);
+
+            if (this.GetCells)
+            {
+                this.WriteObject(dic);                
+            }
+            else
+            {
+                var dt = new System.Data.DataTable("CustomProperties");
+                dt.Columns.Add("ShapeId");
+                dt.Columns.Add("Name");
+                dt.Columns.Add("Value");
+                dt.Columns.Add("Format");
+                dt.Columns.Add("Invisible");
+                dt.Columns.Add("Label");
+                dt.Columns.Add("LangId");
+                dt.Columns.Add("Prompt");
+                dt.Columns.Add("SortKey");
+                dt.Columns.Add("Type");
+                dt.Columns.Add("Verify");
+
+                foreach (var shape_propdic_pair in dic)
+                {
+                    var shape = shape_propdic_pair.Key;
+                    var propdic = shape_propdic_pair.Value;
+                    foreach (var propname_propcells_pair in propdic)
+                    {
+                        string propname = propname_propcells_pair.Key;
+                        var propcells = propname_propcells_pair.Value;
+
+                        dt.Rows.Add(
+                            shape.ID, 
+                            propname, 
+                            propcells.Value.Formula, 
+                            propcells.Format.Formula, 
+                            propcells.Invisible.Formula,
+                            propcells.Label.Formula,
+                            propcells.LangId.Formula, 
+                            propcells.Prompt.Formula, 
+                            propcells.SortKey.Formula,
+                            propcells.Type.Formula,
+                            propcells.Verify.Formula);
+                    }
+                }
+
+                this.WriteObject(dt);
+            }
         }
     }
 }
