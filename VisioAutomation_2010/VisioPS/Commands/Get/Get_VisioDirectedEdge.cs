b0cef010466708690a773206cf6993a8e2faa4f7
saveenr <none@none>
2013-11-02 00:59:19 -0700

    VisioPS - GetVisioDirectedEdge now returns shapeIDs instead of shape objects

--- VisioAutomation_2010/VisioPS/Commands/Get/Get_VisioDirectedEdge.cs
+++ VisioAutomation_2010/VisioPS/Commands/Get/Get_VisioDirectedEdge.cs
@@ -8,13 +8,34 @@ namespace VisioPS.Commands
     public class Get_VisioDirectedEdge : VisioPSCmdlet
     {
         [SMA.Parameter(Position = 0, Mandatory = false)]
-        public ConnectorArrowEdgeHandling TreatAsConnected { get; set; }
+        public SMA.SwitchParameter UndirectedToDirected { get; set; }
 
         protected override void ProcessRecord()
         {
             var scriptingsession = this.ScriptingSession;
-            var edges = scriptingsession.Connection.GetDirectedEdges(TreatAsConnected);
-            this.WriteObject(edges);
+
+            var treat_as_connected = UndirectedToDirected
+                ? ConnectorArrowEdgeHandling.TreatNoArrowEdgesAsBidirectional
+                : ConnectorArrowEdgeHandling.ExcludeNoArrowEdges;
+
+            var edges = scriptingsession.Connection.GetDirectedEdges(treat_as_connected);
+
+            foreach (var edge in edges)
+            {
+                var e = new DirectedEdge();
+                e.FromShapeID = edge.From.ID;
+                e.ToShapeID = edge.To.ID;
+                e.ConnectorID = edge.Connector.ID;
+
+                this.WriteObject(e);                
+            }
         }
+    }
+
+    public class DirectedEdge
+    {
+        public int FromShapeID;
+        public int ToShapeID;
+        public int ConnectorID;
     }
 }
