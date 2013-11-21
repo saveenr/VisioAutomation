4a01b8dcaa397c279a4fc1cb9b27724eacb59b26
saveenr <none@none>
2013-11-21 11:37:04 -0800

    Improved resize page

--- VisioAutomation_2010/VisioPS/Commands/Invoke/Invoke_VisioResizePageToFitContents.cs
+++ VisioAutomation_2010/VisioPS/Commands/Invoke/Invoke_VisioResizePageToFitContents.cs
@@ -3,20 +3,54 @@ using SMA = System.Management.Automation;
 
 namespace VisioPS.Commands
 {
-    [SMA.Cmdlet(SMA.VerbsLifecycle.Invoke, "VisioResizePageToFitContents")]
+    [SMA.Cmdlet(SMA.VerbsCommon.Resize, "VisioPage")]
     public class Invoke_VisioResizePageToFitContents : VisioPS.VisioPSCmdlet
     {
-        [SMA.Parameter(Position = 0, Mandatory = true)]
+        [SMA.Parameter(Mandatory = false)] public double Width = -1;
+
+        [SMA.Parameter(Mandatory = false)] public double Height = -1;
+
+        [SMA.Parameter(Mandatory = false)]
+        public SMA.SwitchParameter FitContents;
+
+        [SMA.Parameter(Mandatory = false)]
         public double BorderWidth { get; set; }
 
-        [SMA.Parameter(Position = 1, Mandatory = true)]
+        [SMA.Parameter(Mandatory = false)]
         public double BorderHeight { get; set; }
 
         protected override void ProcessRecord()
         {
             var scriptingsession = this.ScriptingSession;
-            var bordersize = new VA.Drawing.Size(BorderWidth, BorderWidth);
-            scriptingsession.Page.ResizeToFitContents(bordersize, true);
+
+            if (FitContents)
+            {
+                var bordersize = new VA.Drawing.Size(BorderWidth, BorderWidth);
+                scriptingsession.Page.ResizeToFitContents(bordersize, true);                
+            }
+
+            if (Width > 0 || Height > 0)
+            {
+                var page = scriptingsession.VisioApplication.ActivePage;
+                var pagecells = VA.Pages.PageCells.GetCells(page.PageSheet);
+
+                var newpagecells = new VA.Pages.PageCells();
+                
+                if (Width > 0)
+                {
+                    newpagecells.PageWidth = this.Width;
+                }
+
+                if (Height > 0)
+                {
+                    newpagecells.PageHeight = this.Height;
+                }
+
+                var update = new VA.ShapeSheet.Update();
+                update.SetFormulas(newpagecells);
+                update.BlastGuards = true;
+                update.Execute(page);
+            }
         }
     }
 }
