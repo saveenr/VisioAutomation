be6a1ac061a9a760cfbbfa212bdeb1108aa6ed4f
saveenr <none@none>
2013-11-29 01:52:30 -0800

    Removed Unused intermediate methods

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/CellGroups/BaseCellGroup.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/CellGroups/BaseCellGroup.cs
@@ -7,11 +7,9 @@ namespace VisioAutomation.ShapeSheet.CellGroups
     public abstract class BaseCellGroup
     {
         public delegate T RowToObject<T>(CellData<double>[] data);
-        public delegate T _RowToObject<T>(CellData<string>[] data);
         public delegate T ____RowToObject<T,X>(CellData<X>[] data);
 
         public delegate T QueryResultToObject<T>(Query.CellQuery.QueryResult<CellData<double>> data);
-        public delegate T _QueryResultToObject<T>(Query.CellQuery.QueryResult<CellData<string>> data);
         public delegate T ____QueryResultToObject<T,X>(Query.CellQuery.QueryResult<CellData<X>> data);
 
         public struct SRCValuePair
