a32f03605e671442006616b9083d54e96307e134
Saveen Reddy <saveenr@live.com>
2014-05-21 22:46:34 -0700

    Export-VisioSelection now automatically handles export to XHTML based on  filename

---
+++ VisioAutomation_2010/VisioPS/Commands/Export/Export_VisioSelection.cs
@@ -0,0 +1,47 @@
+﻿using VAS=VisioAutomation.Scripting;
+using SMA = System.Management.Automation;
+
+namespace VisioPS.Commands
+{
+    [SMA.Cmdlet(SMA.VerbsData.Export, "VisioSelectionAsXHTML")]
+    public class Export_VisioSelectionAsXHTML : VisioPS.VisioPSCmdlet
+    {
+        [SMA.Parameter(Position = 0, Mandatory = true)]
+        [SMA.ValidateNotNullOrEmpty]
+        public string Filename;
+
+        [SMA.Parameter(Mandatory = false)]
+        public SMA.SwitchParameter Overwrite;
+
+        protected override void ProcessRecord()
+        {
+            if (!System.IO.File.Exists(this.Filename))
+            {
+                this.WriteVerboseEx("File already exists");
+                if (this.Overwrite)
+                {
+                    System.IO.File.Delete(this.Filename);
+                }
+                else
+                {
+                    var exc = new System.ArgumentException("File already exists");
+                    throw exc;
+                }
+            }
+
+            var scriptingsession = this.ScriptingSession;
+
+            string ext = System.IO.Path.GetExtension(this.Filename).ToLowerInvariant();
+
+            if (ext == ".html" || ext == ".xhtml" || ext == ".htm")
+            {
+                scriptingsession.Export.SelectionToSVGXHTML(this.Filename);                
+            }
+            else
+            {
+                scriptingsession.Export.SelectionToFile(this.Filename);
+               
+            }
+        }
+    }
+}
