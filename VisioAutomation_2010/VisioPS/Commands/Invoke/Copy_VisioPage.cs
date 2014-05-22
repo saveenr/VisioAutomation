f4d3405584f36bd23a70032fc8cd0696da6216ad
Saveen Reddy <saveenr@live.com>
2014-05-21 22:11:56 -0700

    Renamed files

---
+++ VisioAutomation_2010/VisioPS/Commands/Invoke/Copy_VisioPage.cs
@@ -0,0 +1,31 @@
+using SMA = System.Management.Automation;
+using IVisio = Microsoft.Office.Interop.Visio;
+
+namespace VisioPS.Commands
+{
+    [SMA.Cmdlet(SMA.VerbsCommon.Copy, "VisioPage")]
+    public class Copy_VisioPage : VisioPS.VisioPSCmdlet
+    {
+        [SMA.Parameter(Position = 0, Mandatory = false)]
+        public string Name = null;
+
+        [SMA.Parameter(Position = 1, Mandatory = false)]
+        public IVisio.Document ToDocument=null;
+
+        protected override void ProcessRecord()
+        {
+            IVisio.Page newpage;
+            var scriptingsession = this.ScriptingSession;
+            if (this.ToDocument == null)
+            {
+                newpage = scriptingsession.Page.Duplicate();
+            }
+            else
+            {
+                newpage = scriptingsession.Page.Duplicate(this.ToDocument);
+            }
+
+            this.WriteObject(newpage);            
+        }
+    }
+}
---
+++ VisioAutomation_2010/VisioPS/Commands/Invoke/Copy_VisioShape.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using SMA = System.Management.Automation;
+using IVisio = Microsoft.Office.Interop.Visio;
+
+namespace VisioPS.Commands
+{
+    [SMA.Cmdlet(SMA.VerbsCommon.Copy, "VisioShape")]
+    public class Copy_VisioShape : VisioPS.VisioPSCmdlet
+    {
+        [SMA.Parameter(Mandatory = false)]public IVisio.Shape[] Shapes;
+
+        protected override void ProcessRecord()
+        {
+            var scriptingsession = this.ScriptingSession;
+            scriptingsession.Selection.Duplicate(this.Shapes);
+        }
+    }
+}
