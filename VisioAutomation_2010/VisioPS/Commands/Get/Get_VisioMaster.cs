a2c07b114cb7d1ec3f9c0eb242e7e2111361d47b
Saveen Reddy <saveenr@live.com>
2014-06-03 03:43:01 -0700

    Cleanup

--- VisioAutomation_2010/VisioPS/Commands/Get/Get_VisioMaster.cs
+++ VisioAutomation_2010/VisioPS/Commands/Get/Get_VisioMaster.cs
@@ -17,37 +17,39 @@ namespace VisioPS.Commands
         {
             var scriptingsession = this.ScriptingSession;
 
-            bool isprovided_Master = this.Master !=null;
-            bool isprovided_Stencil = this.Stencil !=null;
+            bool master_specified = this.Master !=null;
+            bool stencil_specified = this.Stencil !=null;
 
-            this.WriteVerboseEx("Master name provided: {0}", isprovided_Master);
-            this.WriteVerboseEx("Stencil document provided: {0}", isprovided_Stencil);
-
-            if (!isprovided_Master && isprovided_Stencil)
+            if (master_specified)
             {
-                this.WriteVerbose("Retrieve all the masters in the specified stencil doc");
-                var masters = scriptingsession.Master.Get(this.Stencil);
-                this.WriteObject(masters,false);
+                if (stencil_specified)
+                {
+                    this.WriteVerbose("Get master from specified document");
+                    var master = scriptingsession.Master.Get(this.Master, this.Stencil);
+                    this.WriteObject(master);
+                }
+                else
+                {
+                    this.WriteVerbose("Get master from active document");
+                    var master = scriptingsession.Master.Get(this.Master);
+                    this.WriteObject(master);
+                }
             }
-            else if (isprovided_Master && !isprovided_Stencil)
+            else
             {
-                this.WriteVerbose("Retrieve a specific master in the currently active document");
-                var master = scriptingsession.Master.Get(this.Master);
-                this.WriteObject(master);
+                if (stencil_specified)
+                {
+                    this.WriteVerbose("Get all masters from specified document");
+                    var masters = scriptingsession.Master.Get(this.Stencil);
+                    this.WriteObject(masters, false);                    
+                }
+                else
+                {
+                    this.WriteVerbose("Get all masters from active document");
+                    var masters = scriptingsession.Master.Get();
+                    this.WriteObject(masters, false);                   
+                }
             }
-            else if (!isprovided_Master && !isprovided_Stencil)
-            {
-                this.WriteVerbose("Retrieve all the masters in the currently active document");
-                var masters = scriptingsession.Master.Get();
-                this.WriteObject(masters, false);
-                return;
-            }
-            else if (isprovided_Master && isprovided_Stencil)
-            {
-                this.WriteVerbose("Retrieve a specific master in the specified stencil document");
-                var master = scriptingsession.Master.Get(this.Master, this.Stencil);
-                this.WriteObject(master);
-            }           
         }
     }
 }
