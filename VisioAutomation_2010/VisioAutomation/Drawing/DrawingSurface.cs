c998965d62cfd0a4f34be14c36b0215cf37aceba
Saveen Reddy <saveenr@live.com>
2014-09-01 17:11:16 -0700

    DrawingSurface now accounts for all the Page extension methods

--- VisioAutomation_2010/VisioAutomation/Drawing/DrawingSurface.cs
+++ VisioAutomation_2010/VisioAutomation/Drawing/DrawingSurface.cs
@@ -33,6 +33,30 @@ namespace VisioAutomation.Drawing
             this.Shape = shape;
         }
 
+        public IVisio.Shape DrawLine(VA.Drawing.Point p1, VA.Drawing.Point p2)
+        {
+
+            if (this.Master != null)
+            {
+                var shape = this.Master.DrawLine(p1.X, p1.Y, p2.X, p2.Y);
+                return shape;
+            }
+            else if (this.Page != null)
+            {
+                var shape = this.Page.DrawLine(p1.X, p1.Y, p2.X, p2.Y);
+                return shape;
+            }
+            else if (this.Shape != null)
+            {
+                var shape = this.Shape.DrawLine(p1.X, p1.Y, p2.X, p2.Y);
+                return shape;
+            }
+            else
+            {
+                throw new System.ArgumentException("Unhandled Drawing Surface");
+            }
+
+        }
 
         public IVisio.Shape DrawPolyLine(IList<VA.Drawing.Point> points)
         {
@@ -59,12 +83,10 @@ namespace VisioAutomation.Drawing
             }
         }
 
-        public IVisio.Shape DrawBezier(IList<VA.Drawing.Point> points)
+        public IVisio.Shape DrawBezier(IList<VA.Drawing.Point> points, short degree, short flags)
         {
             var doubles_array = VA.Drawing.Point.ToDoubles(points).ToArray();
-            short degree = 3;
-            short flags = 0;
-
+ 
             if (this.Master != null)
             {
                 var shape = this.Master.DrawBezier(doubles_array, degree, flags);
@@ -84,6 +106,14 @@ namespace VisioAutomation.Drawing
             {
                 throw new System.ArgumentException("Unhandled Drawing Surface");
             }
+        }
+
+        public IVisio.Shape DrawBezier(IList<VA.Drawing.Point> points)
+        {
+            short degree = 3;
+            short flags = 0;
+            var shape = this.DrawBezier(points, degree, flags);
+            return shape;
         }
 
         public IVisio.Shape DrawOval(VA.Drawing.Rectangle rect)
@@ -116,6 +146,12 @@ namespace VisioAutomation.Drawing
             var rect = new VA.Drawing.Rectangle(A, B);
 
             return this.DrawOval(rect);
+        }
+
+        public IVisio.Shape DrawRectangle(VA.Drawing.Rectangle rect)
+        {
+            var shape = this.DrawRectangle(rect.Left, rect.Bottom, rect.Right, rect.Top);
+            return shape;
         }
 
         public IVisio.Shape DrawRectangle(double x0, double y0, double x1, double y1)
@@ -154,10 +190,52 @@ namespace VisioAutomation.Drawing
 
                 return shape;
             }
+            else if (this.Shape != null)
+            {
+                var shape = this.Shape.DrawLine(x0, y0, x1, y1);
+
+                return shape;
+            }
             else
             {
                 throw new System.ArgumentException("Unhandled Drawing Surface");
             }
         }
+
+        public IVisio.Shape DrawNURBS(IList<VA.Drawing.Point> controlpoints,
+                                     IList<double> knots,
+                                     IList<double> weights, int degree)
+        {
+            // flags:
+            // None = 0,
+            // IVisio.VisDrawSplineFlags.visSpline1D
+
+            var flags = 0;
+            double[] pts_dbl_a = VA.Drawing.Point.ToDoubles(controlpoints).ToArray();
+            double[] kts_dbl_a = knots.ToArray();
+            double[] weights_dbl_a = weights.ToArray();
+
+            if (this.Master != null)
+            {
+                var shape = this.Master.DrawNURBS((short)degree, (short)flags, pts_dbl_a, kts_dbl_a, weights_dbl_a);
+                return shape;
+            }
+            else if (this.Page != null)
+            {
+                var shape = this.Page.DrawNURBS((short)degree, (short)flags, pts_dbl_a, kts_dbl_a, weights_dbl_a);
+                return shape;
+            }
+            else if (this.Shape != null)
+            {
+                var shape = this.Shape.DrawNURBS((short)degree, (short)flags, pts_dbl_a, kts_dbl_a, weights_dbl_a);
+                return shape;
+            }
+            else
+            {
+                throw new System.ArgumentException("Unhandled Drawing Surface");
+            }
+
+        }
+
     }
 }
--- VisioAutomation_2010/VisioAutomation/Extensions/PageMethods.cs
+++ VisioAutomation_2010/VisioAutomation/Extensions/PageMethods.cs
@@ -14,42 +14,43 @@ namespace VisioAutomation.Extensions
 
         public static IVisio.Shape DrawLine(this IVisio.Page page, VA.Drawing.Point p1, VA.Drawing.Point p2)
         {
-            var shape = page.DrawLine(p1.X, p1.Y, p2.X, p2.Y);
+            var surface = new VA.Drawing.DrawingSurface(page);
+            var shape = surface.DrawLine(p1.X, p1.Y, p2.X, p2.Y);
             return shape;
         }
 
         public static IVisio.Shape DrawOval(this IVisio.Page page, VA.Drawing.Rectangle rect)
         {
-            var shape = page.DrawOval(rect.Left, rect.Bottom, rect.Right, rect.Top);
+            var surface = new VA.Drawing.DrawingSurface(page);
+            var shape = surface.DrawOval(rect);
             return shape;
         }
 
         public static IVisio.Shape DrawRectangle(this IVisio.Page page, VA.Drawing.Rectangle rect)
         {
-            var shape = page.DrawRectangle(rect.Left, rect.Bottom, rect.Right, rect.Top);
+            var surface = new VA.Drawing.DrawingSurface(page);
+            var shape = surface.DrawRectangle(rect);
             return shape;
         }
 
         public static IVisio.Shape DrawBezier(this IVisio.Page page, IList<VA.Drawing.Point> points)
         {
-            short degree = 3;
-            short flags = 0;
-            var shape = page.DrawBezier(points, degree, flags);
-            return shape;
+            var surface = new VA.Drawing.DrawingSurface(page);
+            var shape = surface.DrawBezier(points);
+            return shape;        
         }
 
         public static IVisio.Shape DrawBezier(this IVisio.Page page, IList<VA.Drawing.Point> points, short degree, short flags)
         {
-            // DrawBezier method: http://msdn.microsoft.com/en-us/library/office/ff766273.aspx
-            var doubles_array = VA.Drawing.Point.ToDoubles(points).ToArray();
-            var shape = page.DrawBezier(doubles_array, degree, flags);
-            return shape;
+            var surface = new VA.Drawing.DrawingSurface(page);
+            var shape = surface.DrawBezier(points,degree,flags);
+            return shape;        
         }
 
         public static IVisio.Shape DrawPolyline(this IVisio.Page page, IList<VA.Drawing.Point> points)
         {
-            var doubles_array = VA.Drawing.Point.ToDoubles(points).ToArray();
-            var shape = page.DrawPolyline(doubles_array, 0);
+            var surface = new VA.Drawing.DrawingSurface(page);
+            var shape = surface.DrawBezier(points);
             return shape;
         }
 
@@ -57,16 +58,8 @@ namespace VisioAutomation.Extensions
                                              IList<double> knots,
                                              IList<double> weights, int degree)
         {
-            // flags:
-            // None = 0,
-            // IVisio.VisDrawSplineFlags.visSpline1D
-
-            var flags = 0;
-            double[] pts_dbl_a = VA.Drawing.Point.ToDoubles(controlpoints).ToArray();
-            double[] kts_dbl_a = knots.ToArray();
-            double[] weights_dbl_a = weights.ToArray();
-
-            var shape = page.DrawNURBS((short) degree, (short) flags, pts_dbl_a, kts_dbl_a, weights_dbl_a);
+            var surface = new VA.Drawing.DrawingSurface(page);
+            var shape = surface.DrawNURBS(controlpoints, knots, weights, degree);
             return shape;
         }
 
