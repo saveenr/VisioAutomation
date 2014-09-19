bfc77ac61ffd43d3a09aea9eca1ddebe9c7e0691
Saveen Reddy <saveenr@live.com>
2014-09-19 04:01:54 -0700

    Removed unused test methods

--- VisioAutomation_2010/TestVisioAutomation/Globals.cs
+++ VisioAutomation_2010/TestVisioAutomation/Globals.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace TestVisioAutomation.Common
+﻿namespace TestVisioAutomation.Common
 {
     public static class Globals
     {
-        public static Helper Helper = new Helper("VisioAutomation Unit Test Output");
+        public static Helper Helper = new Helper("VisioAutomation_Unit_Test_Output");
     }
 }
 
