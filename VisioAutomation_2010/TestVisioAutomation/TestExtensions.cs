1da5df1faa7badc11e8c233db6d54ba4dfe74fe3
Saveen Reddy <saveenr@live.com>
2014-09-19 03:53:45 -0700

    cleanup

--- VisioAutomation_2010/TestVisioAutomation/TestExtensions.cs
+++ VisioAutomation_2010/TestVisioAutomation/TestExtensions.cs
@@ -2,9 +2,9 @@ namespace TestVisioAutomation
 {
     public static class TestExtensions
     {
-        public static VisioAutomation.Drawing.Point Pin(this VisioAutomation.Shapes.XFormCells xthis)
+        public static VisioAutomation.Drawing.Point Pin(this VisioAutomation.Shapes.XFormCells xform)
         {
-            return new VisioAutomation.Drawing.Point(xthis.PinX.Result, xthis.PinY.Result);
+            return new VisioAutomation.Drawing.Point(xform.PinX.Result, xform.PinY.Result);
         }
     }
 }
