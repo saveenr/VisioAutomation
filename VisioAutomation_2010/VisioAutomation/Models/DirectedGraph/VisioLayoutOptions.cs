ff2b32e0d8df4f06724030fad02c280a7c41923e
Saveen Reddy <saveenr@live.com>
2014-12-30 09:30:00 -0800

    Finished incomplete part of previous commit

--- VisioAutomation_2010/VisioAutomation/Models/DirectedGraph/VisioLayoutOptions.cs
+++ VisioAutomation_2010/VisioAutomation/Models/DirectedGraph/VisioLayoutOptions.cs
@@ -1,7 +1,16 @@
+using VA=VisioAutomation;
+
 namespace VisioAutomation.Models.DirectedGraph
 {
     public class VisioLayoutOptions
     {
-        
+        public VA.Pages.PageLayout.Layout Layout;
+
+        public VisioLayoutOptions()
+        {
+            var flowchart = new VA.Pages.PageLayout.FlowchartLayout();
+            flowchart.Direction = VA.Pages.PageLayout.Direction.TopToBottom;
+            this.Layout = flowchart;
+        }        
     }
 }
--- VisioAutomation_2010/VisioAutomation/Models/DirectedGraph/VisioRenderer.cs
+++ VisioAutomation_2010/VisioAutomation/Models/DirectedGraph/VisioRenderer.cs
@@ -4,16 +4,11 @@ namespace VisioAutomation.Models.DirectedGraph
 {
     class VisioRenderer
     {
-        public VA.Pages.PageLayout.Layout Layout;
-
         public VisioRenderer()
         {
-            var flowchart = new VA.Pages.PageLayout.FlowchartLayout();
-            flowchart.Direction = VA.Pages.PageLayout.Direction.TopToBottom;
-            this.Layout = flowchart;
         }
 
-        public void Render(Microsoft.Office.Interop.Visio.Page page, VisioAutomation.Models.DirectedGraph.Drawing drawing, VisioLayoutOptions options)
+        public void Render(Microsoft.Office.Interop.Visio.Page page, VA.Models.DirectedGraph.Drawing drawing, VisioLayoutOptions options)
         {
             // This is Visio-based render - it does NOT use MSAGL
             if (page == null)
@@ -25,15 +20,15 @@ namespace VisioAutomation.Models.DirectedGraph
             {
                 throw new System.ArgumentNullException("options");
             }
-            
-            var page_node = new VisioAutomation.DOM.Page();
+
+            var page_node = new VA.DOM.Page();
             double x = 0;
             double y = 1;
             foreach (var shape in drawing.Shapes)
             {
                 var shape_nodes = page_node.Shapes.Drop(shape.MasterName, shape.StencilName, x, y);
                 shape.DOMNode = shape_nodes;
-                shape.DOMNode.Text = new VisioAutomation.Text.Markup.TextElement(shape.Label);
+                shape.DOMNode.Text = new VA.Text.Markup.TextElement(shape.Label);
                 x += 1.0;
             }
 
@@ -42,14 +37,14 @@ namespace VisioAutomation.Models.DirectedGraph
 
                 var connector_node = page_node.Shapes.Connect("Dynamic Connector", "basic_u.vss", connector.From.DOMNode, connector.To.DOMNode);
                 connector.DOMNode = connector_node;
-                connector.DOMNode.Text = new VisioAutomation.Text.Markup.TextElement(connector.Label);
+                connector.DOMNode.Text = new VA.Text.Markup.TextElement(connector.Label);
             }
 
             page_node.ResizeToFit = true;
             page_node.ResizeToFitMargin = new VisioAutomation.Drawing.Size(0.5, 0.5);
-            if (this.Layout != null)
+            if (options.Layout != null)
             {
-                page_node.Layout = this.Layout;
+                page_node.Layout = options.Layout;
             }
             page_node.Render(page);
         }
