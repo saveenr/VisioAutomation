68128d0d33cb15ffee2997d8957c961277a2d6f2
Saveen Reddy <saveenr@live.com>
2015-01-31 21:45:38 -0800

    cleanup

--- VisioAutomation_2010/TestVisioAutomation/Controls/Control_Tests.cs
+++ VisioAutomation_2010/TestVisioAutomation/Controls/Control_Tests.cs
@@ -1,5 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using VACONT = VisioAutomation.Shapes.Controls;
+using VACTRL = VisioAutomation.Shapes.Controls;
 using VA = VisioAutomation;
 
 namespace TestVisioAutomation
@@ -15,18 +15,18 @@ namespace TestVisioAutomation
             var s1 = page1.DrawRectangle(0, 0, 4, 1);
 
             // Ensure we start with 0 controls
-            Assert.AreEqual(0, VACONT.ControlHelper.GetCount(s1));
+            Assert.AreEqual(0, VACTRL.ControlHelper.GetCount(s1));
 
             // Add the first control
-            int ci1 = VACONT.ControlHelper.Add(s1);
-            Assert.AreEqual(1, VACONT.ControlHelper.GetCount(s1));
+            int ci1 = VACTRL.ControlHelper.Add(s1);
+            Assert.AreEqual(1, VACTRL.ControlHelper.GetCount(s1));
 
             // Add the second control
-            int ci2 = VACONT.ControlHelper.Add(s1);
-            Assert.AreEqual(2, VACONT.ControlHelper.GetCount(s1));
+            int ci2 = VACTRL.ControlHelper.Add(s1);
+            Assert.AreEqual(2, VACTRL.ControlHelper.GetCount(s1));
             
             // retrieve the control information
-            var controls = VACONT.ControlCells.GetCells(s1);
+            var controls = VACTRL.ControlCells.GetCells(s1);
 
             // verify that the controls were set propery
             Assert.AreEqual(2, controls.Count);
@@ -36,10 +36,10 @@ namespace TestVisioAutomation
             Assert.AreEqual("Controls.Row_2", controls[1].XDynamics.Formula);
 
             // Delete both controls
-            VACONT.ControlHelper.Delete(s1, 0);
-            Assert.AreEqual(1, VACONT.ControlHelper.GetCount(s1));
-            VACONT.ControlHelper.Delete(s1, 0);
-            Assert.AreEqual(0, VACONT.ControlHelper.GetCount(s1));
+            VACTRL.ControlHelper.Delete(s1, 0);
+            Assert.AreEqual(1, VACTRL.ControlHelper.GetCount(s1));
+            VACTRL.ControlHelper.Delete(s1, 0);
+            Assert.AreEqual(0, VACTRL.ControlHelper.GetCount(s1));
 
             page1.Delete(0);
         }
