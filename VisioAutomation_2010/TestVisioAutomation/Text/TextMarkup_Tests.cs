1135b4d9ad53a7394a16ecdff522012c1cb14d4d
Saveen Reddy <saveenr@live.com>
2015-05-05 07:45:06 -0700

    whitespace cleanup

--- VisioAutomation_2010/TestVisioAutomation/Text/TextMarkup_Tests.cs
+++ VisioAutomation_2010/TestVisioAutomation/Text/TextMarkup_Tests.cs
@@ -17,21 +17,15 @@ namespace TestVisioAutomation.Text
         {
             Assert.IsTrue(this.Match("*","") );
             Assert.IsTrue(this.Match("*", "AbC"));
-
             Assert.IsTrue(this.Match("A*", "Abc"));
-            Assert.IsFalse(this.Match("A*", "bcA"));
-
             Assert.IsTrue(this.Match("*C", "Abc"));
+            Assert.IsFalse(this.Match("A*", "bcA"));
             Assert.IsFalse(this.Match("*C", "bcA"));
-
             Assert.IsTrue(this.Match("A*C", "AbC"));
             Assert.IsFalse(this.Match("A*C", "AbA"));
-
             Assert.IsTrue(this.Match("A*B*C", "A---b---C"));
             Assert.IsFalse(this.Match("A*B*C", "A---b---A"));
-
             Assert.IsTrue(this.Match("A*B?C", "A---bXC"));
-
         }
     }
 }
