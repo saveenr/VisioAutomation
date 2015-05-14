f97ba16d3e23f3781a71babc535a4b11b605c4ce
Saveen Reddy <saveenr@live.com>
2015-05-14 10:07:14 -0700

    VisioPS unit test cleanup

--- VisioAutomation_2010/TestVisioAutomation/PowerShellContext.cs
+++ VisioAutomation_2010/TestVisioAutomation/PowerShellContext.cs
@@ -1,15 +1,17 @@
 using System;
 using SMA=System.Management.Automation;
+using IVisio = Microsoft.Office.Interop.Visio;
+using System.Collections.Generic;
 
 namespace TestVisioAutomation
 {
+
     public class PowerShellContext
     {
-        public VisioPowerShell.Commands.New_VisioApplication VisioApp = null;
-        public System.Management.Automation.PowerShell PowerShell;
-        public System.Management.Automation.Runspaces.InitialSessionState SessionState;
-        public System.Management.Automation.Runspaces.Runspace RunSpace;
-        public System.Management.Automation.RunspaceInvoke Invoker;
+        private System.Management.Automation.PowerShell PowerShell;
+        private System.Management.Automation.Runspaces.InitialSessionState SessionState;
+        private System.Management.Automation.Runspaces.Runspace RunSpace;
+        private System.Management.Automation.RunspaceInvoke Invoker;
 
         public PowerShellContext()
         {
@@ -42,6 +44,74 @@ namespace TestVisioAutomation
             this.RunSpace = null;
             this.SessionState = null;
             this.PowerShell = null;
+        }
+
+        public IVisio.ShapeClass New_Visio_Container(string cont_master_name, string cont_doc)
+        {
+            var cmd = string.Format("New-VisioContainer -Master (Get-VisioMaster \"{0}\" (Open-VisioDocument \"{1}\"))", cont_master_name, cont_doc);
+            var results = this.Invoker.Invoke(cmd);
+            var shape = (IVisio.ShapeClass)results[0].BaseObject;
+            return shape;
+        }
+
+        public List<IVisio.Shape> New_VisioShape(IVisio.MasterClass master, double[] points)
+        {
+            var pipeline = this.RunSpace.CreatePipeline();
+            var cmd = new SMA.Runspaces.Command(@"New-VisioShape");
+            cmd.AddParameter("Master", master);
+            cmd.AddParameter("Points", points);
+            pipeline.Commands.Add(cmd);
+            var results = pipeline.Invoke();
+            var shapes = (List<IVisio.Shape>)results[0].BaseObject;
+            return shapes;
+        }
+
+        public IVisio.MasterClass Get_Visio_Master(string rectangle, string basic_u_vss)
+        {
+            var cmd = string.Format("(Get-VisioMaster \"{0}\" (Open-VisioDocument \"{1}\"))", rectangle, basic_u_vss);
+            var results = this.Invoker.Invoke(cmd);
+            var master = (IVisio.MasterClass)results[0].BaseObject;
+            return master;
+        }
+
+        public IVisio.DocumentClass New_Visio_Document()
+        {
+            var results = this.Invoker.Invoke("New-VisioDocument");
+            var doc = (IVisio.DocumentClass)results[0].BaseObject;
+            return doc;
+        }
+
+        public IVisio.PageClass New_Visio_Page()
+        {
+            var results = this.Invoker.Invoke("New-VisioPage");
+            var page = (IVisio.PageClass)results[0].BaseObject;
+            return page;
+        }
+
+
+        public IVisio.ApplicationClass Get_Visio_Application()
+        {
+            var app_0 = this.Invoker.Invoke("Get-VisioApplication");
+            var app = (IVisio.ApplicationClass)app_0[0].BaseObject;
+            return app;
+        }
+
+        public System.Data.DataTable Get_Visio_Page_Cell( string [] Cells, bool GetResults, string ResultType)
+        {
+            var pipeline = this.RunSpace.CreatePipeline();
+            var cmd = new SMA.Runspaces.Command(@"Get-VisioPageCell");
+            cmd.AddParameter("Cells", Cells);
+            cmd.AddParameter("GetResults", GetResults);
+            cmd.AddParameter("ResultType", ResultType);
+            pipeline.Commands.Add(cmd);
+            var results = pipeline.Invoke();
+            var dt = (System.Data.DataTable)results[0].BaseObject;
+            return dt;
+        }
+
+        public void Close_Visio_Application()
+        {
+            this.Invoker.Invoke("Close-VisioApplication -Force");
         }
 
     }
--- VisioAutomation_2010/TestVisioAutomation/VisioPowerShellTests.cs
+++ VisioAutomation_2010/TestVisioAutomation/VisioPowerShellTests.cs
@@ -33,42 +33,32 @@ namespace TestVisioAutomation
         [TestMethod]
         public void PSTestNewVisioDocument()
         {
-            var doc = VisioPowerShellTests.New_Visio_Document();
- 
-            // Verify results
+            var doc = ps_context.New_Visio_Document();
             Assert.IsNotNull(doc);
-            var ps_object = doc[0];
-            Assert.AreEqual("Microsoft.Office.Interop.Visio.DocumentClass", ps_object.ToString());
-            Assert.IsNotNull(ps_object.Properties["Name"].Value);
-            Assert.IsFalse(String.IsNullOrEmpty(ps_object.Properties["Name"].Value.ToString()));
- 
-            // Close Visio Application that was created when "New-VisioDocument" was invoked
             VisioPowerShellTests.Close_Visio_Application();
         }
 
         private static void Close_Visio_Application()
         {
-            VisioPowerShellTests.ps_context.Invoker.Invoke("Close-VisioApplication -Force");
+            VisioPowerShellTests.ps_context.Close_Visio_Application();
         }
 
         [TestMethod]
         public void PSTestGetVisioPageCell()
         {
-            var doc = VisioPowerShellTests.New_Visio_Document();
-            var cells1 = VisioPowerShellTests.ps_context.Invoker.Invoke("Get-VisioPageCell -Cells PageWidth,PageHeight -Page (Get-VisioPage -ActivePage) -GetResults -ResultType Double");
-            var data_row_collection1 = (System.Data.DataRowCollection)cells1[0].Properties["Rows"].Value;
-            var results = data_row_collection1[0];
-            Assert.IsNotNull(cells1);
+            var doc = ps_context.New_Visio_Document();
+            var datatable1 = ps_context.Get_Visio_Page_Cell(new[] { "PageWidth", "PageHeight" }, true, "Double");
+            var results = datatable1.Rows[0];
+            Assert.IsNotNull(datatable1);
             Assert.AreEqual(8.5, results["PageWidth"]);
             Assert.AreEqual(11.0, results["PageHeight"]);
             
             //Now lets add another page and get it's width and height
-            var page2 = VisioPowerShellTests.ps_context.Invoker.Invoke("New-VisioPage");
-            var cells2 = VisioPowerShellTests.ps_context.Invoker.Invoke("Get-VisioPageCell -Cells PageWidth,PageHeight -Page (Get-VisioPage -ActivePage) -GetResults -ResultType Double");
-            var data_row_collection2 = (System.Data.DataRowCollection)cells2[0].Properties["Rows"].Value;
-            var results2 = data_row_collection2[0];
+            var page2 = VisioPowerShellTests.ps_context.New_Visio_Page();
+            var datatable2 = ps_context.Get_Visio_Page_Cell(new[] { "PageWidth", "PageHeight" }, true, "Double");
+            var results2 = datatable1.Rows[0];
  
-            Assert.IsNotNull(cells2);
+            Assert.IsNotNull(datatable2);
  	        Assert.AreEqual(8.5, results2["PageWidth"]);
 	        Assert.AreEqual(11.0, results2["PageHeight"]);
 
@@ -79,8 +69,8 @@ namespace TestVisioAutomation
       [TestMethod]
       public void PSNewVisioContainer()
       {
-          var doc = VisioPowerShellTests.New_Visio_Document();
-          var app = VisioPowerShellTests.Get_Visio_Application();
+          var doc = ps_context.New_Visio_Document();
+          var app = ps_context.Get_Visio_Application();
 
           var ver = VisioAutomation.Application.ApplicationHelper.GetVersion(app);
 
@@ -89,60 +79,20 @@ namespace TestVisioAutomation
           var rectangle = "Rectangle";
           var basic_u_vss = "BASIC_U.VSS";
 
-          var rect = VisioPowerShellTests.Get_Visio_Master(rectangle, basic_u_vss);
+          var rect = ps_context.Get_Visio_Master(rectangle, basic_u_vss);
 
-          VisioPowerShellTests.New_VisioShape(rect, new[] {1.0, 1.0});
+          ps_context.New_VisioShape(rect, new[] { 1.0, 1.0 });
 
           // Drop a container on the page... the rectangle we created above should be selected by default. 
           // Since it is selected it will be added as a member to the container.
 
-          var container = VisioPowerShellTests.New_Visio_Container(cont_master_name, cont_doc);
+          var container = ps_context.New_Visio_Container(cont_master_name, cont_doc);
 
           Assert.IsNotNull(container);
 
           VisioPowerShellTests.Close_Visio_Application();
       }
 
-        private static IVisio.ShapeClass New_Visio_Container(string cont_master_name, string cont_doc)
-        {
-            var cmd = string.Format("New-VisioContainer -Master (Get-VisioMaster \"{0}\" (Open-VisioDocument \"{1}\"))", cont_master_name, cont_doc);
-            var results = VisioPowerShellTests.ps_context.Invoker.Invoke(cmd);
-            var shape = (IVisio.ShapeClass)results[0].BaseObject;
-            return shape;
-        }
-
-        private static List<IVisio.Shape> New_VisioShape(IVisio.MasterClass master, double[] points)
-        {
-            var pipeline = VisioPowerShellTests.ps_context.RunSpace.CreatePipeline();
-            var cmd = new SMA.Runspaces.Command(@"New-VisioShape");
-            cmd.AddParameter("Master", master);
-            cmd.AddParameter("Points", points);
-            pipeline.Commands.Add(cmd);
-            var results = pipeline.Invoke();
-            var shapes = (List<IVisio.Shape>)results[0].BaseObject;
-            return shapes;
-        }
-
-        private static IVisio.MasterClass Get_Visio_Master(string rectangle, string basic_u_vss)
-        {
-            var cmd = string.Format("(Get-VisioMaster \"{0}\" (Open-VisioDocument \"{1}\"))", rectangle, basic_u_vss);
-            var results = VisioPowerShellTests.ps_context.Invoker.Invoke(cmd);
-            var master = (IVisio.MasterClass)results[0].BaseObject;
-            return master;
-        }
-
-        private static System.Collections.ObjectModel.Collection<System.Management.Automation.PSObject> New_Visio_Document()
-        {
-            var doc = VisioPowerShellTests.ps_context.Invoker.Invoke("New-VisioDocument");
-            return doc;
-        }
-
-        private static Microsoft.Office.Interop.Visio.ApplicationClass Get_Visio_Application()
-        {
-            var app_0 = VisioPowerShellTests.ps_context.Invoker.Invoke("Get-VisioApplication");
-            var app = (IVisio.ApplicationClass) app_0[0].BaseObject;
-            return app;
-        }
     }
 
     public static class SMA_Extensions
