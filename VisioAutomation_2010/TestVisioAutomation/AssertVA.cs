9cece938a07fa912a8a5400340177726343b700e
Saveen Reddy <saveenr@live.com>
2016-01-02 21:10:29 -0800

    Better parameter names for AssertVA methods

--- VisioAutomation_2010/TestVisioAutomation/AssertVA.cs
+++ VisioAutomation_2010/TestVisioAutomation/AssertVA.cs
@@ -6,30 +6,30 @@ namespace TestVisioAutomation
 {
     public static class AssertVA
     {
-        public static void AreEqual(VADRAW.Point p1, VADRAW.Point p2, double delta)
+        public static void AreEqual(VADRAW.Point point, VADRAW.Point actual_point, double delta)
         {
-            Assert.AreEqual(p1.X,p2.X,delta);
-            Assert.AreEqual(p1.Y, p2.Y, delta);
+            Assert.AreEqual(point.X, actual_point.X,delta);
+            Assert.AreEqual(point.Y, actual_point.Y, delta);
         }
 
-        public static void AreEqual(double x, double y, VADRAW.Point p, double delta)
+        public static void AreEqual(double x, double y, VADRAW.Point actual_point, double delta)
         {
-            Assert.AreEqual(x, p.X, delta);
-            Assert.AreEqual(y, p.Y, delta);
+            Assert.AreEqual(x, actual_point.X, delta);
+            Assert.AreEqual(y, actual_point.Y, delta);
         }
 
-        public static void AreEqual(double left, double bottom, double right, double top, VADRAW.Rectangle r, double delta)
+        public static void AreEqual(double left, double bottom, double right, double top, VADRAW.Rectangle actual_rect, double delta)
         {
-            Assert.AreEqual(left, r.Left, delta);
-            Assert.AreEqual(bottom, r.Bottom, delta);
-            Assert.AreEqual(right, r.Right, delta);
-            Assert.AreEqual(top, r.Top, delta);
+            Assert.AreEqual(left, actual_rect.Left, delta);
+            Assert.AreEqual(bottom, actual_rect.Bottom, delta);
+            Assert.AreEqual(right, actual_rect.Right, delta);
+            Assert.AreEqual(top, actual_rect.Top, delta);
         }
 
-        public static void AreEqual(double x, double y, VADRAW.Size p, double delta)
+        public static void AreEqual(double width, double height, VADRAW.Size actual_size, double delta)
         {
-            Assert.AreEqual(x, p.Width, delta);
-            Assert.AreEqual(y, p.Height, delta);
+            Assert.AreEqual(width, actual_size.Width, delta);
+            Assert.AreEqual(height, actual_size.Height, delta);
         }
 
         public static void AssertSnap(double ex, double ey, VADRAW.SnappingGrid g1, double ix, double iy, double delta)
@@ -37,16 +37,16 @@ namespace TestVisioAutomation
             AssertVA.AreEqual(ex, ey, g1.Snap(ix, iy), delta);
         }
 
-        public static void AreEqual<T>(string formula, T result, VASS.CellData<T> cd)
+        public static void AreEqual<T>(string formula, T result, VASS.CellData<T> actual_celldata)
         {
-            Assert.AreEqual(formula, cd.Formula);
-            Assert.AreEqual(result, cd.Result);
+            Assert.AreEqual(formula, actual_celldata.Formula);
+            Assert.AreEqual(result, actual_celldata.Result);
         }
 
-        public static void AreEqual<T>(string formula, T result, string af, T ar)
+        public static void AreEqual<T>(string formula, T result, string actual_formula, T actual_result)
         {
-            Assert.AreEqual(formula, af);
-            Assert.AreEqual(result, ar);
+            Assert.AreEqual(formula, actual_formula);
+            Assert.AreEqual(result, actual_result);
         }
     }
 }
