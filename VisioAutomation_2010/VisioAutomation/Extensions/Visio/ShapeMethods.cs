526d6e1b476206e08fc9ce18e422a188df205f4e
Saveen Reddy <saveenr@live.com>
2016-04-18 22:51:43 -0700

    Moved extension method bodies into helper methods

--- VisioAutomation_2010/VisioAutomation/Extensions/Visio/ShapeMethods.cs
+++ VisioAutomation_2010/VisioAutomation/Extensions/Visio/ShapeMethods.cs
@@ -7,50 +7,32 @@ namespace VisioAutomation.Extensions
     {
         public static IVisio.Shape DrawLine(this IVisio.Shape shape, Drawing.Point p1, Drawing.Point p2)
         {
-            var surface = new Drawing.DrawingSurface(shape);
-            var s = surface.DrawLine(p1, p2);
-            return s;
+            return Shapes.ShapeHelper.DrawLine(shape, p1, p2);
         }
 
         public static IVisio.Shape DrawQuarterArc(this IVisio.Shape shape, Drawing.Point p0, Drawing.Point p1, IVisio.VisArcSweepFlags flags)
         {
-            var surface = new Drawing.DrawingSurface(shape);
-            var s = surface.DrawQuarterArc(p0, p1, flags);
-            return s;
+            return Shapes.ShapeHelper.DrawQuarterArc(shape, p0, p1, flags);
         }
 
         public static Drawing.Rectangle GetBoundingBox(this IVisio.Shape shape, IVisio.VisBoundingBoxArgs args)
         {
-            var surface = new Drawing.DrawingSurface(shape);
-            var r = surface.GetBoundingBox(args);
-            return r;
+            return Shapes.ShapeHelper.GetBoundingBox(shape, args);
         }
 
         public static Drawing.Point XYFromPage(this IVisio.Shape shape, Drawing.Point xy)
         {
-            // MSDN: http://msdn.microsoft.com/en-us/library/office/ff767213.aspx
-            double xprime;
-            double yprime;
-            shape.XYFromPage(xy.X, xy.Y, out xprime, out yprime);
-            return new Drawing.Point(xprime, yprime);
+            return Shapes.ShapeHelper.XYFromPage(shape, xy);
         }
 
         public static Drawing.Point XYToPage(this IVisio.Shape shape, Drawing.Point xy)
         {
-            // MSDN: http://msdn.microsoft.com/en-us/library/office/ff766239.aspx
-            double xprime;
-            double yprime;
-            shape.XYToPage(xy.X, xy.Y, out xprime, out yprime);
-            return new Drawing.Point(xprime, yprime);
+            return Shapes.ShapeHelper.XYToPage(shape, xy);
         }
 
         public static IEnumerable<IVisio.Shape> ToEnumerable(this IVisio.Shapes shapes)
         {
-            int count = shapes.Count;
-            for (int i = 0; i < count; i++)
-            {
-                yield return shapes[i + 1];
-            }
+            return Shapes.ShapeHelper.ToEnumerable(shapes);
         }
 
         public static IList<IVisio.Shape> GetShapesFromIDs(this IVisio.Shapes shapes, IList<short> shapeids)
--- VisioAutomation_2010/VisioAutomation/Shapes/ShapeHelper.cs
+++ VisioAutomation_2010/VisioAutomation/Shapes/ShapeHelper.cs
@@ -6,6 +6,56 @@ namespace VisioAutomation.Shapes
 {
     public static class ShapeHelper
     {
+        public static IVisio.Shape DrawLine(IVisio.Shape shape, Drawing.Point p1, Drawing.Point p2)
+        {
+            var surface = new Drawing.DrawingSurface(shape);
+            var s = surface.DrawLine(p1, p2);
+            return s;
+        }
+
+        public static IVisio.Shape DrawQuarterArc(IVisio.Shape shape, Drawing.Point p0, Drawing.Point p1, IVisio.VisArcSweepFlags flags)
+        {
+            var surface = new Drawing.DrawingSurface(shape);
+            var s = surface.DrawQuarterArc(p0, p1, flags);
+            return s;
+        }
+
+        public static Drawing.Rectangle GetBoundingBox(IVisio.Shape shape, IVisio.VisBoundingBoxArgs args)
+        {
+            var surface = new Drawing.DrawingSurface(shape);
+            var r = surface.GetBoundingBox(args);
+            return r;
+        }
+
+        public static Drawing.Point XYFromPage(IVisio.Shape shape, Drawing.Point xy)
+        {
+            // MSDN: http://msdn.microsoft.com/en-us/library/office/ff767213.aspx
+            double xprime;
+            double yprime;
+            shape.XYFromPage(xy.X, xy.Y, out xprime, out yprime);
+            return new Drawing.Point(xprime, yprime);
+        }
+
+        public static Drawing.Point XYToPage(IVisio.Shape shape, Drawing.Point xy)
+        {
+            // MSDN: http://msdn.microsoft.com/en-us/library/office/ff766239.aspx
+            double xprime;
+            double yprime;
+            shape.XYToPage(xy.X, xy.Y, out xprime, out yprime);
+            return new Drawing.Point(xprime, yprime);
+        }
+
+        public static IEnumerable<IVisio.Shape> ToEnumerable(IVisio.Shapes shapes)
+        {
+            int count = shapes.Count;
+            for (int i = 0; i < count; i++)
+            {
+                yield return shapes[i + 1];
+            }
+        }
+
+
+
         /// <summary>
         /// Enumerates all shapes contained by a set of shapes recursively
         /// </summary>
