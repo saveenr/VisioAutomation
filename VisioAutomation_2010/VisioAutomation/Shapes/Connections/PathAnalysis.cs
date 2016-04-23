26101e1a8b026ccf5958cf748ffa70e57d8e3436
Saveen Reddy <saveenr@live.com>
2016-04-23 08:38:28 -0700

    Moved BitArray2D class

--- VisioAutomation_2010/VisioAutomation/Shapes/Connections/PathAnalysis.cs
+++ VisioAutomation_2010/VisioAutomation/Shapes/Connections/PathAnalysis.cs
@@ -159,7 +159,7 @@ namespace VisioAutomation.Shapes.Connections
             return edges;
         }
 
-        internal static void PerformWarshall(Internal.BitArray2D adj_matrix)
+        internal static void PerformWarshall(VisioAutomation.Shapes.Connections.BitArray2D adj_matrix)
         {
             if (adj_matrix == null)
             {
@@ -230,7 +230,7 @@ namespace VisioAutomation.Shapes.Connections
             }
 
             int num_vertices = object_to_id.Count;
-            var adj_matrix = new Internal.BitArray2D(num_vertices, num_vertices);
+            var adj_matrix = new VisioAutomation.Shapes.Connections.BitArray2D(num_vertices, num_vertices);
             foreach (var iedge in internal_edges)
             {
                 adj_matrix[iedge.From, iedge.To] = true;
