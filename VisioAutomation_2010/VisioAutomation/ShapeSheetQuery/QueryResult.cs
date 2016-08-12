69a83ad6f4fac1105c6aef920f5fb00b4629eedd
saveenr <saveenr@live.com>
2016-08-11 20:56:18 -0700

    Simplified QueryResult

--- VisioAutomation_2010/VisioAutomation/ShapeSheetQuery/QueryResult.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheetQuery/QueryResult.cs
@@ -2,7 +2,7 @@
 
 namespace VisioAutomation.ShapeSheetQuery
 {
-    public class QueryResult<T> : IEnumerable<T>
+    public class QueryResult<T> 
     {
         public int ShapeID { get; private set; }
         public T[] Cells { get; internal set; }
@@ -11,16 +11,6 @@ namespace VisioAutomation.ShapeSheetQuery
         internal QueryResult(int sid)
         {
             this.ShapeID = sid;
-        }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            return ((IList<T>)this.Cells).GetEnumerator();
-        }
-
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-        {
-            return this.GetEnumerator();
         }
     }
 }
