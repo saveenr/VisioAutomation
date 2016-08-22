1af30459118778c7f48ab74face8378ccc2421b4
Saveen Reddy <saveenr@live.com>
2016-08-22 10:13:20 -0700

    Rename ID to ShapeID in SIDSRC

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/SIDSRC.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/SIDSRC.cs
@@ -5,40 +5,40 @@ namespace VisioAutomation.ShapeSheet
 {
     public struct SIDSRC
     {
-        public short ID { get; }
+        public short ShapeID { get; }
         public short Section { get; }
         public short Row { get; }
         public short Cell { get; }
 
         public SIDSRC(
-            short id,
+            short shape_id,
             IVisio.VisSectionIndices section,
             IVisio.VisRowIndices row,
-            IVisio.VisCellIndices cell) : this(id,(short)section,(short)row,(short)cell)
+            IVisio.VisCellIndices cell) : this(shape_id,(short)section,(short)row,(short)cell)
         {
         }
 
         public SIDSRC(
-            short id,
+            short shape_id,
             short section,
             short row,
             short cell) : this()
         {
-            this.ID = id;
+            this.ShapeID = shape_id;
             this.Section = section;
             this.Row = row;
             this.Cell = cell;
         }
 
         public SIDSRC(
-            short id,
-            SRC src) : this(id,src.Section,src.Row,src.Cell)
+            short shape_id,
+            SRC src) : this(shape_id,src.Section,src.Row,src.Cell)
         {
         }  
         
         public override string ToString()
         {
-            return string.Format("{0}({1},{2},{3},{4})", nameof(SIDSRC),this.ID, this.Section, this.Row, this.Cell);
+            return string.Format("{0}({1},{2},{3},{4})", nameof(SIDSRC),this.ShapeID, this.Section, this.Row, this.Cell);
         }
 
         public static short [] ToStream(IList<SIDSRC> sidsrcs)
@@ -49,7 +49,7 @@ namespace VisioAutomation.ShapeSheet
             {
                 var sidsrc = sidsrcs[i];
                 int pos = i*sidsrc_length;
-                s[pos + 0] = sidsrc.ID;
+                s[pos + 0] = sidsrc.ShapeID;
                 s[pos + 1] = sidsrc.Section;
                 s[pos + 2] = sidsrc.Row;
                 s[pos + 3] = sidsrc.Cell;
