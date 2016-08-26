cdc69c51f1ea63f1404b2da21bedd5bbb43c9245
Saveen Reddy <saveenr@live.com>
2016-08-26 09:40:41 -0700

    Added samples for dropping masters

--- VisioAutomation_2010/VisioAutomation_Documentation_Samples/Program.cs
+++ VisioAutomation_2010/VisioAutomation_Documentation_Samples/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using IVisio = Microsoft.Office.Interop.Visio;
 
 namespace VisioAutomation_Documentation_Samples
 {
@@ -17,77 +16,9 @@ namespace VisioAutomation_Documentation_Samples
 
             CustomProperties_Samples.Set_Custom_Property_on_Shape(doc);
             CustomProperties_Samples.Set_Custom_Property_on_multiple_Shapes(doc);
+            Dropping_Shapes_Using_Masters.One_shape_at_a_time(doc);
+            Dropping_Shapes_Using_Masters.Multiple_shapes_at_a_time(doc);
 
         }
-    }
-
-
-    public static class CustomProperties_Samples
-    {
-        public static void Set_Custom_Property_on_Shape(IVisio.Document doc)
-        {
-            // Set Custom Property_on_a_shape
-
-            var page = doc.Pages.Add();
-            var s1 = page.DrawRectangle(0, 0, 1, 1);
-            var cp = new VisioAutomation.Shapes.CustomProperties.CustomPropertyCells();
-            cp.Value = "Hello World";
-            VisioAutomation.Shapes.CustomProperties.CustomPropertyHelper.Set(s1, "Propname", cp);
-
-            // Retrieve all the Custom properties from a shape
-
-            var props = VisioAutomation.Shapes.CustomProperties.CustomPropertyHelper.Get(s1);
-
-
-
-            //cleanup
-            page.Delete(0);
-        }
-
-        public static void Set_Custom_Property_on_multiple_Shapes(IVisio.Document doc)
-        {
-            // Set Custom Property_on_a_shape
-
-            var page = doc.Pages.Add();
-            var s1 = page.DrawRectangle(0, 0, 1, 1);
-            var s2 = page.DrawRectangle(2, 2, 4, 4);
-
-            var cp1 = new VisioAutomation.Shapes.CustomProperties.CustomPropertyCells();
-            cp1.Value = "Hello";
-            VisioAutomation.Shapes.CustomProperties.CustomPropertyHelper.Set(s1, "Propname", cp1);
-
-            var cp2 = new VisioAutomation.Shapes.CustomProperties.CustomPropertyCells();
-            cp2.Value = "World";
-            VisioAutomation.Shapes.CustomProperties.CustomPropertyHelper.Set(s2, "Propname", cp2);
-
-            // Retrieve all the Custom properties from multiple shapes
-
-            var shapes = new[] {s1, s2};
-            var props = VisioAutomation.Shapes.CustomProperties.CustomPropertyHelper.Get(page,shapes);
-
-            //cleanup
-            page.Delete(0);
-        }
-
-
-        public static void Deleting_a_custom_property_from_a_single_shape(IVisio.Document doc)
-        {
-            // Set Custom Property_on_a_shape
-
-            var page = doc.Pages.Add();
-            var s1 = page.DrawRectangle(0, 0, 1, 1);
-
-            var cp1 = new VisioAutomation.Shapes.CustomProperties.CustomPropertyCells();
-            cp1.Value = "Hello";
-            VisioAutomation.Shapes.CustomProperties.CustomPropertyHelper.Set(s1, "Propname", cp1);
-
-            VisioAutomation.Shapes.CustomProperties.CustomPropertyHelper.Delete(s1,"Propname");
-
-            //cleanup
-            page.Delete(0);
-        }
-
-
-
     }
 }
