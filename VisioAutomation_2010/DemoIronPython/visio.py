903a28bc0c537a5d6b6f8dacd8464a0169f48c78
Saveen Reddy <saveenr@live.com>
2016-09-10 14:41:58 -0700

    cleanup

--- VisioAutomation_2010/DemoIronPython/visio.py
+++ VisioAutomation_2010/DemoIronPython/visio.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import sys
 import os
 import clr
@@ -14,7 +12,8 @@ import Microsoft.Office.Interop.Visio
 
 # Load VisioAutomation
 visauto_nuget_package_name = "VisioAutomation2010"
-visauto_path = os.path.join(script_path , "packages", visauto_nuget_package_name, "lib", "net40" )
+visauto_path = os.path.join(script_path , "packages", visauto_nuget_package_name )
+visauto_dll_path = os.path.join(visauto_path, "lib", "net40" )
 visauto_assemblies = [
     "VisioAutomation.dll",
     "VisioAutomation.DocumentAnalysis.dll",
@@ -24,7 +23,8 @@ visauto_assemblies = [
 
 for asm in visauto_assemblies :
     print "Loading", asm
-    clr.AddReferenceToFileAndPath( os.path.join(visauto_path, asm ) )
+    asm_full_path = os.path.join(visauto_dll_path, asm )
+    clr.AddReferenceToFileAndPath( asm_full_path )
 
 import VisioAutomation
 import VisioAutomation.DocumentAnalysis
