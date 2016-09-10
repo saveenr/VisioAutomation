9387d5b16257aa809818c831cd65d0912d152f7f
Saveen Reddy <saveenr@live.com>
2016-09-10 14:03:56 -0700

    Made the code for nuget download more reusable

--- VisioAutomation_2010/DemoIronPython/download_visioautomation_nuget.ps1
+++ VisioAutomation_2010/DemoIronPython/download_visioautomation_nuget.ps1
@@ -1,8 +1,4 @@
-$scriptpath = $PSScriptRoot
 
-
-$pkgs_folder = Join-Path $scriptpath "packages"
-$pkgs_va_folder = Join-Path $pkgs_folder  "VisioAutomation"
 
 
 function makedirsafe ($p)
@@ -14,22 +10,33 @@ function makedirsafe ($p)
 
 }
 
-makedirsafe $pkgs_folder
+function download_nuget_package( $packagename , $url )
+{ 
+    $scriptpath = $PSScriptRoot
 
-if (Test-Path $pkgs_va_folder)
-{
-    Remove-Item -Recurse -Force $pkgs_va_folder
-}
+    $pkgs_folder = Join-Path $scriptpath "packages"
+    $pkgs_va_folder = Join-Path $pkgs_folder  $packagename 
+
+
+    makedirsafe $pkgs_folder
+
+    if (Test-Path $pkgs_va_folder)
+    {
+        Remove-Item -Recurse -Force $pkgs_va_folder
+    }
  
-makedirsafe $pkgs_va_folder 
+    makedirsafe $pkgs_va_folder 
 
-$url = "http://www.nuget.org/api/v2/package/VisioAutomation2010"
-$zipfile = Join-Path $pkgs_folder  "VisioAutomation2010.zip"
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $zipfile)
+    $zipfile = Join-Path $pkgs_folder  ( $packagename  + ".zip" )
+    $wc = New-Object System.Net.WebClient
+    $wc.DownloadFile($url, $zipfile)
 
 
-[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
-[System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $pkgs_va_folder)
-Remove-Item $zipfile
+    [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $pkgs_va_folder)
+    Remove-Item $zipfile
 
+}
+
+
+download_nuget_package "VisioAutomation" "http://www.nuget.org/api/v2/package/VisioAutomation2010"
