4612394f6679b9d49c03fdef41f1a9491d1222c1
Saveen Reddy <saveenr@live.com>
2016-09-12 04:34:25 -0700

    part of last checkin

--- VisioAutomation_2010/DemoIronPython/visio.py
+++ VisioAutomation_2010/DemoIronPython/visio.py
@@ -47,4 +47,4 @@ import VisioAutomation.Scripting
 
 context = VisioAutomation.Scripting.DefaultContext()
 client = VisioAutomation.Scripting.Client(None,context)
-client.VerboseLogging = False
+client.VerboseLogging = Falses
