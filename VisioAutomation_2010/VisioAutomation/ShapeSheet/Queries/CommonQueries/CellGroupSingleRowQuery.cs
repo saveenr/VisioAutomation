3cd55b9075248a7e1804b85475b488cba4241ea4
Saveen Reddy <saveenr@live.com>
2016-09-17 14:38:09 -0700

    Refactored CellGroupQuery classes

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/Queries/CommonQueries/CellGroupSingleRowQuery.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/Queries/CommonQueries/CellGroupSingleRowQuery.cs
@@ -5,16 +5,25 @@ using IVisio=Microsoft.Office.Interop.Visio;
 
 namespace VisioAutomation.ShapeSheet.Queries.CommonQueries
 {
-    public abstract class CellGroupSingleRowQuery<TCellGroup,TResult>
+    public abstract class CellGroupQuery<TCellGroup, TResult>
     {
         protected VisioAutomation.ShapeSheet.Queries.Query query;
 
-        protected CellGroupSingleRowQuery()
+        protected CellGroupQuery()
         {
             this.query = new Query();
         }
 
-        void verify_singlerow_query()
+        protected abstract void validate_query();
+
+        public abstract TCellGroup CellDataToCellGroup(ShapeSheet.CellData<TResult>[] row);
+
+    }
+
+    public abstract class CellGroupSingleRowQuery<TCellGroup,TResult>: CellGroupQuery<TCellGroup, TResult>
+    {
+
+        protected override void validate_query()
         {
             if (this.query.Cells.Count < 1)
             {
@@ -31,7 +40,7 @@ namespace VisioAutomation.ShapeSheet.Queries.CommonQueries
             Microsoft.Office.Interop.Visio.Page page,
             IList<int> shapeids)
         {
-            verify_singlerow_query();
+            validate_query();
 
             var surface = new ShapeSheetSurface(page);
             var data_for_shapes = this.query.GetFormulasAndResults<TResult>(surface, shapeids);
@@ -44,48 +53,35 @@ namespace VisioAutomation.ShapeSheet.Queries.CommonQueries
         public TCellGroup GetCells(
             Microsoft.Office.Interop.Visio.Shape shape)
         {
-            verify_singlerow_query();
-
+            validate_query();
             var surface = new ShapeSheetSurface(shape);
             var data_for_shape = this.query.GetFormulasAndResults<TResult>(surface);
             var cells = this.CellDataToCellGroup(data_for_shape.Cells);
             return cells;
         }
-
-        public abstract TCellGroup CellDataToCellGroup(ShapeSheet.CellData<TResult>[] row);
     }
 
 
-    public abstract class CellGroupMultiRowQuery<TCellGroup, TResult>
+    public abstract class CellGroupMultiRowQuery<TCellGroup, TResult> : CellGroupQuery<TCellGroup, TResult>
     {
-        protected VisioAutomation.ShapeSheet.Queries.Query query;
-
-        protected CellGroupMultiRowQuery()
+        protected override void validate_query()
         {
-            this.query = new Query();
-        }
-
-
-
-        private void verify_multirow_query(Query query)
-        {
-            if (query.Cells.Count != 0)
+            if (this.query.Cells.Count != 0)
             {
                 throw new InternalAssertionException("Query should not contain any cells");
             }
 
-            if (query.SubQueries.Count != 1)
+            if (this.query.SubQueries.Count != 1)
             {
                 throw new InternalAssertionException("Query should contain contain exactly 1 subquery");
             }
         }
 
-
         public List<List<TCellGroup>> GetCells(
             IVisio.Page page,
             IList<int> shapeids)
         {
-            this.verify_multirow_query(query);
+            this.validate_query();
 
             var surface = new ShapeSheetSurface(page);
             var data_for_shapes = query.GetFormulasAndResults<TResult>(surface, shapeids);
@@ -95,30 +91,22 @@ namespace VisioAutomation.ShapeSheet.Queries.CommonQueries
             return list;
         }
 
-        public List<TCellGroup> GetCells(
-            IVisio.Shape shape)
+        public List<TCellGroup> GetCells(IVisio.Shape shape)
         {
-            this.verify_multirow_query(query);
-
+            this.validate_query();
             var surface = new ShapeSheetSurface(shape);
             var data_for_shape = query.GetFormulasAndResults<TResult>(surface);
             var sec = data_for_shape.Sections[0];
-            var sec_objects = this.SectionRowsToObjects(sec);
-
-            return sec_objects;
+            var cellgroups = this.SectionRowsToObjects(sec);
+            return cellgroups;
         }
 
-        private List<TCellGroup> SectionRowsToObjects(
-            VisioAutomation.ShapeSheet.Queries.Outputs.SubQueryOutput<ShapeSheet.CellData<TResult>> sec)
+        private List<TCellGroup> SectionRowsToObjects(VisioAutomation.ShapeSheet.Queries.Outputs.SubQueryOutput<ShapeSheet.CellData<TResult>> subquery_output)
         {
-            var sec_objects = new List<TCellGroup>(sec.Rows.Count);
-            var objects = sec.Rows.Select(row => this.CellDataToCellGroup(row.Cells));
-            sec_objects.AddRange(objects);
-            return sec_objects;
+            var list_celldata = subquery_output.Rows.Select(row => this.CellDataToCellGroup(row.Cells));
+            var cellgroups = new List<TCellGroup>(subquery_output.Rows.Count);
+            cellgroups.AddRange(list_celldata);
+            return cellgroups;
         }
-
-        public abstract TCellGroup CellDataToCellGroup(ShapeSheet.CellData<TResult>[] row);
-
     }
-
 }
