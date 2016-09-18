c1436e9398bbeb98c69e97a0ce322197ea238cdf
Saveen Reddy <saveenr@live.com>
2016-09-17 22:46:30 -0700

    internal method rename

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/CellGroups/Queries/CellGroupMultiRowQuery.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/CellGroups/Queries/CellGroupMultiRowQuery.cs
@@ -28,7 +28,7 @@ namespace VisioAutomation.ShapeSheet.CellGroups.Queries
             var surface = new ShapeSheetSurface(page);
             var data_for_shapes = query.GetFormulasAndResults<TResult>(surface, shapeids);
             var list = new List<List<TCellGroup>>(shapeids.Count);
-            var objects = data_for_shapes.Select(d => this.SectionRowsToObjects(d.Sections[0]));
+            var objects = data_for_shapes.Select(d => this.SubQueryRowsToCellGroups(d.Sections[0]));
             list.AddRange(objects);
             return list;
         }
@@ -39,11 +39,11 @@ namespace VisioAutomation.ShapeSheet.CellGroups.Queries
             var surface = new ShapeSheetSurface(shape);
             var data_for_shape = query.GetFormulasAndResults<TResult>(surface);
             var sec = data_for_shape.Sections[0];
-            var cellgroups = this.SectionRowsToObjects(sec);
+            var cellgroups = this.SubQueryRowsToCellGroups(sec);
             return cellgroups;
         }
 
-        private List<TCellGroup> SectionRowsToObjects(VisioAutomation.ShapeSheet.Queries.Outputs.SubQueryOutput<ShapeSheet.CellData<TResult>> subquery_output)
+        private List<TCellGroup> SubQueryRowsToCellGroups(VisioAutomation.ShapeSheet.Queries.Outputs.SubQueryOutput<ShapeSheet.CellData<TResult>> subquery_output)
         {
             var list_celldata = subquery_output.Rows.Select(row => this.CellDataToCellGroup(row.Cells));
             var cellgroups = new List<TCellGroup>(subquery_output.Rows.Count);
