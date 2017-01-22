d1d7aa0b37bd2243179f0942efb0d60db435d659
Saveen Reddy <saveenr@live.com>
2017-01-22 11:01:36 -0800

    Update PIA nuget build

--- NuGet_PIAs/build_pia_packashes.cmd
+++ NuGet_PIAs/build_pia_packashes.cmd
@@ -1,3 +1,3 @@
-nuget.exe pack NuGet_Visio2010PrimaryInteropAssembly\Visio2010.PrimaryInteropAssembly.nuspec
-nuget.exe pack NuGet_Visio2013PrimaryInteropAssembly\Visio2013.PrimaryInteropAssembly.nuspec
+"%~dp0nuget.exe" pack Visio2010PIA\Visio2010.PrimaryInteropAssembly.nuspec
+"%~dp0nuget.exe" pack Visio2013PIA\Visio2013.PrimaryInteropAssembly.nuspec
 
