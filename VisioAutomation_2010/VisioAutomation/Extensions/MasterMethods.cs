d75a1d448e248a8eafbfef60c5263c6fc5cc6e84
Saveen Reddy <saveenr@live.com>
2017-02-21 08:19:25 -0800

    Refined extensions methods

--- VisioAutomation_2010/VisioAutomation/Extensions/MasterMethods.cs
+++ VisioAutomation_2010/VisioAutomation/Extensions/MasterMethods.cs
@@ -7,11 +7,8 @@ namespace VisioAutomation.Extensions
     {
         public static Drawing.Rectangle GetBoundingBox(this IVisio.Master master, IVisio.VisBoundingBoxArgs args)
         {
-            // MSDN: http://msdn.microsoft.com/library/default.asp?url=/library/en-us/vissdk11/html/vimthBoundingBox_HV81900422.asp
-            double bbx0, bby0, bbx1, bby1;
-            master.BoundingBox((short)args, out bbx0, out bby0, out bbx1, out bby1);
-            var r = new Drawing.Rectangle(bbx0, bby0, bbx1, bby1);
-            return r;
+            var surface = new VisioAutomation.Drawing.DrawingSurface(master);
+            return surface.GetBoundingBox(args);
         }
 
         public static IEnumerable<IVisio.Master> ToEnumerable(this IVisio.Masters masters)
