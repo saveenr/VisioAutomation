d037bdd46095e0af81fd5dc750f04bbbb734a5a7
Saveen Reddy <saveenr@live.com>
2017-03-05 06:26:02 -0800

    cleanup

--- VisioAutomation_2010/VisioAutomation/Utilities/ArraySegment.cs
+++ VisioAutomation_2010/VisioAutomation/Utilities/ArraySegment.cs
@@ -4,13 +4,13 @@ namespace VisioAutomation.Utilities
 {
     public struct ArraySegment<T> : IEnumerable<T>
     {
-        private readonly T[] Array;
+        private readonly T[] _array;
         private readonly int _offset;
         private readonly int _length;
 
         public ArraySegment(T[] array, int offset, int length)
         {
-            this.Array = array;
+            this._array = array;
             this._offset = offset;
             this._length = length;
         }
@@ -18,18 +18,20 @@ namespace VisioAutomation.Utilities
         public T this[int index]
         {
             get { return get_value_at_index(index); }
-            set
-            {
-                validate_index(index);
-                this.Array[this._offset + index] = value;
-            }
+            set { set_value_at_index(index, value); }
+        }
+
+        private void set_value_at_index(int index, T value)
+        {
+            validate_index(index);
+            this._array[this._offset + index] = value;
         }
 
         private T get_value_at_index(int index)
         {
             validate_index(index);
 
-            var value = this.Array[this._offset + index];
+            var value = this._array[this._offset + index];
             return value;
         }
 
@@ -45,7 +47,7 @@ namespace VisioAutomation.Utilities
         {
             for (int i = 0; i < this._length; i++)
             {
-                yield return this.Array[_offset + i];
+                yield return this._array[_offset + i];
             }
         }
 
