63286d9828cde31455eee891b7dc2fb1f740dc2e
Saveen Reddy <saveenr@live.com>
2017-03-07 00:34:22 -0800

    part of last commit

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/Streams/SidSrcStreamBuilder.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/Streams/SidSrcStreamBuilder.cs
@@ -1,4 +1,6 @@
 
+using System.Collections.Generic;
+
 namespace VisioAutomation.ShapeSheet.Streams
 {
     public class SidSrcStreamBuilder : StreamBuilder<SidSrc>
@@ -15,8 +17,24 @@ namespace VisioAutomation.ShapeSheet.Streams
 
         protected override StreamArray build_stream()
         {
-            var short_array = SidSrc.ToStream(this._items);
+            var short_array = ToStream(this._items);
             return new StreamArray(short_array, Streams.StreamType.SidSrc, this.Count);
+        }
+
+        private short[] ToStream(IList<SidSrc> sidsrcs)
+        {
+            const int sidsrc_length = 4;
+            var sidsrcstream = new short[sidsrc_length * sidsrcs.Count];
+            for (int i = 0; i < sidsrcs.Count; i++)
+            {
+                var sidsrc = sidsrcs[i];
+                int pos = i * sidsrc_length;
+                sidsrcstream[pos + 0] = sidsrc.ShapeID;
+                sidsrcstream[pos + 1] = sidsrc.Src.Section;
+                sidsrcstream[pos + 2] = sidsrc.Src.Row;
+                sidsrcstream[pos + 3] = sidsrc.Src.Cell;
+            }
+            return sidsrcstream;
         }
     }
 }
--- VisioAutomation_2010/VisioAutomation/ShapeSheet/Streams/SrcStreamBuilder.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/Streams/SrcStreamBuilder.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace VisioAutomation.ShapeSheet.Streams
 {
     public class SrcStreamBuilder : StreamBuilder<Src>
@@ -14,8 +16,23 @@ namespace VisioAutomation.ShapeSheet.Streams
 
         protected override StreamArray build_stream()
         {
-            var short_array = Src.ToStream(this._items);
+            var short_array = ToStream(this._items);
             return new StreamArray(short_array, Streams.StreamType.Src, this.Count);
+        }
+
+        private short[] ToStream(IList<Src> srcs)
+        {
+            const int src_length = 3;
+            var src_stream = new short[src_length * srcs.Count];
+            for (int i = 0; i < srcs.Count; i++)
+            {
+                var sidsrc = srcs[i];
+                int pos = i * src_length;
+                src_stream[pos + 0] = sidsrc.Section;
+                src_stream[pos + 1] = sidsrc.Row;
+                src_stream[pos + 2] = sidsrc.Cell;
+            }
+            return src_stream;
         }
     }
 }
