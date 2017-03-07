d9bac185a8753a015613711f52443cfd8c17383a
Saveen Reddy <saveenr@live.com>
2017-03-07 00:30:24 -0800

    moved code

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/SIDSRC.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/SIDSRC.cs
@@ -19,21 +19,5 @@ namespace VisioAutomation.ShapeSheet
         {
             return string.Format("{0}({1},{2},{3},{4})", nameof(SidSrc),this.ShapeID, this.Src.Section, this.Src.Row, this.Src.Cell);
         }
-
-        public static short [] ToStream(IList<SidSrc> sidsrcs)
-        {
-            const int sidsrc_length = 4;
-            var sidsrcstream = new short[sidsrc_length*sidsrcs.Count];
-            for (int i = 0; i < sidsrcs.Count; i++)
-            {
-                var sidsrc = sidsrcs[i];
-                int pos = i*sidsrc_length;
-                sidsrcstream[pos + 0] = sidsrc.ShapeID;
-                sidsrcstream[pos + 1] = sidsrc.Src.Section;
-                sidsrcstream[pos + 2] = sidsrc.Src.Row;
-                sidsrcstream[pos + 3] = sidsrc.Src.Cell;
-            }
-            return sidsrcstream;
-        }
     }
 }
--- VisioAutomation_2010/VisioAutomation/ShapeSheet/SRC.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/SRC.cs
@@ -38,20 +38,5 @@ namespace VisioAutomation.ShapeSheet
             // Src that has a different row index. Very common scenario
             return new Src(this.Section, row, this.Cell);
         }
-
-        public static short[] ToStream(IList<Src> srcs)
-        {
-            const int src_length = 3;
-            var src_stream = new short[src_length * srcs.Count];
-            for (int i = 0; i < srcs.Count; i++)
-            {
-                var sidsrc = srcs[i];
-                int pos = i * src_length;
-                src_stream[pos + 0] = sidsrc.Section;
-                src_stream[pos + 1] = sidsrc.Row;
-                src_stream[pos + 2] = sidsrc.Cell;
-            }
-            return src_stream;
-        }
     }
 }
