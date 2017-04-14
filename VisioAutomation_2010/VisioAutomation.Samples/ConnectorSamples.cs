df6274d236d03a9a45f21209390937d90ae42486
Saveen Reddy <saveenr@live.com>
2017-04-13 22:43:59 -0700

    Fixed a bug in the samples

--- VisioAutomation_2010/VisioAutomation.Samples/ConnectorSamples.cs
+++ VisioAutomation_2010/VisioAutomation.Samples/ConnectorSamples.cs
@@ -12,27 +12,16 @@ namespace VisioAutomationSamples
             var doc = SampleEnvironment.Application.ActiveDocument;
             var pages = doc.Pages;
             var page = pages.Add();
-
-            // get the data and the labels to use
-            var data = new double[] {1, 2, 3, 4, 5, 6};
-
-            var radius = 3.0;
-            var center = new VA.Drawing.Point(4, 4);
-            var slices = VA.Models.Charting.PieSlice.GetSlicesFromValues(center, radius, data);
-            foreach (var slice in slices)
-            {
-                slice.Render(page);
-            }
-
+            
             // based on this example: http://www.visguy.com/2009/06/17/send-all-connectors-to-back/
 
             var stencil = SampleEnvironment.Application.Documents.OpenStencil("basic_u.vss");
             var master = stencil.Masters["Dynamic Connector"];
-            var connector = page.Drop(master, 0, 0);
+            var r0 = page.DrawRectangle(3,3,5,5);
             var r1 = page.DrawRectangle(0, 1, 2, 2);
             var r2 = page.DrawRectangle(7, 7, 8, 8);
-
-            ConnectorHelper.ConnectShapes(connector, r1, r2);
+            var connector = page.Drop(master, 0, 0);
+            ConnectorHelper.ConnectShapes(r1, r2, connector);
 
             var con_layer = page.Layers["Connector"];
 
