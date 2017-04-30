141f3ecbe471dff5441cfc31932c93e896a49ac6
Saveen Reddy <saveenr@live.com>
2017-04-30 14:12:49 -0700

    internal cleanup

--- VisioAutomation_2010/VisioAutomation/Utilities/Convert.cs
+++ VisioAutomation_2010/VisioAutomation/Utilities/Convert.cs
@@ -2,8 +2,8 @@
 {
     public static class Convert
     {
-        private const string quote = "\"";
-        private const string quotequote = "\"\"";
+        private const string doublequote = "\"";
+        private const string doublequote_x2 = "\"\"";
 
         public static short BoolToShort(bool b)
         {
@@ -47,7 +47,7 @@
                 return s;
             }
 
-            string result = string.Format("\"{0}\"", s.Replace(Convert.quote, Convert.quotequote));
+            string result = string.Format("\"{0}\"", s.Replace(Convert.doublequote, Convert.doublequote_x2));
             return result;
         }
 
@@ -63,14 +63,14 @@
 
             // Check if this formula value is a quoted string.
             // If it is, remove extra quote characters.
-            if (output_string.StartsWith(Convert.quote) &&
-                output_string.EndsWith(Convert.quote))
+            if (output_string.StartsWith(Convert.doublequote) &&
+                output_string.EndsWith(Convert.doublequote))
             {
 
                 // Remove the wrapping quote characters as well as any
                 // extra quote marks in the body of the string.
                 output_string = output_string.Substring(1, (output_string.Length - 2));
-                output_string = output_string.Replace(Convert.quotequote, Convert.quote);
+                output_string = output_string.Replace(Convert.doublequote_x2, Convert.doublequote);
             }
 
             return output_string;
