6cd11534c793bb7ca14f1f1c9f8654dd561b2b1a
Saveen Reddy <saveenr@live.com>
2017-05-13 22:53:46 -0700

    namespace cleanup

--- VisioAutomation_2010/VisioAutomation_Documentation_Samples/CustomProperties_Samples.cs
+++ VisioAutomation_2010/VisioAutomation_Documentation_Samples/CustomProperties_Samples.cs
@@ -1,5 +1,4 @@
-﻿using VisioAutomation.Shapes;
-using IVisio = Microsoft.Office.Interop.Visio;
+﻿using IVisio = Microsoft.Office.Interop.Visio;
 
 namespace VisioAutomation_Documentation_Samples
 {
@@ -11,17 +10,17 @@ namespace VisioAutomation_Documentation_Samples
 
             var page = doc.Pages.Add();
             var s1 = page.DrawRectangle(0, 0, 1, 1);
-            var cp = new CustomPropertyCells();
+            var cp = new VisioAutomation.Shapes.CustomPropertyCells();
             cp.Value = "Hello World";
-            CustomPropertyHelper.Set(s1, "Propname", cp);
+            VisioAutomation.Shapes.CustomPropertyHelper.Set(s1, "Propname", cp);
 
             // Retrieve all the Custom properties from a shape
 
-            var props = CustomPropertyHelper.Get(s1);
+            var props = VisioAutomation.Shapes.CustomPropertyHelper.Get(s1);
 
             // Delete the property from the shape
 
-            CustomPropertyHelper.Delete(s1, "Propname");
+            VisioAutomation.Shapes.CustomPropertyHelper.Delete(s1, "Propname");
 
             //cleanup
             page.Delete(0);
@@ -35,22 +34,22 @@ namespace VisioAutomation_Documentation_Samples
             var s1 = page.DrawRectangle(0, 0, 1, 1);
             var s2 = page.DrawRectangle(2, 2, 4, 4);
 
-            var cp1 = new CustomPropertyCells();
+            var cp1 = new VisioAutomation.Shapes.CustomPropertyCells();
             cp1.Value = "Hello";
-            CustomPropertyHelper.Set(s1, "Propname", cp1);
+            VisioAutomation.Shapes.CustomPropertyHelper.Set(s1, "Propname", cp1);
 
-            var cp2 = new CustomPropertyCells();
+            var cp2 = new VisioAutomation.Shapes.CustomPropertyCells();
             cp2.Value = "World";
-            CustomPropertyHelper.Set(s2, "Propname", cp2);
+            VisioAutomation.Shapes.CustomPropertyHelper.Set(s2, "Propname", cp2);
 
             // Retrieve all the Custom properties from multiple shapes
 
             var shapes = new[] {s1, s2};
-            var props = CustomPropertyHelper.Get(page,shapes);
+            var props = VisioAutomation.Shapes.CustomPropertyHelper.Get(page,shapes);
 
             // Delete the properties from the shapes
-            CustomPropertyHelper.Delete(s1, "Propname");
-            CustomPropertyHelper.Delete(s2, "Propname");
+            VisioAutomation.Shapes.CustomPropertyHelper.Delete(s1, "Propname");
+            VisioAutomation.Shapes.CustomPropertyHelper.Delete(s2, "Propname");
 
             //cleanup
             page.Delete(0);
@@ -64,12 +63,12 @@ namespace VisioAutomation_Documentation_Samples
             var page = doc.Pages.Add();
             var s1 = page.DrawRectangle(0, 0, 1, 1);
 
-            var cp1 = new CustomPropertyCells();
+            var cp1 = new VisioAutomation.Shapes.CustomPropertyCells();
             cp1.Value = "Hello";
-            CustomPropertyHelper.Set(s1, "Propname", cp1);
+            VisioAutomation.Shapes.CustomPropertyHelper.Set(s1, "Propname", cp1);
 
-            int num_custom_props = CustomPropertyHelper.GetCount(s1);
-            var custom_prop_names = CustomPropertyHelper.GetNames(s1);
+            int num_custom_props = VisioAutomation.Shapes.CustomPropertyHelper.GetCount(s1);
+            var custom_prop_names = VisioAutomation.Shapes.CustomPropertyHelper.GetNames(s1);
 
             //cleanup
             page.Delete(0);
