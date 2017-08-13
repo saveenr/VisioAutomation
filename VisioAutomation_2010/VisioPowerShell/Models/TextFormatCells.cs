89b4ebe159e803c6c5138a36d45dc13dae9c29fa
Saveen Reddy <saveenr@live.com>
2017-08-13 13:02:10 -0700

    Cleanup

--- VisioAutomation_2010/VisioPowerShell/Models/TextFormatCells.cs
+++ VisioAutomation_2010/VisioPowerShell/Models/TextFormatCells.cs
@@ -43,26 +43,4 @@ namespace VisioPowerShell.Models
             yield return new CellTuple(nameof(SrcConstants.CharStrikethru), SrcConstants.CharStrikethru, this.CharStrikethru);
         }
     }
-
-    public class TextBlockCells : VisioPowerShell.Models.BaseCells
-    {
-        public string TextXFormAngle;
-        public string TextXFormHeight;
-        public string TextXFormLocPinX;
-        public string TextXFormLocPinY;
-        public string TextXFormPinX;
-        public string TextXFormPinY;
-        public string TextXFormWidth;
-
-        public override IEnumerable<CellTuple> GetCellTuples()
-        {
-            yield return new CellTuple(nameof(SrcConstants.TextXFormAngle), SrcConstants.TextXFormAngle, this.TextXFormAngle);
-            yield return new CellTuple(nameof(SrcConstants.TextXFormHeight), SrcConstants.TextXFormHeight, this.TextXFormHeight);
-            yield return new CellTuple(nameof(SrcConstants.TextXFormLocPinX), SrcConstants.TextXFormLocPinX, this.TextXFormLocPinX);
-            yield return new CellTuple(nameof(SrcConstants.TextXFormLocPinY), SrcConstants.TextXFormLocPinY, this.TextXFormLocPinY);
-            yield return new CellTuple(nameof(SrcConstants.TextXFormPinX), SrcConstants.TextXFormPinX, this.TextXFormPinX);
-            yield return new CellTuple(nameof(SrcConstants.TextXFormPinY), SrcConstants.TextXFormPinY, this.TextXFormPinY);
-            yield return new CellTuple(nameof(SrcConstants.TextXFormWidth), SrcConstants.TextXFormWidth, this.TextXFormWidth);
-        }
-    }
 }
