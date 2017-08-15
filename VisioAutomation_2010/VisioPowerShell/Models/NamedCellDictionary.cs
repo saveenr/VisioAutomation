261ba2659885d89b753080a08a3661d258563a7c
Saveen Reddy <saveenr@live.com>
2017-08-14 22:42:31 -0700

    cleanup

--- VisioAutomation_2010/VisioPowerShell/Models/NamedCellDictionary.cs
+++ VisioAutomation_2010/VisioPowerShell/Models/NamedCellDictionary.cs
@@ -1,38 +1,9 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace VisioPowerShell.Models
 {
     public class NamedCellDictionary : NamedDictionary<VisioAutomation.ShapeSheet.Src>
     {
-        public VisioAutomation.ShapeSheet.Query.ShapeSheetQuery ToQuery(IList<string> cells)
-        {
-            var invalid_names = cells.Where(cellname => !this.ContainsKey(cellname)).ToList();
-
-            if (invalid_names.Count > 0)
-            {
-                string msg = "Invalid cell names: " + string.Join(",", invalid_names);
-                throw new ArgumentException(msg);
-            }
-
-            var query = new VisioAutomation.ShapeSheet.Query.ShapeSheetQuery();
-
-            foreach (string cell in cells)
-            {
-                foreach (var resolved_cellname in this.ExpandKeyWildcard(cell))
-                {
-                    if (!query.Cells.Contains(resolved_cellname))
-                    {
-                        var resolved_src = this[resolved_cellname];
-                        query.AddCell(resolved_src, resolved_cellname);
-                    }
-                }
-            }
-
-            return query;
-        }
-
         public static NamedCellDictionary FromCells(BaseCells cells)
         {
             var tuples = cells.GetCellTuples();
