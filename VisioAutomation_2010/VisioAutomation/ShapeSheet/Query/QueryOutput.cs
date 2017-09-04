6b4700c2cebe3c94c6befb927eb282bc6012b7e4
Saveen Reddy <saveenr@live.com>
2017-09-03 23:17:28 -0700

    cleanup

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/Query/QueryOutput.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/Query/QueryOutput.cs
@@ -2,43 +2,33 @@
 
 namespace VisioAutomation.ShapeSheet.Query
 {
-    public class QueryOutput<T> 
+    public class QueryOutputBase<T> 
     {
         public int ShapeID { get; private set; }
-        public VisioAutomation.Utilities.ArraySegment<T> Cells { get; internal set; }
-        public List<SubQueryOutput<T>> Sections { get; internal set; }
 
         public int TotalCellCount;
 
-        internal QueryOutput(int shape_id)
+        internal QueryOutputBase(int shape_id)
         {
             this.ShapeID = shape_id;
         }
     }
 
-    public class QueryOutputCells<T>
+    public class QueryOutputCells<T>: QueryOutputBase<T>
     {
-        public int ShapeID { get; private set; }
         public VisioAutomation.Utilities.ArraySegment<T> Cells { get; internal set; }
 
-        public int TotalCellCount;
-
-        internal QueryOutputCells(int shape_id)
+        internal QueryOutputCells(int shape_id) : base(shape_id)
         {
-            this.ShapeID = shape_id;
         }
     }
 
-    public class QueryOutputSections<T>
+    public class QueryOutputSections<T> : QueryOutputBase<T>
     {
-        public int ShapeID { get; private set; }
         public List<SubQueryOutput<T>> Sections { get; internal set; }
 
-        public int TotalCellCount;
-
-        internal QueryOutputSections(int shape_id)
+        internal QueryOutputSections(int shape_id) : base(shape_id)
         {
-            this.ShapeID = shape_id;
         }
     }
 }
