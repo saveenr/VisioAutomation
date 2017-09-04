b396b877dcc172029bced540aa6da0edb52c3209
Saveen Reddy <saveenr@live.com>
2017-09-04 10:08:35 -0700

    cleanup

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/Query/QueryOutputList.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/Query/QueryOutputList.cs
@@ -2,13 +2,6 @@
 
 namespace VisioAutomation.ShapeSheet.Query
 {
-    public class CellQueryOutputList<T> : List<CellQueryOutput<T>>
-    {
-        internal CellQueryOutputList() : base()
-        {
-        }
-    }
-
     public class SectionQueryOutputList<T> : List<SectionsQueryOutput<T>>
     {
         internal SectionQueryOutputList() : base()
