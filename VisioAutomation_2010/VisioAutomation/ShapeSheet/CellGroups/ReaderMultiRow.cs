53791c7036215fb2fedbdb4f51b8534951188556
Saveen Reddy <saveenr@live.com>
2017-09-04 20:11:04 -0700

    cleanup

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/CellGroups/ReaderMultiRow.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/CellGroups/ReaderMultiRow.cs
@@ -16,17 +16,8 @@ namespace VisioAutomation.ShapeSheet.CellGroups
         
         public abstract TGroup CellDataToCellGroup(VisioAutomation.Utilities.ArraySegment<ShapeSheet.CellData> row);
 
-        protected void validate_query()
-        {
-            if (this.query.SectionQueries.Count != 1)
-            {
-                throw new InternalAssertionException("Query should contain contain exactly 1 subquery");
-            }
-        }
-
         public List<List<TGroup>> GetCellGroups(Microsoft.Office.Interop.Visio.Page page, IList<int> shapeids)
         {
-            this.validate_query();
             var data_for_shapes = query.GetFormulasAndResults(page, shapeids);
             var list = new List<List<TGroup>>(shapeids.Count);
             var objects = data_for_shapes.Select(d => this.SubQueryRowsToCellGroups(d.Sections[0]));
@@ -36,7 +27,6 @@ namespace VisioAutomation.ShapeSheet.CellGroups
 
         public List<TGroup> GetCellGroups(Microsoft.Office.Interop.Visio.Shape shape)
         {
-            this.validate_query();
             var data_for_shape = query.GetFormulasAndResults(shape);
             var sec = data_for_shape.Sections[0];
             var cellgroups = this.SubQueryRowsToCellGroups(sec);
--- VisioAutomation_2010/VisioAutomation/ShapeSheet/CellGroups/ReaderSingleRow.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/CellGroups/ReaderSingleRow.cs
@@ -16,18 +16,8 @@ namespace VisioAutomation.ShapeSheet.CellGroups
 
         public abstract TGroup CellDataToCellGroup(VisioAutomation.Utilities.ArraySegment<ShapeSheet.CellData> row);
 
-        protected void validate_query()
-        {
-            if (this.query.Columns.Count < 1)
-            {
-                throw new InternalAssertionException("Query must contain at least one cell");
-            }
-        }
-
         public List<TGroup> GetCellGroups(IVisio.Page page, IList<int> shapeids)
         {
-            validate_query();
-
             var data_for_shapes = this.query.GetFormulasAndResults(page, shapeids);
             var list = new List<TGroup>(shapeids.Count);
             var objects = data_for_shapes.Select(d => this.CellDataToCellGroup(d.Cells));
@@ -37,7 +27,6 @@ namespace VisioAutomation.ShapeSheet.CellGroups
 
         public TGroup GetCellGroup(IVisio.Shape shape)
         {
-            validate_query();
             var data_for_shape = this.query.GetFormulasAndResults(shape);
             var cells = this.CellDataToCellGroup(data_for_shape.Cells);
             return cells;
