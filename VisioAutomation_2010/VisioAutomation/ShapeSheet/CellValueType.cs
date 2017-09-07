febd4ef96f042c84375d91019aa80b10c99a7f86
Saveen Reddy <saveenr@live.com>
2017-09-07 01:24:17 -0700

    cleanup

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/CellValueType.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/CellValueType.cs
@@ -1,10 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
-using VisioAutomation.Exceptions;
-using VisioAutomation.ShapeSheet.Query;
-using IVisio = Microsoft.Office.Interop.Visio;
-
-
 namespace VisioAutomation.ShapeSheet
 {
     public enum CellValueType
