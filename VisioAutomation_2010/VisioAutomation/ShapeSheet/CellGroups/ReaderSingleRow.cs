d642ec72b04dafb35ef025c2f1024441d043e248
Saveen Reddy <saveenr@live.com>
2017-09-07 17:32:40 -0700

    cleanup

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/CellGroups/ReaderSingleRow.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/CellGroups/ReaderSingleRow.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using VisioAutomation.Exceptions;
-using VisioAutomation.ShapeSheet.Query;
 using IVisio = Microsoft.Office.Interop.Visio;
 
 namespace VisioAutomation.ShapeSheet.CellGroups
