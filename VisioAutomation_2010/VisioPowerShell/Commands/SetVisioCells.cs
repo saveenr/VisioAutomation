2dd3fefc5b64c9999f46fe9d60142c652d71315f
Saveen Reddy <saveenr@live.com>
2017-10-02 19:11:51 -0700

    moved parameters

--- VisioAutomation_2010/VisioPowerShell/Commands/SetVisioCells.cs
+++ VisioAutomation_2010/VisioPowerShell/Commands/SetVisioCells.cs
@@ -11,13 +11,13 @@ namespace VisioPowerShell.Commands
         public VisioPowerShell.Models.BaseCells[] Cells { get; set; }
 
         [SMA.Parameter(Mandatory = false)]
+        public IVisio.Shape[] Shapes { get; set; }
+
+        [SMA.Parameter(Mandatory = false)]
         public SMA.SwitchParameter BlastGuards { get; set; }
 
         [SMA.Parameter(Mandatory = false)]
         public SMA.SwitchParameter TestCircular { get; set; }
-
-        [SMA.Parameter(Mandatory = false)]
-        public IVisio.Shape[] Shapes { get; set; }
 
         protected override void ProcessRecord()
         {
