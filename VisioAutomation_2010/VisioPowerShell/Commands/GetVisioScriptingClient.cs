5c879f56b2dd516a49fc0e8f72f7291daf34f191
Saveen Reddy <saveenr@live.com>
2017-10-07 03:27:05 -0700

    Cleanup of unused Nouns

--- VisioAutomation_2010/VisioPowerShell/Commands/GetVisioScriptingClient.cs
+++ VisioAutomation_2010/VisioPowerShell/Commands/GetVisioScriptingClient.cs
@@ -2,8 +2,8 @@ using SMA = System.Management.Automation;
 
 namespace VisioPowerShell.Commands
 {
-    [SMA.Cmdlet(SMA.VerbsCommon.Get, VisioPowerShell.Commands.Nouns.VisioClient)]
-    public class GetVisioClient : VisioCmdlet
+    [SMA.Cmdlet(SMA.VerbsCommon.Get, VisioPowerShell.Commands.Nouns.VisioScriptingClient)]
+    public class GetVisioScriptingClient : VisioCmdlet
     {
         protected override void ProcessRecord()
         {
--- VisioAutomation_2010/VisioPowerShell/Commands/Nouns.cs
+++ VisioAutomation_2010/VisioPowerShell/Commands/Nouns.cs
@@ -4,41 +4,36 @@ namespace VisioPowerShell.Commands
     {
         public const string Visio = nameof(Visio);
         public const string VisioApplication = nameof(VisioApplication);
-
+        public const string VisioConnection = nameof(VisioConnection);
+        public const string VisioConnectionPoint = nameof(VisioConnectionPoint);
+        public const string VisioContainer = nameof(VisioContainer);
+        public const string VisioControl = nameof(VisioControl);
+        public const string VisioCustomProperty = nameof(VisioCustomProperty);
+        public const string VisioDirectedEdge = nameof(VisioDirectedEdge);
+        public const string VisioDocument = nameof(VisioDocument);
+        public const string VisioGroup = nameof(VisioGroup);
+        public const string VisioHyperlink = nameof(VisioHyperlink);
+        public const string VisioLayer = nameof(VisioLayer);
+        public const string VisioLockCells = nameof(VisioLockCells);
+        public const string VisioMaster = nameof(VisioMaster);
+        public const string VisioModel = nameof(VisioModel);
         public const string VisioModelAreaChart = nameof(VisioModelAreaChart);
         public const string VisioModelBarChart = nameof(VisioModelBarChart);
         public const string VisioModelDirectedGraph = nameof(VisioModelDirectedGraph);
         public const string VisioModelGrid = nameof(VisioModelGrid);
         public const string VisioModelOrgChart = nameof(VisioModelOrgChart);
         public const string VisioModelPieChart = nameof(VisioModelPieChart);
-
-        public const string VisioShapeCells = nameof(VisioShapeCells);
-        public const string VisioPageCells = nameof(VisioPageCells);
-
-        public const string VisioClient = nameof(VisioClient);
-        public const string VisioConnection = nameof(VisioConnection);
-        public const string VisioConnectionPoint = nameof(VisioConnectionPoint);
-        public const string VisioContainer = nameof(VisioContainer);
-        public const string VisioControl = nameof(VisioControl);
-        public const string VisioHyperlink = nameof(VisioHyperlink);
-        public const string VisioCustomProperty = nameof(VisioCustomProperty);
-        public const string VisioDirectedEdge = nameof(VisioDirectedEdge);
-        public const string VisioDocument = nameof(VisioDocument);
-        public const string VisioGroup = nameof(VisioGroup);
-        public const string VisioLayer = nameof(VisioLayer);
-        public const string VisioMaster = nameof(VisioMaster);
-        public const string VisioModel = nameof(VisioModel);
         public const string VisioPage = nameof(VisioPage);
-        public const string VisioPageCell = nameof(VisioPageCell);
+        public const string VisioPageCells = nameof(VisioPageCells);
         public const string VisioPageLayout = nameof(VisioPageLayout);
+        public const string VisioScriptingClient = nameof(VisioScriptingClient);
         public const string VisioSelectedShapes = nameof(VisioSelectedShapes);
         public const string VisioSelectionAsHtml = nameof(VisioSelectionAsHtml);
         public const string VisioShape = nameof(VisioShape);
-        public const string VisioShapeSheet = nameof(VisioShapeSheet);
+        public const string VisioShapeCells = nameof(VisioShapeCells);
         public const string VisioText = nameof(VisioText);
         public const string VisioUserDefinedCell = nameof(VisioUserDefinedCell);
         public const string VisioWindow = nameof(VisioWindow);
         public const string VisioZoom = nameof(VisioZoom);
-        public const string VisioLockCells = nameof(VisioLockCells);
     }
 }
