0e7d213a64a279bcd82a2dc8fa86264495fcb8ef
Saveen Reddy <saveenr@live.com>
2017-10-08 19:56:37 -0700

    Set-VisioWindow now handles X & Y positions

--- VisioAutomation_2010/VisioPowerShell/Commands/SetVisioWindowSize.cs
+++ VisioAutomation_2010/VisioPowerShell/Commands/SetVisioWindowSize.cs
@@ -5,20 +5,46 @@ namespace VisioPowerShell.Commands
     [SMA.Cmdlet(SMA.VerbsCommon.Set, VisioPowerShell.Commands.Nouns.VisioWindow)]
     public class SetVisioWindow : VisioCmdlet
     {
-        [SMA.Parameter(Position = 0, Mandatory = true)]
-        public int Width { get; set; }
+        [SMA.Parameter(Position = 0)]
+        public int Width = -1;
 
-        [SMA.Parameter(Position = 1, Mandatory = true)]
-        public int Height { get; set; }
-        
+        [SMA.Parameter(Position = 1)]
+        public int Height = -1;
+
+        [SMA.Parameter(Position = 2)]
+        public int X = -1;
+
+        [SMA.Parameter(Position = 3)]
+        public int Y = -1;
+
         protected override void ProcessRecord()
         {
             if (this.Width > 0 || this.Height > 0)
             {
-                var w = this.Client.Window;
                 var old_rect = this.Client.Window.GetRectangle();
-                var new_rect = new System.Drawing.Rectangle(old_rect.X, old_rect.Y, this.Width,this.Height);
-                w.SetRectangle(new_rect);
+                var new_rect = old_rect;
+
+                if (this.Width > 0)
+                {
+                    new_rect.Width = this.Width;
+                }
+
+                if (this.Height > 0)
+                {
+                    new_rect.Height = this.Height;
+                }
+
+                if (this.X >= 0)
+                {
+                    new_rect.X = this.X;
+                }
+
+                if (this.Y >= 0)
+                {
+                    new_rect.Y = this.Y;
+                }
+
+                this.Client.Window.SetRectangle(new_rect);
             }
         }
     }
