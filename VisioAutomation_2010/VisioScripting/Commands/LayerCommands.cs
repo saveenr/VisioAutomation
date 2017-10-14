2945f9a186b443fe596145b2add610ff482b08b4
Saveen Reddy <saveenr@live.com>
2017-10-13 19:56:35 -0700

    cleanup

--- VisioAutomation_2010/VisioScripting/Commands/LayerCommands.cs
+++ VisioAutomation_2010/VisioScripting/Commands/LayerCommands.cs
@@ -12,33 +12,32 @@ namespace VisioScripting.Commands
 
         }
 
-        public IVisio.Layer Get(string layername)
+        public IVisio.Layer Get(string name)
         {
-            var cmdtarget = this._client.GetCommandTarget( CommandTargetFlags.Application | CommandTargetFlags.ActiveDocument);
+            var cmdtarget = this._client.GetCommandTarget( CommandTargetFlags.Application | CommandTargetFlags.ActiveDocument | CommandTargetFlags.ActivePage);
 
 
-            if (layername == null)
+            if (name == null)
             {
-                throw new System.ArgumentNullException(nameof(layername));
+                throw new System.ArgumentNullException(nameof(name));
             }
 
-            if (layername.Length < 1)
+            if (name.Length < 1)
             {
-                throw new System.ArgumentException("Layer name cannot be empty", nameof(layername));
+                throw new System.ArgumentException("Layer name cannot be empty", nameof(name));
             }
 
-            var application = cmdtarget.Application;
-            var page = application.ActivePage;
+            var page = cmdtarget.ActivePage;
             IVisio.Layer layer = null;
             try
             {
-                this._client.Output.WriteVerbose("Trying to find layer named \"{0}\"",layername);
+                this._client.Output.WriteVerbose("Trying to find layer named \"{0}\"",name);
                 var layers = page.Layers;
-                layer = layers.ItemU[layername];
+                layer = layers.ItemU[name];
             }
             catch (System.Runtime.InteropServices.COMException)
             {
-                string msg = string.Format("No layer with name \"{0}\"", layername);
+                string msg = string.Format("No layer with name \"{0}\"", name);
                 throw new VisioAutomation.Exceptions.VisioOperationException(msg);
             }
             return layer;
@@ -46,12 +45,9 @@ namespace VisioScripting.Commands
 
         public List<IVisio.Layer> Get()
         {
-            var cmdtarget = this._client.GetCommandTarget( CommandTargetFlags.Application | CommandTargetFlags.ActiveDocument);
+            var cmdtarget = this._client.GetCommandTarget( CommandTargetFlags.Application | CommandTargetFlags.ActiveDocument | CommandTargetFlags.ActivePage);
 
-            var application = cmdtarget.Application;
-            var page = application.ActivePage;
-
-            return page.Layers.ToList();
+            return cmdtarget.ActivePage.Layers.ToList();
         }
     }
 }
