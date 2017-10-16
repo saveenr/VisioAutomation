83ce887ad10215287f6161bdadcad803436adabe
Saveen Reddy <saveenr@live.com>
2017-10-15 22:19:06 -0700

    VisioPS: added stencil and template parameters to New-VisioDocument

--- VisioAutomation_2010/VisioPowerShell/Commands/NewVisioDocument.cs
+++ VisioAutomation_2010/VisioPowerShell/Commands/NewVisioDocument.cs
@@ -1,11 +1,17 @@
 using SMA = System.Management.Automation;
-
+using IVisio = Microsoft.Office.Interop.Visio;
 
 namespace VisioPowerShell.Commands
 {
     [SMA.Cmdlet(SMA.VerbsCommon.New, VisioPowerShell.Commands.Nouns.VisioDocument)]
     public class NewVisioDocument : VisioCmdlet
     {
+        [SMA.Parameter(Mandatory = false)]
+        public string Stencil { get; set; }
+
+        [SMA.Parameter(Mandatory = false)]
+        public string Template { get; set; }
+
         protected override void ProcessRecord()
         {
             if (!this.Client.Application.HasActiveApplication)
@@ -19,7 +25,23 @@ namespace VisioPowerShell.Commands
                     this.Client.Application.NewActiveApplication();
                 }
             }
-            var doc = this.Client.Document.NewDocument();
+
+            IVisio.Document doc;
+
+            if (string.IsNullOrEmpty(this.Template))
+            {
+                doc = this.Client.Document.NewDocument();
+            }
+            else
+            {
+                doc = this.Client.Document.NewDocumentFromTemplate(this.Template);
+            }
+
+            if (!string.IsNullOrEmpty(this.Stencil))
+            {
+                this.Client.Document.OpenStencilDocument(this.Stencil);
+            }
+
             this.WriteObject(doc);
         }
     }
