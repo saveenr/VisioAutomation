5770ce1ffff1e8a403b0f8f72241ce85539703a8
Saveen Reddy <saveenr@live.com>
2017-11-04 10:15:42 -0700

    improved exception messages

--- VisioAutomation_2010/VisioScripting/Commands/CommandTarget.cs
+++ VisioAutomation_2010/VisioScripting/Commands/CommandTarget.cs
@@ -6,13 +6,9 @@ namespace VisioScripting.Commands
     public class CommandTarget
     {
         public readonly Client Client;
-        public IVisio.Application Application;
-        public IVisio.Document ActiveDocument;
-        public IVisio.Page ActivePage;
-
-        public bool HasActiveApplication => this.Application != null;
-        public bool HasActiveDocument => this.ActiveDocument != null;
-        public bool HasActivePage => this.ActivePage != null;
+        public IVisio.Application Application { get; private set; }
+        public IVisio.Document ActiveDocument { get; private set; }
+        public IVisio.Page ActivePage { get; private set; }
 
         public CommandTarget(Client client, CommandTargetFlags flags)
         {
@@ -37,13 +33,15 @@ namespace VisioScripting.Commands
                 var has_app = this.Client.Application.HasActiveApplication;
                 if (!has_app)
                 {
-                    throw new System.ArgumentException("CommandTarget: No Visio Application available");
+                    string msg = string.Format("{0}: No Visio Application available", nameof(CommandTarget));
+                    throw new System.ArgumentException(msg);
                 }
             }
 
             if (require_app && this.Application == null)
             {
-                throw new VisioOperationException("CommandTarget: No Application");
+                string msg = string.Format("{0}: No Application", nameof(CommandTarget));
+                throw new VisioOperationException(msg);
             }
 
             if ((this.ActiveDocument == null) && require_document)
@@ -57,34 +55,36 @@ namespace VisioScripting.Commands
 
                 if (is_drawing)
                 {
-                    this.Client.Output.WriteVerbose("CommandTarget: Verified a drawing is available for use");
+                    this.Client.Output.WriteVerbose("{0}: Verified a drawing is available for use",nameof(CommandTarget));
                 }
                 else
                 {
-                    throw new VisioOperationException("CommandTarget: The Document is not a drawing document. " + errmsg);
+                    string msg = string.Format("{0}: The Document is not a drawing document", nameof(CommandTarget));
+                    throw new VisioOperationException(msg);
                 }
             }
 
             if (this.ActiveDocument == null && require_document)
             {
-                throw new VisioOperationException("CommandTarget: No Document");
+                string msg = string.Format("{0}: No Document", nameof(CommandTarget));
+                throw new VisioOperationException(msg);
             }
 
             if ((this.ActivePage == null) && ((flags & CommandTargetFlags.ActivePage) != 0))
             {
                 if (this.Application == null)
                 {
-                    throw new VisioOperationException(
-                        "CommandTarget: Internal error application should never be null in this case");
+                    string msg = string.Format("{0}: Internal error application should never be null in this case", nameof(CommandTarget));
+                    throw new VisioOperationException(msg);
                 }
                 this.ActivePage = this.Application.ActivePage;
             }
 
             if (this.ActivePage == null && require_page)
             {
-                throw new VisioOperationException("CommandTarget: No Page");
+                string msg = string.Format("{0}: No Page", nameof(CommandTarget));
+                throw new VisioOperationException(msg);
             }
-
         }
 
         public static bool IsDocumentADrawing(IVisio.Application app, IVisio.Document doc, out string errmsg)
@@ -101,8 +101,8 @@ namespace VisioScripting.Commands
 
             if (doc.Application != app)
             {
-                string msg = "application does not match doc.application";
-                new System.ArgumentException(msg);
+                string msg = string.Format("{0}: application does not match doc.application", nameof(CommandTarget));
+                throw new System.ArgumentException(msg);
             }
 
 
@@ -111,7 +111,7 @@ namespace VisioScripting.Commands
             // If there's no active window there can't be an active document
             if (active_window == null)
             {
-                errmsg = "ActiveDocumentIsDrawing: No Active Window";
+                errmsg = string.Format("{0}: No Active Window", nameof(CommandTarget));
                 return false;
             }
 
@@ -124,7 +124,7 @@ namespace VisioScripting.Commands
             // this.Client.Output.WriteVerbose("ActiveDocumentIsDrawing: The Active Window: Type={0} & SybType={1}", active_window_type, active_window.SubType);
             if (!(active_window_type == vis_drawing || active_window_type == vis_master))
             {
-                errmsg = string.Format("ActiveDocumentIsDrawing: The Active Window Type must be one of {0} or {1}", IVisio.VisWinTypes.visDrawing, IVisio.VisWinTypes.visMasterWin);
+                errmsg = string.Format("{0}: The Active Window Type must be one of {1} or {2}", nameof(CommandTarget), IVisio.VisWinTypes.visDrawing, IVisio.VisWinTypes.visMasterWin);
                 return false;
             }
 
@@ -135,7 +135,7 @@ namespace VisioScripting.Commands
                 // 64 means master is being edited
                 if (active_window.SubType != 64)
                 {
-                    errmsg = "ActiveDocumentIsDrawing: Window is not editing a master";
+                    errmsg = string.Format("{0}: Window is not editing a master", nameof(CommandTarget));
                     return false;
                 }
             }
