cb6c3e715604a71fa8a69a75ae0c69c2f41fc0aa
Saveen Reddy <saveenr@microsoft.com>
2019-03-06 14:04:46 -0800

    Moved class to a different namespace

--- VisioAutomation_2010/VisioPowerShell/Commands/VisioPageLayout/NewVisioPageLayout.cs
+++ VisioAutomation_2010/VisioPowerShell/Commands/VisioPageLayout/NewVisioPageLayout.cs
@@ -7,32 +7,32 @@ namespace VisioPowerShell.Commands
     public class NewVisioPageLayout : VisioCmdlet
     {
         [SMA.Parameter(Mandatory = true)]
-        public PageLayoutType LayoutType { get; set; }
+        public VisioPowerShell.Models.PageLayoutType LayoutType { get; set; }
 
 
         protected override void ProcessRecord()
         {
-            if (this.LayoutType == PageLayoutType.FlowChart)
+            if (this.LayoutType == VisioPowerShell.Models.PageLayoutType.FlowChart)
             {
                 var plo = new VisioAutomation.PageLayouts.FlowchartLayout();
                 this.WriteObject(plo);
             }
-            else if (this.LayoutType == PageLayoutType.Hierarchy)
+            else if (this.LayoutType == VisioPowerShell.Models.PageLayoutType.Hierarchy)
             {
                 var plo = new VisioAutomation.PageLayouts.HierarchyLayout();
                 this.WriteObject(plo);
             }
-            else if (this.LayoutType == PageLayoutType.Circular)
+            else if (this.LayoutType == VisioPowerShell.Models.PageLayoutType.Circular)
             {
                 var plo = new VisioAutomation.PageLayouts.CircularLayout();
                 this.WriteObject(plo);
             }
-            else if (this.LayoutType == PageLayoutType.CompactTree)
+            else if (this.LayoutType == VisioPowerShell.Models.PageLayoutType.CompactTree)
             {
                 var plo = new VisioAutomation.PageLayouts.CompactTreeLayout();
                 this.WriteObject(plo);
             }
-            else if (this.LayoutType == PageLayoutType.RadialLayout)
+            else if (this.LayoutType == VisioPowerShell.Models.PageLayoutType.RadialLayout)
             {
                 var plo = new VisioAutomation.PageLayouts.RadialLayout();
                 this.WriteObject(plo);
