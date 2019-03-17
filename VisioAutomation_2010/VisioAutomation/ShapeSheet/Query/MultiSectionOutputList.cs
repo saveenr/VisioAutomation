c31bd878c373926d303004ea25562df10adc0183
Saveen Reddy <saveenr@live.com>
2019-03-17 03:20:15 -0700

    shapesheat cleanup

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/Query/MultiSectionOutputList.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/Query/MultiSectionOutputList.cs
@@ -1,11 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace VisioAutomation.ShapeSheet.Query
 {
-    public class MultiSectionOuputList<T> : List<MultiSectionOutput<T>>
+    public class MultiSectionOuputList<T> : IEnumerable<MultiSectionOutput<T>>
     {
-        internal MultiSectionOuputList() : base()
+        List<MultiSectionOutput<T>> items;
+
+        internal MultiSectionOuputList()
         {
+            this.items = new List<MultiSectionOutput<T>>();
+        }
+
+        public void Add(MultiSectionOutput<T> item)
+        {
+            this.items.Add(item);
+        }
+
+        public IEnumerator<MultiSectionOutput<T>> GetEnumerator()
+        {
+            return this.items.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.items.Count;
+            }
+        }
+
+        public MultiSectionOutput<T> this[int index]
+        {
+            get
+            {
+                return this.items[index];
+            }
         }
     }
 }
