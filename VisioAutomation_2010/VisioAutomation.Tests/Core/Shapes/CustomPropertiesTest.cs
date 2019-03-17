01ebabda4c6af5c7b747cb6c744821676df4a827
Saveen Reddy <saveenr@live.com>
2019-03-17 15:41:11 -0700

    Fixed a sample and added a unit test

--- VisioAutomation_2010/VisioAutomation.Tests/Core/Shapes/CustomPropertiesTest.cs
+++ VisioAutomation_2010/VisioAutomation.Tests/Core/Shapes/CustomPropertiesTest.cs
@@ -22,6 +22,35 @@ namespace VisioAutomation_Tests.Core.Shapes
         }
 
         [TestMethod]
+        public void SimpleCP()
+        {
+            var page1 = this.GetNewPage();
+
+            // Draw a shape
+            var s1 = page1.DrawRectangle(1, 1, 4, 3);
+
+            int cp_type = 0; // string type
+
+            // Set some properties on it
+            CustomPropertyHelper.Set(s1, "FOO1", "\"BAR1\"", cp_type);
+            CustomPropertyHelper.Set(s1, "FOO2", "\"BAR2\"", cp_type);
+            CustomPropertyHelper.Set(s1, "FOO3", "\"BAR3\"", cp_type);
+
+            // Delete one of those properties
+            CustomPropertyHelper.Delete(s1, "FOO2");
+
+            // Set the value of an existing properties
+            CustomPropertyHelper.Set(s1, "FOO3", "\"BAR3updated\"", cp_type);
+
+            // retrieve all the properties
+            var props = CustomPropertyHelper.GetCells(s1, CellValueType.Formula);
+
+            var cp_foo1 = props["FOO1"];
+            // var cp_foo2 = props["FOO2"]; there is no prop called FOO2
+            var cp_foo3 = props["FOO3"];
+        }
+
+        [TestMethod]
         public void CustomProps_CRUD()
         {
             var page1 = this.GetNewPage();
