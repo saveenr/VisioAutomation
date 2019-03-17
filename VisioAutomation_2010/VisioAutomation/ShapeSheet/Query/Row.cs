0ea687e6f17c182aa6ca6ee4775d06b0a974931b
Saveen Reddy <saveenr@live.com>
2019-03-17 11:54:21 -0700

    Added ROwBase class back

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/Query/Row.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/Query/Row.cs
@@ -5,52 +5,23 @@ using System.Linq;
 
 namespace VisioAutomation.ShapeSheet.Query
 {
-    public class Row<T> : IEnumerable<T>
+
+
+    public class Row<T> : RowBase<T>
     {
-        public int ShapeID { get; private set; }
         public readonly IVisio.VisSectionIndices SectionIndex;
         public readonly int RowIndex;
-        private readonly VASS.Internal.ArraySegment<T> Cells;
 
-        internal Row(int shapeid, VASS.Internal.ArraySegment<T>  cells)
+        internal Row(int shapeid, VASS.Internal.ArraySegment<T>  cells) : base(shapeid,cells)
         {
-            this.ShapeID = shapeid;
-            this.Cells = cells;
+            this.SectionIndex = IVisio.VisSectionIndices.visSectionInval;
+            this.RowIndex = -1;
         }
 
-        internal Row(int shapeid, IVisio.VisSectionIndices secindex, int rowindex, VASS.Internal.ArraySegment<T> cells)
+        internal Row(int shapeid, IVisio.VisSectionIndices secindex, int rowindex, VASS.Internal.ArraySegment<T> cells) : base (shapeid,cells)
         {
-            this.ShapeID = shapeid;
             this.SectionIndex = secindex;
             this.RowIndex = rowindex;
-            this.Cells = cells;
-        }
-
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            return this.Cells.GetEnumerator();
-        }
-
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-        {
-            return this.GetEnumerator();
-        }
-
-        public int Count
-        {
-            get
-            {
-                return this.Cells.Count;
-            }
-        }
-
-        public T this[int index]
-        {
-            get
-            {
-                return this.Cells[index];
-            }
         }
     }
 }
---
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/Query/RowBase.cs
@@ -0,0 +1,47 @@
+﻿using VASS = VisioAutomation.ShapeSheet;
+using IVisio = Microsoft.Office.Interop.Visio;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VisioAutomation.ShapeSheet.Query
+{
+
+    public class RowBase<T> : IEnumerable<T>
+    {
+        public int ShapeID { get; private set; }
+        private readonly VASS.Internal.ArraySegment<T> Cells;
+
+        internal RowBase(int shapeid, VASS.Internal.ArraySegment<T> cells)
+        {
+            this.ShapeID = shapeid;
+            this.Cells = cells;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.Cells.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.Cells.Count;
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return this.Cells[index];
+            }
+        }
+    }
+
+}
