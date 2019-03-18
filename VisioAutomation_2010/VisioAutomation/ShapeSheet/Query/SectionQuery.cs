663cb1e2909418ef2b1f46957723c2c6d280e30d
Saveen Reddy <saveenr@live.com>
2019-03-18 08:36:36 -0700

    Merged two classes

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/Query/SectionQuery.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/Query/SectionQuery.cs
@@ -5,12 +5,16 @@ using VASS = VisioAutomation.ShapeSheet;
 
 namespace VisioAutomation.ShapeSheet.Query
 {
-    public class SectionQuery : SectionQueryItems
+    public class SectionQuery : IEnumerable<SectionColumns>
     {
+        private IList<SectionColumns> _list { get; }
+        private readonly Dictionary<IVisio.VisSectionIndices, SectionColumns> _map_secindex_to_sec_cols;
         private SectionQueryCache _cache;
 
         public SectionQuery() : base()
         {
+            this._list = new List<SectionColumns>();
+            this._map_secindex_to_sec_cols = new Dictionary<IVisio.VisSectionIndices, SectionColumns>();
         }
 
         private static void RestrictToShapesOnly(SurfaceTarget surface)
@@ -270,5 +274,39 @@ namespace VisioAutomation.ShapeSheet.Query
             var shapecacheitem = new ShapeCacheItem(sec_cols, row_count, shape.ID16);
             return shapecacheitem;
         }
+
+        public IEnumerator<SectionColumns> GetEnumerator()
+        {
+            return this._list.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public SectionColumns this[int index] => this._list[index];
+
+        public SectionColumns Add(IVisio.VisSectionIndices sec_index)
+        {
+            if (this._map_secindex_to_sec_cols.ContainsKey(sec_index))
+            {
+                string msg = string.Format("Already contains section index {0} (value={1})", sec_index, (int)sec_index);
+                throw new System.ArgumentException(msg);
+            }
+
+            var sec_cols = new SectionColumns(sec_index);
+            this._list.Add(sec_cols);
+            this._map_secindex_to_sec_cols[sec_index] = sec_cols;
+            return sec_cols;
+        }
+
+        public SectionColumns Add(Src src)
+        {
+            return this.Add((IVisio.VisSectionIndices)src.Section);
+        }
+
+        public int Count => this._list.Count;
+
     }
 }
