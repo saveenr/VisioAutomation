e0870aa73e5550e9deac3e941e2f634b1865a244
Saveen Reddy <saveenr@live.com>
2019-03-29 23:40:53 -0700

    visiops - Get-VisioUserDefinedCell and Get-VisioCustomProperty now work similarly

--- VisioAutomation_2010/VisioPowerShell/Commands/VisioUserDefinedCell/GetVisioUserDefinedCell.cs
+++ VisioAutomation_2010/VisioPowerShell/Commands/VisioUserDefinedCell/GetVisioUserDefinedCell.cs
@@ -24,6 +24,23 @@ namespace VisioPowerShell.Commands.VisioUserDefinedCell
                 return;
             }
 
+            foreach (var shape_udcelldic_pair in dicof_shape_to_udcelldic)
+            {
+                var shape = shape_udcelldic_pair.Key;
+                var udcelldic = shape_udcelldic_pair.Value;
+                int shapeid = shape.ID;
+                foreach (var udcellname_udcellcells_pair in udcelldic)
+                {
+                    string udcellname = udcellname_udcellcells_pair.Key;
+                    var udcellcells = udcellname_udcellcells_pair.Value;
+                    string udcell_value = udcellcells.Value.ToString();
+                    string udcell_prompt = udcellcells.Prompt.ToString();
+
+                    var udcell_obj = new VisioPowerShell.Models.UserDefinedCell(shapeid, udcellname, udcell_value, udcell_prompt);
+                    this.WriteObject(udcell_obj);
+                }
+            }
+
             this.WriteObject(dicof_shape_to_udcelldic);
         }
     }
