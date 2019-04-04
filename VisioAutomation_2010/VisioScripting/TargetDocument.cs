7c3df6f9ed31750aae3a53318d5eee9e796c9e58
Saveen Reddy <saveenr@live.com>
2019-04-04 10:44:08 -0700

    moved class to separate file

--- VisioAutomation_2010/VisioScripting/TargetDocument.cs
+++ VisioAutomation_2010/VisioScripting/TargetDocument.cs
@@ -2,31 +2,6 @@ using IVisio = Microsoft.Office.Interop.Visio;
 
 namespace VisioScripting
 {
-    public class TargetObject<T> where  T: class
-    {
-        public readonly T Item;
-        public readonly bool UseContext;
-
-        public TargetObject()
-        {
-            this.Item = null;
-            this.UseContext = true;
-        }
-        
-        public TargetObject(T item)
-        {
-            this.Item = item;
-            this.UseContext = this.Item == null;
-        }
-        public TargetObject(T item, bool isresolved)
-        {
-            this.Item = item;
-            this.UseContext = !isresolved;
-        }
-
-        public bool IsResolved => !this.UseContext;
-    }
-
     public class TargetDocument: TargetObject<IVisio.Document>
     {
         public TargetDocument() :base()
