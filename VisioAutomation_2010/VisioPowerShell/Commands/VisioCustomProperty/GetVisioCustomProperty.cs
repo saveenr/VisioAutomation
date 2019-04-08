a2947289effc92fa05624b05e1da374877e79a7c
Saveen Reddy <saveenr@live.com>
2019-04-08 09:07:40 -0700

    VisioPS - remoned class for CustomProp and switched to using the one in VisioAutomation

--- VisioAutomation_2010/VisioPowerShell/Commands/VisioCustomProperty/GetVisioCustomProperty.cs
+++ VisioAutomation_2010/VisioPowerShell/Commands/VisioCustomProperty/GetVisioCustomProperty.cs
@@ -9,34 +9,12 @@ namespace VisioPowerShell.Commands.VisioCustomProperty
     {
         [SMA.Parameter(Mandatory = false)]
         public IVisio.Shape[] Shapes;
-
-        [SMA.Parameter(Mandatory = false)]
-        public SMA.SwitchParameter GetCells;
-
+        
         protected override void ProcessRecord()
         {
             var targetshapes = new VisioScripting.TargetShapes(this.Shapes);
             var dicof_shape_to_cpdic = this.Client.CustomProperty.GetCustomProperties(targetshapes);
-
-            if (this.GetCells)
-            {
-                this.WriteObject(dicof_shape_to_cpdic);                
-                return;
-            }
-
-            foreach (var shape_cppdic_pair in dicof_shape_to_cpdic)
-            {
-                var shape = shape_cppdic_pair.Key;
-                var cpdic = shape_cppdic_pair.Value;
-                int shapeid = shape.ID;
-                foreach (var cpname_cpcells_pair in cpdic)
-                {
-                    string cpname = cpname_cpcells_pair.Key;
-                    var cpcells = cpname_cpcells_pair.Value;
-                    var cp_obj = new CustomProperty(shapeid, cpname, cpcells);
-                    this.WriteObject(cp_obj);
-                }
-            }
+            this.WriteObject(dicof_shape_to_cpdic);                
         }
     }
 }
--- VisioAutomation_2010/VisioPowerShell/Commands/VisioUserDefinedCell/GetVisioUserDefinedCell.cs
+++ VisioAutomation_2010/VisioPowerShell/Commands/VisioUserDefinedCell/GetVisioUserDefinedCell.cs
@@ -10,36 +10,10 @@ namespace VisioPowerShell.Commands.VisioUserDefinedCell
         [SMA.Parameter(Mandatory = false)]
         public IVisio.Shape[] Shapes;
 
-        [SMA.Parameter(Mandatory = false)]
-        public SMA.SwitchParameter GetCells;
-
         protected override void ProcessRecord()
         {
             var targetshapes = new VisioScripting.TargetShapes(this.Shapes);
             var dicof_shape_to_udcelldic = this.Client.UserDefinedCell.GetUserDefinedCells(targetshapes, CellValueType.Formula);
-
-            if (this.GetCells)
-            {
-                this.WriteObject(dicof_shape_to_udcelldic);
-                return;
-            }
-
-            foreach (var shape_udcelldic_pair in dicof_shape_to_udcelldic)
-            {
-                var shape = shape_udcelldic_pair.Key;
-                var udcelldic = shape_udcelldic_pair.Value;
-                int shapeid = shape.ID;
-                foreach (var udcellname_udcellcells_pair in udcelldic)
-                {
-                    string udcellname = udcellname_udcellcells_pair.Key;
-                    var udcellcells = udcellname_udcellcells_pair.Value;
-                    string udcell_value = udcellcells.Value.ToString();
-                    string udcell_prompt = udcellcells.Prompt.ToString();
-
-                    var udcell_obj = new VisioPowerShell.Models.UserDefinedCell(shapeid, udcellname, udcell_value, udcell_prompt);
-                    this.WriteObject(udcell_obj);
-                }
-            }
 
             this.WriteObject(dicof_shape_to_udcelldic);
         }
