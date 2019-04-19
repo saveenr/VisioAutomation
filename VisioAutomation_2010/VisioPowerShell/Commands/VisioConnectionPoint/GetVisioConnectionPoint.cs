749e9ffea553e758d674d03bc381ff37d1942138
Saveen Reddy <saveenr@live.com>
2019-04-19 02:15:19 -0700

    removed unused class

--- VisioAutomation_2010/VisioPowerShell/Commands/VisioConnectionPoint/GetVisioConnectionPoint.cs
+++ VisioAutomation_2010/VisioPowerShell/Commands/VisioConnectionPoint/GetVisioConnectionPoint.cs
@@ -9,35 +9,15 @@ namespace VisioPowerShell.Commands.VisioConnectionPoint
         [SMA.Parameter(Mandatory = false)]
         public IVisio.Shape[] Shapes;
 
-        [SMA.Parameter(Mandatory = false)]
-        public SMA.SwitchParameter GetCells;
-
         protected override void ProcessRecord()
         {
             var targetshapes = new VisioScripting.TargetShapes(this.Shapes);
 
             var dic = this.Client.ConnectionPoint.GetConnectionPoints(targetshapes);
 
-            if (this.GetCells)
-            {
-                this.WriteObject(dic);
-                return;
-            }
+            this.WriteObject(dic);
+            return;
 
-            foreach (var shape_points in dic)
-            {
-                var shape = shape_points.Key;
-                var points = shape_points.Value;
-
-                int shapeid = shape.ID;
-
-                foreach (var point_cells in points)
-                {
-                    var cp = new Models.ConnectionPoint(shapeid, point_cells);
-                    this.WriteObject(cp);
-                }
-            }
         }
-
     }
 }
