e13de9000e08a8d9ba9ade449cca7049abcad57c
Saveen Reddy <saveenr@microsoft.com>
2019-04-20 18:48:05 -0700

    removed unused member

--- VisioAutomation_2010/VisioPowerShell/Commands/Nouns.cs
+++ VisioAutomation_2010/VisioPowerShell/Commands/Nouns.cs
@@ -16,7 +16,6 @@ namespace VisioPowerShell.Commands
         public const string VisioModel = nameof(VisioModel);
         public const string VisioPage = nameof(VisioPage);
         public const string VisioPageCells = nameof(VisioPageCells);
-        public const string LayoutStyle = nameof(LayoutStyle);
         public const string VisioScriptingClient = nameof(VisioScriptingClient);
         public const string VisioSelection = nameof(VisioSelection);
         public const string VisioShape = nameof(VisioShape);
