f1226c24fff7913c5bee7a74dcefe84021bf850a
Saveen Reddy <saveenr@live.com>
2019-04-25 00:41:41 -0700

    VisioPS - Added count parameter for creating cells

--- VisioAutomation_2010/VisioPowerShell/Commands/VisioShapeCells/NewVisioShapeCells.cs
+++ VisioAutomation_2010/VisioPowerShell/Commands/VisioShapeCells/NewVisioShapeCells.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using SMA = System.Management.Automation;
 
 namespace VisioPowerShell.Commands.VisioShapeCells
@@ -5,10 +8,23 @@ namespace VisioPowerShell.Commands.VisioShapeCells
     [SMA.Cmdlet(SMA.VerbsCommon.New, Nouns.VisioShapeCells)]
     public class NewVisioShapeCells : VisioCmdlet
     {
+        [SMA.Parameter(Mandatory = false)]
+        public int Count=-1;
         protected override void ProcessRecord()
         {
-            var cells = new VisioPowerShell.Models.ShapeCells();
-            this.WriteObject(cells);
+            if (Count < 0)
+            {
+                var cells = new VisioPowerShell.Models.ShapeCells();
+                this.WriteObject(cells);
+            }
+            else
+            {
+                var list_cells = new List<VisioPowerShell.Models.ShapeCells>(this.Count);
+                var indices = Enumerable.Range(0, this.Count);
+                var enum_cells = indices.Select(i => new VisioPowerShell.Models.ShapeCells());
+                list_cells.AddRange(enum_cells);
+                this.WriteObject(list_cells,false);
+            }
         }
     }
 }
