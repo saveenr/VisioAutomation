71bb189b6c1769dda41c7206d053997c2881954f
Saveen Reddy <saveenr@live.com>
2019-06-04 03:50:56 -0700

    removed unused methods

--- VisioAutomation_2010/VisioAutomation.Models/Layouts/DirectedGraph/DirectedGraphLayout.cs
+++ VisioAutomation_2010/VisioAutomation.Models/Layouts/DirectedGraph/DirectedGraphLayout.cs
@@ -25,62 +25,17 @@ namespace VisioAutomation.Models.Layouts.DirectedGraph
             return s0;
         }
 
-        public Connector AddConnection(string id, Shape from, Shape to)
-        {
-            return this.AddConnection(id, from, to, id, ConnectorType.Default);
-        }
-
         public Connector AddConnection(
             string id,
             Shape from,
             Shape to,
             string label,
-             ConnectorType type)
+            ConnectorType type)
         {
             var new_connector = new Connector(from, to);
             new_connector.ID = id;
             new_connector.Label = label;
             new_connector.ConnectorType = type;
-
-            this.Connectors.Add(id, new_connector);
-            return new_connector;
-        }
-
-        public Connector AddConnection(string id, Shape from, Shape to, string label, string stencil_name, string master_name)
-        {
-            var new_connector = new Connector(from, to);
-            new_connector.ID = id;
-            new_connector.Label = label;
-            new_connector.StencilName = stencil_name;
-            new_connector.MasterName = master_name;
-
-            this.Connectors.Add(id, new_connector);
-            return new_connector;
-        }
-
-        public Connector AddConnection(string id, Shape from, Shape to, string label,
-            ConnectorType type, int begin_arrow, int end_arrow, string hyperlink)
-        {
-            var new_connector = new Connector(from, to);
-            new_connector.ID = id;
-            new_connector.Label = label;
-            new_connector.ConnectorType = type;
-            new_connector.Cells = new ShapeCells();
-            new_connector.Cells.LineBeginArrow = begin_arrow;
-            new_connector.Cells.LineBeginArrowSize = begin_arrow;
-            new_connector.Cells.LineEndArrow = end_arrow;
-            new_connector.Cells.LineEndArrowSize = end_arrow;
-
-            if (!string.IsNullOrEmpty(hyperlink))
-            {
-
-
-                //new_connector.VisioShape = IVisio.Shape; // IVisio.Shape();
-                var h = new_connector.VisioShape.Hyperlinks.Add();
-
-                h.Name = hyperlink; // Name of Hyperlink
-                h.Address = hyperlink; // Address of Hyperlink
-            }
 
             this.Connectors.Add(id, new_connector);
             return new_connector;
