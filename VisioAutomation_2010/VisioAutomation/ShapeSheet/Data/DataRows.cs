cd5cd3f98ebca9c874167fae609ad713a654dc19
Saveen Reddy <saveenr@microsoft.com>
2021-12-07 21:05:35 -0800

    moved class file

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/Data/DataRows.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/Data/DataRows.cs
@@ -1,55 +1,7 @@
-using System.Collections.Generic;
 using IVisio=Microsoft.Office.Interop.Visio;
 
 namespace VisioAutomation.Core
 {
-    public class BasicList<T> : IEnumerable<T>
-    {
-        private readonly List<T> _list;
-
-        internal BasicList()
-        {
-            this._list = new List<T>();
-        }
-
-        internal BasicList(int capacity)
-        {
-            this._list = new List<T>(capacity);
-        }
-        public IEnumerator<T> GetEnumerator()
-        {
-            return this._list.GetEnumerator();
-        }
-
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public void Add(T r)
-        {
-            this._list.Add(r);
-        }
-
-        public void AddRange(IEnumerable<T> rows)
-        {
-            this._list.AddRange(rows);
-        }
-
-        public int Count
-        {
-            get
-            {
-                return this._list.Count;
-            }
-        }
-
-        public T this[int index]
-        {
-            get { return this._list[0]; }
-            //set { /* set the specified index to value here */ }
-        }
-    }
 }
 
 namespace VisioAutomation.ShapeSheet.Data
