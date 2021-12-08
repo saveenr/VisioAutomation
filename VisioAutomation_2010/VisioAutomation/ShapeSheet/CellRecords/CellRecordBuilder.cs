51ad3d505cae31cbb3e4acb4bd07e60e805e4bf9
Saveen Reddy <saveenr@microsoft.com>
2021-12-07 16:25:32 -0800

    cleanup

--- VisioAutomation_2010/VisioAutomation/ShapeSheet/CellRecords/CellRecordBuilder.cs
+++ VisioAutomation_2010/VisioAutomation/ShapeSheet/CellRecords/CellRecordBuilder.cs
@@ -2,6 +2,9 @@ using System.Collections.Generic;
 using System.Linq;
 using IVisio = Microsoft.Office.Interop.Visio;
 
+using ROWS= VisioAutomation.ShapeSheet.Data.DataRows<string>;
+using COLS = VisioAutomation.ShapeSheet.Data.DataColumns;
+
 namespace VisioAutomation.ShapeSheet.CellRecords
 {
     public abstract class CellRecordBuilder<TGroup> where TGroup : CellRecord, new()
@@ -45,15 +48,17 @@ namespace VisioAutomation.ShapeSheet.CellRecords
 
         public abstract TGroup ToCellRecord(Data.DataRow<string> row, Data.DataColumns cols);
 
-        public List<TGroup> GetCellsMultipleShapesSingleRow(IVisio.Page page, IList<int> shapeids,
+        public List<TGroup> GetCellsMultipleShapesSingleRow(
+            IVisio.Page page, 
+            IList<int> shapeids,
             Core.CellValueType type)
         {
-            this._enforce_type(CellRecordCategory.SingleRow);
+            this._enforce_category(CellRecordCategory.SingleRow);
             var cell_records = new List<TGroup>(shapeids.Count);
             var cols = this.query_cells_singlerow.Columns;
-            var rows_for_shapes =
+            ROWS rows =
                 this.__QueryCells_MultipleShapes_SingleRow(query_cells_singlerow, page, shapeids, type);
-            foreach (var row in rows_for_shapes)
+            foreach (var row in rows)
             {
                 var cell_record = this.ToCellRecord(row, cols);
                 cell_records.Add(cell_record);
@@ -62,112 +67,129 @@ namespace VisioAutomation.ShapeSheet.CellRecords
             return cell_records;
         }
 
-        private void _enforce_type(CellRecordCategory buildertype)
+        private void _enforce_category(CellRecordCategory category)
         {
-            if (this.Type != buildertype)
+            if (this.Type != category)
             {
                 throw new Exceptions.InternalAssertionException();
             }
         }
 
-        public TGroup GetCellsSingleShapeSingleRow(IVisio.Shape shape, Core.CellValueType type)
+        public TGroup GetCellsSingleShapeSingleRow(
+            IVisio.Shape shape,
+            Core.CellValueType type)
         {
-            this._enforce_type(CellRecordCategory.SingleRow);
-            var cellqueryresult = this.__QueryCells_SingleShape_SingleRow(query_cells_singlerow, shape, type);
+            this._enforce_category(CellRecordCategory.SingleRow);
+            var rows = this.__QueryCells_SingleShape_SingleRow(query_cells_singlerow, shape, type);
             var cols = this.query_cells_singlerow.Columns;
-            var first_row = cellqueryresult[0];
+            var first_row = rows[0];
             var cells = this.ToCellRecord(first_row, cols);
             return cells;
         }
 
-        public List<List<TGroup>> GetCellsMultipleShapesMultipleRows(IVisio.Page page, Core.ShapeIDPairs shapeidpairs,
+        public List<List<TGroup>> GetCellsMultipleShapesMultipleRows(
+            IVisio.Page page, 
+            Core.ShapeIDPairs shapeidpairs,
             Core.CellValueType type)
         {
-            this._enforce_type(CellRecordCategory.MultiRow);
+            this._enforce_category(CellRecordCategory.MultiRow);
             var sec_cols = this.query_sections_multirow[0];
 
-            var cellqueryresult =
+            var rowgroups =
                 __QueryCells_MultipleShapes_MultipleRows(query_sections_multirow, page, shapeidpairs, type);
             var list_cellrecords = new List<List<TGroup>>(shapeidpairs.Count);
-            foreach (var data_for_shape in cellqueryresult)
+            foreach (var rowgroup in rowgroups)
             {
-                var first_section_results = data_for_shape[0];
-                var cell_records = this._sectionshaperows_to_cellrecords(first_section_results, sec_cols);
-                list_cellrecords.Add(cell_records);
+                var first_rowgroup = rowgroup[0];
+                var records = this._sectionshaperows_to_cellrecords(first_rowgroup, sec_cols);
+                list_cellrecords.Add(records);
             }
 
             return list_cellrecords;
         }
 
-        public List<TGroup> GetCellsSingleShapeMultipleRows(IVisio.Shape shape, Core.CellValueType type)
+        public List<TGroup> GetCellsSingleShapeMultipleRows(
+            IVisio.Shape shape, 
+            Core.CellValueType type)
         {
-            this._enforce_type(CellRecordCategory.MultiRow);
+            this._enforce_category(CellRecordCategory.MultiRow);
             var sec_cols = this.query_sections_multirow[0];
-            var cellqueryresult = __QueryCells_SingleShape_MultipleRows(query_sections_multirow, shape, type);
-            var shape0_sectionshaperows0 = cellqueryresult[0];
-            var cellrecords = this._sectionshaperows_to_cellrecords(shape0_sectionshaperows0, sec_cols);
-            return cellrecords;
+            var rowgroup = __QueryCells_SingleShape_MultipleRows(query_sections_multirow, shape, type);
+            var first_rows = rowgroup[0];
+            var records = this._sectionshaperows_to_cellrecords(first_rows, sec_cols);
+            return records;
         }
 
-        private List<TGroup> _sectionshaperows_to_cellrecords(Data.DataRows<string> group_cell_value_data_row_collection,
-            Data.DataColumns cols)
+        private List<TGroup> _sectionshaperows_to_cellrecords(
+            ROWS group_cell_value_data_row_collection,
+            COLS cols)
         {
-            var cell_records = new List<TGroup>(group_cell_value_data_row_collection.Count);
+            var records = new List<TGroup>(group_cell_value_data_row_collection.Count);
             foreach (var section_row in group_cell_value_data_row_collection)
             {
-                var cell_record = this.ToCellRecord(section_row, cols);
-                cell_records.Add(cell_record);
+                var record = this.ToCellRecord(section_row, cols);
+                records.Add(record);
             }
 
-            return cell_records;
+            return records;
         }
 
-        private Data.DataRowGroup<string> __QueryCells_SingleShape_MultipleRows(Query.SectionQuery query,
-            IVisio.Shape shape, Core.CellValueType type)
+        private Data.DataRowGroup<string> __QueryCells_SingleShape_MultipleRows(
+            Query.SectionQuery query,
+            IVisio.Shape shape, 
+            Core.CellValueType type)
         {
-            var results = type switch
+            var rowgroup = type switch
             {
                 Core.CellValueType.Formula => query.GetFormulas(shape),
                 Core.CellValueType.Result => query.GetResults<string>(shape),
                 _ => throw new System.ArgumentOutOfRangeException(nameof(type))
             };
-            return results;
+            return rowgroup;
         }
 
-        private Data.DataRowGroups<string> __QueryCells_MultipleShapes_MultipleRows(Query.SectionQuery query,
-            IVisio.Page page, Core.ShapeIDPairs shapeidpairs, Core.CellValueType type)
+        private Data.DataRowGroups<string> __QueryCells_MultipleShapes_MultipleRows(
+            Query.SectionQuery query,
+            IVisio.Page page, 
+            Core.ShapeIDPairs shapeidpairs, 
+            Core.CellValueType type)
         {
-            var results = type switch
+            var rowgroups = type switch
             {
                 Core.CellValueType.Formula => query.GetFormulas(page, shapeidpairs),
                 Core.CellValueType.Result => query.GetResults<string>(page, shapeidpairs),
                 _ => throw new System.ArgumentOutOfRangeException(nameof(type))
             };
-            return results;
+            return rowgroups;
         }
 
-        private Data.DataRows<string> __QueryCells_SingleShape_SingleRow(Query.CellQuery query,
-            IVisio.Shape shape, Core.CellValueType type)
+        private ROWS __QueryCells_SingleShape_SingleRow(
+            Query.CellQuery query,
+            IVisio.Shape shape, 
+            Core.CellValueType type)
         {
-            var results = type switch
+            ROWS rows = type switch
             {
                 Core.CellValueType.Formula => query.GetFormulas(shape),
                 Core.CellValueType.Result => query.GetResults<string>(shape),
                 _ => throw new System.ArgumentOutOfRangeException(nameof(type))
             };
-            return results;
+            return rows;
         }
 
-        private Data.DataRows<string> __QueryCells_MultipleShapes_SingleRow(Query.CellQuery query,
-            IVisio.Page page, IList<int> shapeids, Core.CellValueType type)
+        private ROWS __QueryCells_MultipleShapes_SingleRow(
+            Query.CellQuery query,
+            IVisio.Page page, 
+            IList<int> shapeids, 
+            Core.CellValueType type)
         {
-            var results = type switch
+            ROWS rows = type switch
             {
                 Core.CellValueType.Formula => query.GetFormulas(page, shapeids),
                 Core.CellValueType.Result => query.GetResults<string>(page, shapeids),
                 _ => throw new System.ArgumentOutOfRangeException(nameof(type))
             };
-            return results;
+            return rows;
         }
     }
 }
